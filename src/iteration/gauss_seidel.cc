56cce4b0542454f43276ee491064d1c3ea5cd799
Weixiong Zheng <weixiong.zheng@berkeley.edu>
2017-11-03 16:48:52 -0700

    Documented and restructured GaussSeidel #47
    GaussSeidel<dim>::do_iterations has been removed.

--- src/iteration/gauss_seidel.cc
+++ src/iteration/gauss_seidel.cc
@@ -13,39 +13,6 @@ GaussSeidel<dim>::~GaussSeidel ()
 }
 
 template <int dim>
-void GaussSeidel<dim>::do_iterations
-(std::vector<Vector<double> > &sflxes_proc,
- std::vector<std_cxx11::shared_ptr<EquationBase<dim> > > &equ_ptrs,
- std_cxx11::shared_ptr<IGBase<dim> > ig_ptr)
-{
-  AssertThrow (!this->is_eigen_problem,
-               ExcMessage("This function shall not be called if it's eigen prob."));
-  // purpose of overriding do_iteration is such that we provide another outer
-  // iteration here if NDA is used
-  if (this->do_nda)
-  {
-    AssertThrow (equ_ptrs.size()==2,
-                 ExcMessage("There should be two equation pointers if do NDA"));
-    // assemble bilinear forms of available equations
-    for (unsigned int i=0; i<equ_ptrs.size(); ++i)
-      equ_ptrs[i]->assemble_bilinear_form ();
-    // do nothing for closure at the beginning
-    equ_ptrs.back()->assemble_closure_bilinear_form (equ_ptrs.front(), false);
-    // TODO: fill this up with NDA
-  }
-  else
-  {
-    AssertThrow (equ_ptrs.size()==1,
-                 ExcMessage("There should be one equation pointer without NDA"));
-    // assemble bilinear forms of available equations
-    equ_ptrs.back()->assemble_bilinear_form ();
-    // multigroup iterations. Note we would not need to override mg_iterations
-    // until we want to do JFNK
-    this->mg_iterations (sflxes_proc, equ_ptrs, ig_ptr);
-  }
-}
-
-template <int dim>
 void GaussSeidel<dim>::nonthermal_solves
 (std::vector<Vector<double> > &sflxes_proc,
  std::vector<std_cxx11::shared_ptr<EquationBase<dim> > > &equ_ptrs,
--- src/iteration/gauss_seidel.h
+++ src/iteration/gauss_seidel.h
@@ -3,24 +3,57 @@
 
 #include "mg_base.h"
 
+//! This class provides Gauss-Seidel scheme for MG calculations.
+/*!
+ This class implements Gauss-Seidel scheme for MG calculations. Specifically,
+ MGBase<dim>::nonthermal_solves and MGBase<dim>::thermal_iterations are overriden
+ using Gauss-Seidel iterations.
+ 
+ \author Weixiong Zheng
+ \date 2017/09
+ */
 template <int dim>
 class GaussSeidel : public MGBase<dim>
 {
 public:
+  /*!
+   Class constructor.
+   
+   \param prm const ParameterHandler object.
+   */
   GaussSeidel (const ParameterHandler &prm);
+  
+  //! Class destructor.
   ~GaussSeidel ();
   
+  /*!
+   A function overriding MGBase<dim>::nonthermal_solves. The function implements
+   Gauss-Seidel scheme with one-pass serial solves (energy groupwise) from highest
+   energy group until reaching out to the last group before the thermal group 
+   boundary.
+   
+   \param sflxes_proc Scalar fluxes living on current processor for all groups.
+   \param equ_ptrs A vector of pointers of EquationBase<dim> object.
+   \param ig_ptr A pointer of IGBase<dim> object.
+   \return Void.
+   */
   void nonthermal_solves
   (std::vector<Vector<double> > &sflxes_proc,
    std::vector<std_cxx11::shared_ptr<EquationBase<dim> > > &equ_ptrs,
    std_cxx11::shared_ptr<IGBase<dim> > ig_ptr);
   
+  /*!
+   A function overriding MGBase<dim>::thermal_iterations. The function implements
+   Gauss-Seidel scheme for solving transport problems in thermal groups 
+   iteratively. The starting group is the highest energy group affected by 
+   upscattering.
+   
+   \param sflxes_proc Scalar fluxes living on current processor for all groups.
+   \param equ_ptrs A vector of pointers of EquationBase<dim> object.
+   \param ig_ptr A pointer of IGBase<dim> object.
+   \return Void.
+   */
   void thermal_iterations
-  (std::vector<Vector<double> > &sflxes_proc,
-   std::vector<std_cxx11::shared_ptr<EquationBase<dim> > > &equ_ptrs,
-   std_cxx11::shared_ptr<IGBase<dim> > ig_ptr);
-  
-  void do_iterations
   (std::vector<Vector<double> > &sflxes_proc,
    std::vector<std_cxx11::shared_ptr<EquationBase<dim> > > &equ_ptrs,
    std_cxx11::shared_ptr<IGBase<dim> > ig_ptr);
