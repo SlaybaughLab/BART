30d7dd7da6826bc8c38c64e318d52d39ff5da310
Weixiong Zheng <weixiong.zheng@berkeley.edu>
2017-11-07 13:02:46 -0800

    Documented LSGC #39
    The main change is adding descriptions and literature reference
    in hyperlink.

--- src/aqdata/lsgc.h
+++ src/aqdata/lsgc.h
@@ -6,6 +6,17 @@
 using namespace dealii;
 
 //! This class produces level-symmetric Gauss-Chebyshev quadrature.
+/*!
+ This class is derived from AQBase<dim> with LSGC rule. The polar angular points
+ are determined by 1D Gauss-Legendre quadrature. Azimuthally, the points are
+ placed uniformly per polar level. Detailed descriptions can be found in literature
+ such as <a href="http://oaktrust.library.tamu.edu/bitstream/handle/1969.1/ETD-T
+ AMU-2010-12-8586/JARRELL-DISSERTATION.pdf?sequence=2" style="color:blue"><b>
+ J. J. Jarrell's dissertation</b></a>.
+ 
+ \author Weixiong Zheng
+ \date 2017/04
+ */
 template <int dim>
 class LSGC : public AQBase<dim>
 {
@@ -21,7 +32,7 @@ public:
   ~LSGC ();
   
   /*!
-   This function override AQBase<dim>::produce_angular_quad () to specifically
+   This function overrides AQBase<dim>::produce_angular_quad () to specifically
    produce level-symmetric Gauss-Chebyshev quadrature.
    
    \return Void.
