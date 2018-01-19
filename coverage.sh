c093fc371bf164fbaf96df5ac91d03c497bfe99c
Joshua Rehak <jsrehak@gmail.com>
2018-01-19 09:46:13 -0800

    added extra wildcard to test to cover cpp and cc files

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,5 @@
 lcov --directory . --capture --output-file coverage.info # capture coverage info
 lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
-lcov --remove coverage.info '*_test.cpp' --output-file coverage.info
+lcov --remove coverage.info '*_test.*' --output-file coverage.info
 lcov --list coverage.info #debug info
 bash <(curl -s https://codecov.io/bash)
