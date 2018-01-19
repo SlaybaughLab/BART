69c48c4b51f3c998b0ef4859f40a4feb7a2923bd
Joshua Rehak <jsrehak@gmail.com>
2018-01-19 09:43:26 -0800

    added testing to main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,14 @@
 #include "aqdata/aq_base.h"
 
-int main ()
-{
+#ifdef TEST
+#include "gtest/gtest.h"
+#endif
+
+int main(int argc, char* argv[]) {
+#ifdef TEST
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+#else  
   return 0;
+#endif
 }
