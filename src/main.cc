dbe667e7873fa068668d3372784e6711c1d29016
Joshua Rehak <jsrehak@gmail.com>
2018-01-30 15:40:35 -0800

    added google mock and command line parser to main

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,25 @@
-#include "aqdata/aq_base.h"
-
 #ifdef TEST
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include <unistd.h>
+#include <cstdlib>
+//bool TEST_REPORT = false;
 #endif
+
+//#include "aqdata/aq_base.h"
 
 int main(int argc, char* argv[]) {
 #ifdef TEST
-  ::testing::InitGoogleTest(&argc, argv);
+  // Parse optional arguments
+  // int c;
+  // while ((c = getopt (argc, argv, "r")) != -1)
+  //   switch(c) {
+  //     case 'r':
+  //       TEST_REPORT = true;
+  //   }
+        
+  // // Testing
+  ::testing::InitGoogleMock(&argc, argv);
   return RUN_ALL_TESTS();
 #else  
   return 0;
