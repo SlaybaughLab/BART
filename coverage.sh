fc076c81f469add00e98a9cf18a81a4e5f35c5f7
Joshua Rehak <jsrehak@gmail.com>
2018-01-30 15:41:23 -0800

    updated travist.yml and coverage.sh to work properly

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,9 @@
+find . -type f \( -iname \*.gcno -or -iname \*.gcda \) -exec cp {} . \;
 lcov --directory . --capture --output-file coverage.info # capture coverage info
-lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
-lcov --remove coverage.info '*_test.*' --output-file coverage.info
+lcov --remove coverage.info '/usr/*' \
+     '*_test.*' \
+     '/home/dealii/BART/inc/*' \
+     '/home/dealii/dealii-v8.5.0/*' \
+     '/home/dealii/libs/*' \
+     --output-file coverage.info # filter out system and includes
 lcov --list coverage.info #debug info
-bash <(curl -s https://codecov.io/bash)
