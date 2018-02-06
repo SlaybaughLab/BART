6ca66e14ef565950a68d59f1536b82be87ca254f
Joshua Rehak <jsrehak@gmail.com>
2018-02-06 14:09:14 -0800

    changed temp to actual in GoldStreamEvaluator

--- src/test_helpers/gold_stream_evaluator.cc
+++ src/test_helpers/gold_stream_evaluator.cc
@@ -7,34 +7,34 @@
 namespace btest {
 
 void GoldStreamEvaluator::AdoptStreams(std::unique_ptr<std::istream> gold_stream,
-                                   std::unique_ptr<std::istream> temp_stream) {
+                                   std::unique_ptr<std::istream> actual_stream) {
   gold_stream_ = std::move(gold_stream);
-  temp_stream_ = std::move(temp_stream);
+  actual_stream_ = std::move(actual_stream);
   gold_good_ = gold_stream_->good();
-  temp_good_ = temp_stream_->good();
+  actual_good_ = actual_stream_->good();
 }
 
 bool GoldStreamEvaluator::Compare() {
-  if (!gold_good_ || !temp_good_)
+  if (!gold_good_ || !actual_good_)
     throw std::runtime_error("Cannot compare bad streams");
 
   ResetStreams();
 
   std::string gold_line;
-  std::string temp_line;
+  std::string actual_line;
   bool same = true;
 
   while (getline(*gold_stream_, gold_line))
   {
     //Check if files are identical
-    getline(*temp_stream_, temp_line);
-    if (gold_line != temp_line)
+    getline(*actual_stream_, actual_line);
+    if (gold_line != actual_line)
     {
       same = false;
     }
   }
 
-  if (!gold_stream_->eof() || !temp_stream_->eof())
+  if (!gold_stream_->eof() || !actual_stream_->eof())
   {
     //One file longer than the other
     same = false;
@@ -45,22 +45,22 @@ bool GoldStreamEvaluator::Compare() {
 }
 
 std::string GoldStreamEvaluator::GetDiff() {
-  if (!gold_good_ || !temp_good_)
+  if (!gold_good_ || !actual_good_)
     throw std::runtime_error("Cannot diff bad streams");
   
   //Generate diff and return output stream with it
   std::ostringstream diff_stream;
   std::string line_buffer;
-  std::vector<std::string> gold_lines, temp_lines;
+  std::vector<std::string> gold_lines, actual_lines;
 
   ResetStreams();
 
   while(getline(*gold_stream_, line_buffer))
     gold_lines.push_back(line_buffer);
-  while(getline(*temp_stream_, line_buffer))
-    temp_lines.push_back(line_buffer);
+  while(getline(*actual_stream_, line_buffer))
+    actual_lines.push_back(line_buffer);
 
-  dtl::Diff<std::string> diff(gold_lines, temp_lines);
+  dtl::Diff<std::string> diff(gold_lines, actual_lines);
   diff.onHuge();
   diff.compose();
   diff.composeUnifiedHunks();
@@ -71,7 +71,7 @@ std::string GoldStreamEvaluator::GetDiff() {
 
 void GoldStreamEvaluator::ResetStreams() {
   gold_stream_->seekg(0, std::ios::beg);
-  temp_stream_->seekg(0, std::ios::beg);
+  actual_stream_->seekg(0, std::ios::beg);
 }
 
 }
