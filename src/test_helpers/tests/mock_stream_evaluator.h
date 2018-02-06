860cf2a45fc75d33111b0c1a0f64bd87652e59ae
Joshua Rehak <jsrehak@gmail.com>
2018-02-06 15:36:00 -0800

    added goldtest to BartTestHelper

--- src/test_helpers/tests/mock_stream_evaluator.h
+++ src/test_helpers/tests/mock_stream_evaluator.h
@@ -7,8 +7,9 @@ class MockStreamEvaluator : public StreamEvaluatorI {
  public:
   MOCK_CONST_METHOD0(Compare, bool());
   MOCK_CONST_METHOD0(GetDiff, std::string());
-  MOCK_CONST_METHOD0(GoldGood, bool());
-  MOCK_CONST_METHOD0(TempGood, bool());
+  MOCK_CONST_METHOD0(RunGoldTest, bool());
+  MOCK_CONST_METHOD0(GoldGood, const bool&());
+  MOCK_CONST_METHOD0(ActualGood, const bool&());
 };
 
 }
