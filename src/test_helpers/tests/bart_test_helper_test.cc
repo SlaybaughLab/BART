d0ffacd7343738557578d8a6202540a73fe76560
Joshua Rehak <jsrehak@gmail.com>
2018-02-08 15:30:56 -0800

    moved rmdir in tests to teardown

--- src/test_helpers/tests/bart_test_helper_test.cc
+++ src/test_helpers/tests/bart_test_helper_test.cc
@@ -13,16 +13,21 @@ class BartTestHelperTest : public ::testing::Test {
  protected:
   virtual void SetUp() {}
   std::string gold_files_directory = "test_data/";
+  btest::BartTestHelper test_helper;
+  virtual void TearDown() {
+    std::string report_directory = test_helper.GetReportDirectory();
+    if (report_directory != "")
+      rmdir(test_helper.GetReportDirectory().c_str());
+  }
 };
 
 TEST_F(BartTestHelperTest, InitializationNoReport) {
-  btest::BartTestHelper test_helper;
   ASSERT_EQ(test_helper.GetReportDirectory(), "");
 }
 
-TEST_F(BartTestHelperTest, InitalizeReport) {
-  btest::BartTestHelper test_helper(true, gold_files_directory);
-  std::string report_directory = test_helper.GetReportDirectory();
+TEST_F(BartTestHelperTest, ConstructorReport) {
+  btest::BartTestHelper test_helper2(true, gold_files_directory);
+  std::string report_directory = test_helper2.GetReportDirectory();
   // Verify report directory name
   EXPECT_THAT(report_directory,
               MatchesRegex(gold_files_directory + "........_...._fail"));
@@ -33,7 +38,6 @@ TEST_F(BartTestHelperTest, InitalizeReport) {
 }
 
 TEST_F(BartTestHelperTest, ReInitalizeReport) {
-  btest::BartTestHelper test_helper;
   test_helper.ReInit(true, gold_files_directory);
   std::string report_directory = test_helper.GetReportDirectory();
   // Verify report directory name
@@ -42,17 +46,17 @@ TEST_F(BartTestHelperTest, ReInitalizeReport) {
   // Verify report directory existence
   struct stat sb;
   ASSERT_TRUE(stat(report_directory.c_str(), &sb) == 0 && S_ISDIR(sb.st_mode));
-  rmdir(report_directory.c_str());
+  //rmdir(report_directory.c_str());
 }
 
 TEST_F(BartTestHelperTest, InitalizeBadDirectory) {
   std::string bad_directory = "testing_data/";
-  ASSERT_THROW(btest::BartTestHelper test_helper(true, bad_directory),
+  ASSERT_THROW(btest::BartTestHelper new_test_helper(true, bad_directory),
                std::runtime_error);
 }
 
 TEST_F(BartTestHelperTest, CleanupSuccess) {
-  btest::BartTestHelper test_helper;
+  //btest::BartTestHelper test_helper;
   std::string filename = "actual.temp";
   std::ofstream actual_stream(filename, std::ios_base::out);
   actual_stream << "actual data";
@@ -64,7 +68,7 @@ TEST_F(BartTestHelperTest, CleanupSuccess) {
 }
 
 TEST_F(BartTestHelperTest, CleanupFail) {
-  btest::BartTestHelper test_helper;
+  //btest::BartTestHelper test_helper;
   std::string filename = "actual.temp";
   std::ofstream actual_stream(filename, std::ios_base::out);
   actual_stream << "actual data";
@@ -76,7 +80,8 @@ TEST_F(BartTestHelperTest, CleanupFail) {
 }
 
 TEST_F(BartTestHelperTest, CleanupSuccessReport) {
-  btest::BartTestHelper test_helper(true, gold_files_directory);
+  //btest::BartTestHelper test_helper(true, gold_files_directory);
+  test_helper.ReInit(true, gold_files_directory);
   std::string report_directory = test_helper.GetReportDirectory();
   std::string filename = "actual.temp";
   std::ofstream actual_stream(filename, std::ios_base::out);
@@ -86,11 +91,12 @@ TEST_F(BartTestHelperTest, CleanupSuccessReport) {
   ASSERT_TRUE(stat(filename.c_str(), &sb) == 0 && S_ISREG(sb.st_mode));
   test_helper.CleanupGold(filename, true, true);
   ASSERT_FALSE(stat(filename.c_str(), &sb) == 0);
-  rmdir(report_directory.c_str());
+  //rmdir(report_directory.c_str());
 }
 
 TEST_F(BartTestHelperTest, CleanupFailReport) {
-  btest::BartTestHelper test_helper(true, gold_files_directory);
+  //btest::BartTestHelper test_helper(true, gold_files_directory);
+  test_helper.ReInit(true, gold_files_directory);
   std::string report_directory = test_helper.GetReportDirectory();
   std::string filename = "actual.temp";
   std::ofstream actual_stream(filename, std::ios_base::out);
@@ -105,17 +111,18 @@ TEST_F(BartTestHelperTest, CleanupFailReport) {
       stat(new_name.c_str(), &sb) == 0 &&
       S_ISREG(sb.st_mode));
   remove(new_name.c_str());
-  rmdir(report_directory.c_str());
+  //rmdir(report_directory.c_str());
 }
 
 TEST_F(BartTestHelperTest, CleanupBadFileDelete) {
-  btest::BartTestHelper test_helper(false, gold_files_directory);
+  //btest::BartTestHelper test_helper(false, gold_files_directory);
+  //test_helper.ReInit(false, gold_files_directory);
   ASSERT_THROW(test_helper.CleanupGold("bad_file", true, true),
                std::runtime_error);
 }
 
 TEST_F(BartTestHelperTest, CleanupBadFileRename) {
-  btest::BartTestHelper test_helper(false, gold_files_directory);
+  //btest::BartTestHelper test_helper(false, gold_files_directory);
   ASSERT_THROW(test_helper.CleanupGold("bad_file", false, true),
                std::runtime_error);
 }
