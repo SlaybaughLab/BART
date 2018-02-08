ea9fef570a1ea66f99652776f0594652dcd9cf08
Joshua Rehak <jsrehak@gmail.com>
2018-02-08 14:57:47 -0800

    added reinit function and default constructor to bart_test_helper

--- src/test_helpers/bart_test_helper.h
+++ src/test_helpers/bart_test_helper.h
@@ -7,16 +7,17 @@ namespace btest {
 
 class BartTestHelper {
  public:
-  BartTestHelper(bool report = false,
-                 std::string gold_files_directory="/test_data");
+  BartTestHelper();
+  BartTestHelper(bool report, std::string gold_files_directory);
+  void ReInit(bool report, std::string gold_files_directory);
   const std::string& GetReportDirectory() const { return report_directory_; };
   bool GoldTest(std::string filename);
   void CleanupGold(std::string filename, bool result, bool actual_good);
   void MakeDiff(std::string filename, std::string diff);
  private:
   void MakeReportDirectory();
-  const bool report_;
-  const std::string gold_files_directory_;
+  bool report_;
+  std::string gold_files_directory_;
   std::string report_directory_;
 };
 
