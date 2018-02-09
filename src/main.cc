e96c38c29788940d57287ed40e7a53599993de5b
Joshua Rehak <jsrehak@gmail.com>
2018-02-08 16:01:35 -0800

    added reinit to main

--- src/main.cc
+++ src/main.cc
@@ -13,12 +13,12 @@
 int main(int argc, char* argv[]) {
 #ifdef TEST
   // Parse optional arguments
-  // int c;
-  // while ((c = getopt (argc, argv, "r")) != -1)
-  //   switch(c) {
-  //     case 'r':
-  //       btest::GlobalBartTestHelper().ReInit(true, "test_data/");
-  //   }
+  int c;
+  while ((c = getopt (argc, argv, "r")) != -1)
+    switch(c) {
+      case 'r':
+        btest::GlobalBartTestHelper().ReInit(true, "test_data/");
+    }
         
   // // Testing
   ::testing::InitGoogleMock(&argc, argv);
