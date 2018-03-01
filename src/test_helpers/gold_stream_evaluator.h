d7ff02ac4d6b47af0a803cedb29a2ab50593818d
Joshua Rehak <jsrehak@gmail.com>
2018-03-01 11:47:44 -0800

    added documentation for GoldStreamEvaluator and the StreamEvaluatorI interface

--- src/test_helpers/gold_stream_evaluator.h
+++ src/test_helpers/gold_stream_evaluator.h
@@ -7,25 +7,41 @@
 #include "stream_evaluator_i.h"
 
 namespace btest {
-
+//! This class will conduct a line by line comparison test on two streams.
+/*!
+ The two streams are the "gold" standard `gold_stream` and an "actual" stream
+ `actual_stream`.
+ 
+ \author Joshua Rehak
+ \date 2018/2
+ */
 class GoldStreamEvaluator : public StreamEvaluatorI {
  public:
+  //! Constructor, takes ownership of two streams for comparison
   GoldStreamEvaluator(std::unique_ptr<std::istream> gold_stream,
                     std::unique_ptr<std::istream> actual_stream);
   ~GoldStreamEvaluator() {};
+  //! Returns a bool indicating if the streams are line-by-line identical
   bool Compare() const;
+  //! Returns a diff string in unified format if the streams are different
   std::string GetDiff() const;
+  /*! Returns the result of a "Gold" test, `true` if both streams are good
+    and identical, `false` otherwise
+  */
   bool RunGoldTest() const;
+  //! Returns the status of the gold_stream
   const bool& GoldGood() const { return gold_good_; };
+  //! Returns the status of the actual_stream
   const bool& ActualGood() const { return actual_good_; };
+  //! Closes both stream
   void CloseStreams();
  private:
+  //! Resets the streams to `bof`
   void ResetStreams() const;
   bool gold_good_ = false;
   bool actual_good_ = false;
   mutable std::unique_ptr<std::istream> gold_stream_;
-  mutable std::unique_ptr<std::istream> actual_stream_;
-    
+  mutable std::unique_ptr<std::istream> actual_stream_;   
 };
 
 }
--- src/test_helpers/stream_evaluator_i.h
+++ src/test_helpers/stream_evaluator_i.h
@@ -4,14 +4,25 @@
 #include <string>
 
 namespace btest {
-
+//! This class provides an interface for a class to evaluate two streams
+/*!
+  The two streams are the "gold" standard `gold_stream` and an "actual" stream
+  `actual_stream`.
+  \author Joshua Rehak
+  \date 2018/2
+*/
 class StreamEvaluatorI {
  public:
   virtual ~StreamEvaluatorI() {};
+  //! Returns the result of the comparison
   virtual bool Compare() const = 0;
+  //! Returns the difference between the two streams
   virtual std::string GetDiff() const = 0;
+  //! Returns the result of a gold test on the two stream
   virtual bool RunGoldTest() const = 0;
+  //! Returns the status of the gold stream
   virtual const bool& GoldGood() const = 0;
+  //! Returns the status of the actual stream
   virtual const bool& ActualGood() const = 0;
 };
 
