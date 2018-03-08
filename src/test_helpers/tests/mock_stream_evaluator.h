73c0998ac26bbd67d032816c6c59f3fd7607380e
Joshua Rehak <jsrehak@gmail.com>
2018-03-08 10:16:46 -0800

    fixed formatting in MockStreamEvaluator

--- src/test_helpers/tests/mock_stream_evaluator.h
+++ src/test_helpers/tests/mock_stream_evaluator.h
@@ -1,3 +1,6 @@
+#ifndef BART_SRC_TEST_HELPERS_TEST_MOCK_STREAM_EVALUATOR_H_
+#define BART_SRC_TEST_HELPERS_TEST_MOCK_STREAM_EVALUATOR_H_
+
 #include "gmock/gmock.h"
 #include "../stream_evaluator_i.h"
 
@@ -6,10 +9,16 @@ namespace btest {
 class MockStreamEvaluator : public StreamEvaluatorI {
  public:
   MOCK_CONST_METHOD0(Compare, bool());
+  
   MOCK_CONST_METHOD0(GetDiff, std::string());
+  
   MOCK_CONST_METHOD0(RunGoldTest, bool());
+  
   MOCK_CONST_METHOD0(GoldGood, const bool&());
+  
   MOCK_CONST_METHOD0(ActualGood, const bool&());
 };
 
-}
+} // namespace btest
+
+#endif // BART_SRC_TEST_HELPERS_TEST_MOCK_STREAM_EVALUATOR_H_
