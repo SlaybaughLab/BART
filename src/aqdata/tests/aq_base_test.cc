5986939ad61fe8699b5c3ae8bdf0f98f4a2904d6
Joshua Rehak <jsrehak@gmail.com>
2018-03-09 11:03:53 -0800

    added unit test for AQBase MakeAQ() in 1D, regular, BART-84

---
+++ src/aqdata/tests/aq_base_test.cc
@@ -0,0 +1,49 @@
+#include "../aq_base.h"
+
+#include <string>
+
+#include "gtest/gtest.h"
+#include "../../test_helpers/bart_test_helper.h"
+
+class AQBaseTest : public ::testing::Test {
+ protected:
+  void SetUp ();
+
+  template<int dim>
+  void AQTest ();
+  
+  dealii::ParameterHandler prm;
+};
+
+void AQBaseTest::SetUp () {
+  prm.declare_entry ("have reflective BC", "false", dealii::Patterns::Bool(), "");
+  prm.declare_entry ("angular quadrature order", "4", dealii::Patterns::Integer (), "");
+  prm.declare_entry ("angular quadrature name", "gl",
+                     dealii::Patterns::Selection ("gl"), "");
+  prm.declare_entry ("number of groups", "1", dealii::Patterns::Integer (), "");
+}
+
+template<int dim>
+void AQBaseTest::AQTest () {
+  std::unique_ptr<AQBase<dim>> gl_ptr =
+      std::unique_ptr<AQBase<dim>> (new AQBase<dim>(prm));
+  gl_ptr->MakeAQ ();
+  auto wi = gl_ptr->GetAQWeights ();
+  auto omega_i = gl_ptr->GetAQDirs ();
+  for (int i=0; i<wi.size(); ++i) {
+    dealii::deallog << "Weight: " << wi[i] << "; Omega: ";
+    for (int j=0; j<dim; ++j)
+      dealii::deallog << omega_i[i][j] << " ";
+    dealii::deallog << std::endl;
+  }
+}
+
+TEST_F (AQBaseTest, AQBase1DTest) {
+  std::string filename = "aq_base_1d";
+  btest::GoldTestInit(filename); // Opens deal log
+  prm.declare_entry ("transport model", "regular",
+                     dealii::Patterns::Selection("regular|ep"), "");
+  AQTest<1>();
+
+  btest::GoldTestRun(filename); // Closes deal log
+}
