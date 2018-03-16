ffcf0c746383fb975811316c2e83955a29210bd3
Joshua Rehak <jsrehak@gmail.com>
2018-03-16 13:36:00 -0700

    added 2d reflection direction test for LSGC

--- src/aqdata/tests/lsgc_test.cc
+++ src/aqdata/tests/lsgc_test.cc
@@ -14,10 +14,15 @@ class LSGCTest : public ::testing::Test {
                        dealii::Patterns::Selection ("lsgc"), "");
     prm.declare_entry ("number of groups", "1",
                        dealii::Patterns::Integer (), "");
+    prm.declare_entry ("transport model", "regular",
+                     dealii::Patterns::Selection("regular|ep"), "");
   }
 
   template <int dim>
   void AQDataTest();
+
+  template<int dim>
+  void InitRefBCAndOutput();
   
   dealii::ParameterHandler prm;
 };
@@ -38,40 +43,67 @@ void LSGCTest::AQDataTest() {
   }
 }
 
-TEST_F(LSGCTest, LSGC_2d_EpTest) {
+template<int dim>
+void LSGCTest::InitRefBCAndOutput () {
+  // Initializes BC reflection directions and outputs them to the deallog
+  std::unique_ptr<AQBase<dim>> aq_base_ptr =
+      std::unique_ptr<AQBase<dim>> (new LSGC<dim>(prm));
+  aq_base_ptr->MakeAQ();
+  std::map<std::pair<int, int>, int > reflection_dirs =
+      aq_base_ptr->GetRefDirInd();
+
+  // Print values to the deallog:
+  for (auto const& entry : reflection_dirs) {
+    dealii::deallog << "Boundary ID: " << entry.first.first
+                    << ";Current dir: " << entry.first.second
+                    << ";Reflect dir: " << entry.second
+                    << std::endl;
+  } 
+}
+
+TEST_F (LSGCTest, LSGC_2d_EpTest) {
   std::string filename = "lsgc_ep_2d";
   btest::GoldTestInit(filename);
-  prm.declare_entry ("transport model", "ep",
-                     dealii::Patterns::Selection("ep"), "");
+  prm.set ("transport model", "ep");
+  
   AQDataTest<2>();
 
   btest::GoldTestRun(filename);
 }
 
-TEST_F(LSGCTest, LSGC_3d_EpTest) {
+TEST_F (LSGCTest, LSGC_3d_EpTest) {
   std::string filename = "lsgc_ep_3d";
   btest::GoldTestInit(filename);
-  prm.declare_entry ("transport model", "ep",
-                     dealii::Patterns::Selection("ep"), "");
+  prm.set ("transport model", "ep");
+
   AQDataTest<3>();
   
   btest::GoldTestRun(filename);
 }
 
-TEST_F(LSGCTest, LSGC_2d_Test) {
+TEST_F (LSGCTest, LSGC_2d_Test) {
   std::string filename = "lsgc_2d";
   btest::GoldTestInit(filename);
-  prm.declare_entry ("transport model", "regular",
-                     dealii::Patterns::Selection("regular"), "");
+
   AQDataTest<2>();
   btest::GoldTestRun(filename);
 }
 
-TEST_F(LSGCTest, LSGC_3d_Test) {
+TEST_F (LSGCTest, LSGC_3d_Test) {
   std::string filename = "lsgc_3d";
   btest::GoldTestInit(filename);
-  prm.declare_entry ("transport model", "regular",
-                     dealii::Patterns::Selection("regular"), "");
+
   AQDataTest<3>();
+  btest::GoldTestRun(filename);
+}
+
+TEST_F (LSGCTest, AQBase2DReflDir) {
+  std::string filename = "lsgc_2d_refl";
+  prm.set("have reflective BC", "true");
+
+  btest::GoldTestInit(filename);
+  
+  InitRefBCAndOutput<2>();
+  
   btest::GoldTestRun(filename);
 }
