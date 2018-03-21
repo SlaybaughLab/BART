749f144a15f25f220b12bf9f1b544ad2670a7697
Weixiong Zheng <weixiong.zheng@berkeley.edu>
2018-03-20 23:52:29 -0700

    unit testing for aq builder #75 BART-89

--- src/common/tests/bart_builder_test.cc
+++ src/common/tests/bart_builder_test.cc
@@ -2,13 +2,19 @@
 #include "../problem_definition.h"
 
 #include "gtest/gtest.h"
+#include "../../test_helpers/bart_test_helper.h"
 
 class BARTBuilderTest : public ::testing::Test {
  protected:
   void SetUp ();
 
+  // FE builder test
   template <int dim>
   void FEBuilderTest ();
+
+  // AQ builder test
+  template <int dim>
+  void AQBuilderTest ();
 
   dealii::ParameterHandler prm;
 };
@@ -69,4 +75,43 @@ TEST_F (BARTBuilderTest, FEBuilder2DTest) {
 
 TEST_F (BARTBuilderTest, FEBuilder3DTest) {
   FEBuilderTest<3> ();
+}
+
+template <int dim>
+void BARTBuilderTest::AQBuilderTest () {
+  BARTBuilder<dim> builders (prm);
+  std::unique_ptr<AQBase<dim>> aq_ptr;
+  std::string aq_name = (dim==1) ? "gl" : "lsgc";
+  prm.set ("angular quadrature name", aq_name);
+  prm.set ("angular quadrature order", "4");
+
+  // call builder function to build aq
+  builders.BuildAQ (prm, aq_ptr);
+  aq_ptr->MakeAQ();
+  auto wi = aq_ptr->GetAQWeights();
+  auto omega_i = aq_ptr->GetAQDirs();
+
+  // check output
+  std::string filename = "aq_builder_"+dealii::Utilities::int_to_string(dim)+"d";
+  btest::GoldTestInit(filename);
+  for (unsigned int i=0; i<wi.size(); ++i)
+  {
+    dealii::deallog << "Weight: " << wi[i] << "; Omega: ";
+    for (int j=0; j<dim; ++j)
+      dealii::deallog << omega_i[i][j] << " ";
+    dealii::deallog << std::endl;
+  }
+  btest::GoldTestRun(filename);
+}
+
+TEST_F (BARTBuilderTest, AQBuilder1DTest) {
+  AQBuilderTest<1> ();
+}
+
+TEST_F (BARTBuilderTest, AQBuilder2DTest) {
+  AQBuilderTest<2> ();
+}
+
+TEST_F (BARTBuilderTest, AQBuilder3DTest) {
+  AQBuilderTest<3> ();
 }
