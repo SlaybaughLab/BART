c769babc0364d3acc314e19b84d649ebc744615e
Weixiong Zheng <weixiong.zheng@berkeley.edu>
2018-03-23 14:11:58 -0700

    Added parameter declaring functions to namespace #77

--- src/common/problem_definition.h
+++ src/common/problem_definition.h
@@ -2,49 +2,43 @@
 #define BART_SRC_COMMON_PROBLEM_DEFINITION_H_
 
 #include <deal.II/base/parameter_handler.h>
-#include <deal.II/base/tensor.h>
-#include <deal.II/base/quadrature_lib.h>
 
-#include <iostream>
-#include <string>
-#include <map>
-#include <unordered_map>
-#include <vector>
-#include <set>
+const int kNMat = 50;
+const int kNGrp = 30;
 
-
-const int k_nmat = 50;
-const int k_ngrp = 30;
-
-//! This class performs parameter parsing.
+//! This namespace contains functions to perform parameter parsing.
 /*!
- This class handles user-defined parameters. For details about parameter parsing,
+ This namespace handles user-defined parameters. For details about parameter parsing,
  refer to <a href="https://www.dealii.org/8.5.0/doxygen/deal.II/classParameterHa
  ndler.html" style="color:blue"><b>ParameterHandler</b></a>.
 
  \author Weixiong Zheng
- \date 2017/06
+ \date 2017/06, 2018/03
  */
-class ProblemDefinition {
- public:
-  //! Class constructor.
-  ProblemDefinition ();
+namespace bparams {
+//! ParameterHandler object in the global scope of BART
+extern dealii::ParameterHandler GlobPrm;
 
-  //! Class destructor.
-  ~ProblemDefinition ();
+/*!
+ This function process ParameterHandler object using info read from user-provided
+ input. Specifically, it declares all possible parameter entries and parse info
+ from user-defined input file to ParameterHandler object local_prm in the arguments.
+ After the processing, local_prm contains all the necessary info to define the
+ problem and ready for other classes to retrieve the info.
 
-  /*!
-   This function process ParameterHandler object using info read from user-provided
-   input. Specifically, it declares all possible parameter entries and parse info
-   from user-defined input file to prm. After the processing, prm contains all the
-   necessary info to define the problem and ready for other classes to retrieve
-   the info.
+ \param local_prm A ParameterHandler object to be used for declaring parameter entries.
+ \return Void.
+ */
+void DeclareParameters (dealii::ParameterHandler &local_prm);
 
-   \param prm ParameterHandler object.
-   \return Void.
-   */
-  static void DeclareParameters (dealii::ParameterHandler &prm);
-};
+/*!
+ This function will call the previous function to declare parameter entries in
+ bparams::prm.
+
+ \return Void.
+*/
+void DeclareParameters ();
+}
 
 
 #endif  // BART_SRC_COMMON_PROBLEM_DEFINITION_H_
