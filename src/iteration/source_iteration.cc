1e9ef86c87b4ee9178976a3e0369da3b04f1bdbb
Weixiong Zheng <weixiong.zheng@berkeley.edu>
2018-06-17 18:00:18 -0700

    added source iteration

---
+++ src/iteration/source_iteration.cc
@@ -0,0 +1,36 @@
+#include "source_iteration.h"
+
+template <int dim>
+SourceIteration<dim>::SourceIteration (const ParameterHandler &prm,
+    std::shared_ptr<MatrixVector> mat_vec)
+    :
+    IGBase<dim> (prm, mat_vec) {}
+
+template <int dim>
+SourceIteration<dim>::~SourceIteration () {}
+
+template <int dim>
+void SourceIteration<dim>::IGIterations (
+    std::unique_ptr<EquationBase<dim>> equ_ptr,
+    const int &g) {
+  double err = 1.0;
+  int iter = 0;
+  const std::string equ_name = equ_ptr->GetEquName();
+  while (err>this->err_phi_tol_) {
+    // generate rhs for group g
+    equ_ptr->AssembleLinearForms (g);
+    // solve all the directions in group g
+    equ_ptr->SolveInGroup (g);
+    // generate moments
+    equ_ptr->GenerateMoments (this->mat_vec_->moments[equ_name],
+        this->moments_prev_, g);
+    // calculate the difference of moments for convergence check
+    err = this->EstimatePhiDiff (this->mat_vec_->moments[equ_name],
+        this->moments_prev_);
+    //this->pcout << "SI error: " << err << ", Iter: " << iter++ << std::endl;
+  }
+}
+
+template class SourceIteration<1>;
+template class SourceIteration<2>;
+template class SourceIteration<3>;
---
+++ src/iteration/source_iteration.h
@@ -0,0 +1,37 @@
+#ifndef BART_SRC_ITERATION_SOURCE_ITERATION_H_
+#define BART_SRC_ITERATION_SOURCE_ITERATION_H_
+
+#include "ig_base.h"
+
+//! This class provides source iteration scheme for in group solve.
+/*!
+ \author Weixiong Zheng
+ \date 2017/08
+ */
+template <int dim>
+class SourceIteration : public IGBase<dim> {
+ public:
+  /*!
+   Class constructor.
+
+   \param prm Const ParameterHandler object.
+   */
+  SourceIteration (const ParameterHandler &prm,
+      std::shared_ptr<FundamentalData<dim>> dat_ptr);
+
+  //! Class destructor.
+  ~SourceIteration ();
+
+  /*!
+   A function to solve SN in group using source iteration scheme.
+
+   \param sflxes_proc Scalar fluxes for all groups living on current processor.
+   \param equ_ptr A pointer to the equation of interest.
+   \param g Group index.
+   */
+  void IGIterations (
+   std::unique_ptr<EquationBase<dim> > equ_ptr,
+   const int &g);
+};
+
+#endif //BART_SRC_ITERATION_SOURCE_ITERATION_H_
