ffbb842c5f5fbc16d08af9071d3c5fff3121a43e
Weixiong Zheng <weixiong.zheng@berkeley.edu>
2018-07-29 19:50:16 -0700

    debugged iteration base

--- src/iteration/iteration_base.cc
+++ src/iteration/iteration_base.cc
@@ -1,13 +1,13 @@
 #include "iteration_base.h"
 
 template <int dim>
-IterationBase<dim>::IterationBase (const ParameterHandler &prm,
-    std::shared_ptr<FundamentalData<dim>> dat_ptr)
+IterationBase<dim>::IterationBase (const dealii::ParameterHandler &prm,
+    std::shared_ptr<FundamentalData<dim>> &dat_ptr)
     :
     mat_vec_(dat_ptr->mat_vec),
     n_group_(prm.get_integer("number of groups")),
     is_eigen_problem_(prm.get_bool("do eigenvalue calculations")),
-    do_nda_(prm.get_bool("do NDA")),
+    do_nda_(prm.get_bool("do nda")),
     ho_equ_name_(prm.get("transport model")) {}
 
 template <int dim>
@@ -20,10 +20,10 @@ IterationBase<dim>::~IterationBase () {}
 
 template <int dim>
 double IterationBase<dim>::EstimatePhiDiff (
-    std::vector<PETScWrappers::MPI::Vector*> &phis1,
-    std::vector<PETScWrappers::MPI::Vector*> &phis2) {
+    std::vector<dealii::PETScWrappers::MPI::Vector*> &phis1,
+    std::vector<dealii::PETScWrappers::MPI::Vector*> &phis2) {
   AssertThrow (phis1.size ()== phis2.size (),
-               ExcMessage ("n_groups for different phis should be identical"));
+               dealii::ExcMessage ("n_groups for different phis should be identical"));
   double err = 0.0;
   for (int i=0; i<phis1.size (); ++i)
     err = std::max (err, EstimatePhiDiff(phis1[i],phis2[i]));
@@ -45,24 +45,25 @@ double IterationBase<dim>::EstimatePhiDiff (
   dealii::Vector<double> dif = phi1;
   dif -= phi2;
   double local_numerator = dif.l1_norm ();
-  double numerator = Utilities::MPI::sum (local_numerator, MPI_COMM_WORLD);
+  double numerator = dealii::Utilities::MPI::sum (local_numerator, MPI_COMM_WORLD);
   double local_denorminator = phi1.l1_norm ();
-  double denorminator = Utilities::MPI::sum (local_denorminator, MPI_COMM_WORLD);
+  double denorminator = dealii::Utilities::MPI::sum (local_denorminator, MPI_COMM_WORLD);
   return numerator / denorminator;
 }
 
 template <int dim>
 double IterationBase<dim>::EstimatePhiDiff (
-    std::map<std::pair<int,int>, dealii::Vector<double>> &phis1,
-    std::map<std::pair<int,int>, dealii::Vector<double>> &phis2) {
+    std::map<std::tuple<int,int,int>, dealii::Vector<double>> &phis1,
+    std::map<std::tuple<int,int,int>, dealii::Vector<double>> &phis2) {
   AssertThrow (phis1.size()==phis2.size(),
-      ExcMessage("lengths of both vectors of solutions should be the same"));
+      dealii::ExcMessage("lengths of both vectors of solutions should be the same"));
   double err = 0.0;
   for (int g=0; g<phis1.size(); ++g)
-    err = std::max(err, EstimatePhiDiff(phis1[{g,0}], phis2[{g,0}]));
+    err = std::max(err, EstimatePhiDiff(phis1[std::make_tuple(g,0,0)],
+        phis2[std::make_tuple(g,0,0)]));
   return err;
 }
 
-template class IterationBase<1>
+template class IterationBase<1>;
 template class IterationBase<2>;
 template class IterationBase<3>;
--- src/iteration/iteration_base.h
+++ src/iteration/iteration_base.h
@@ -22,8 +22,8 @@ class IterationBase {
 
    \param prm Const dealii::ParameterHandler object.
    */
-  IterationBase (const ParameterHandler &prm,
-      std::shared_ptr<FundamentalData<dim>> dat_ptr);
+  IterationBase (const dealii::ParameterHandler &prm,
+      std::shared_ptr<FundamentalData<dim>> &dat_ptr);
 
   //! Virtual class destructor.
   virtual ~IterationBase ();
@@ -57,8 +57,8 @@ class IterationBase {
    * \return relative difference in vector l1 norm measure
    */
   double EstimatePhiDiff (
-      std::map<std::pair<int,int>, dealii::Vector<double>> &phis1,
-      std::map<std::pair<int,int>, dealii::Vector<double>> &phis2);
+      std::map<std::tuple<int,int,int>, dealii::Vector<double>> &phis1,
+      std::map<std::tuple<int,int,int>, dealii::Vector<double>> &phis2);
 
   /**
    * Function to measure the relative difference between two sets of deal.II
@@ -71,18 +71,20 @@ class IterationBase {
   double EstimatePhiDiff (
       dealii::Vector<double> &phi1, dealii::Vector<double> &phi2);
 
-  const unsigned int n_group_;//!< Number of groups.
+  const int n_group_;//!< Number of groups.
   const bool is_eigen_problem_;//!< Boolean to determine if it's eigenvalue problem.
   const bool do_nda_;//!< Boolean to determine if NDA is used.
 
   double total_calculation_time_; /**< total time for calculations+assemblies*/
-  unsigned int ct_ho_iters_; /**< HO iteration counts*/
-  unsigned int ct_nda_iters_; /**< NDA iteration counts*/
+  int ct_ho_iters_; /**< HO iteration counts*/
+  int ct_nda_iters_; /**< NDA iteration counts*/
 
   std::string ho_equ_name_;//!< High-order equation name
   std::string iter_equ_name_;//!< Name of equation used in iterations
 
-  dealii::Vector<double> moment_prev_;
+  //! moments from previou iteration
+  std::map<std::tuple<int,int,int>, dealii::Vector<double>> moments_prev_;
+  std::shared_ptr<MatrixVector> mat_vec_;//!< Pointer to MatrixVector object
 };
 
 #endif // BART_SRC_ITERATION_ITERATION_BASE_H_
