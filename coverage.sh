69347cbe2496cf6e25b5458dbc5b43f9ed824f3d
Joshua Rehak <jsrehak@gmail.com>
2018-08-22 13:22:50 -0700

    updated dealii version in coverage.sh, added inc folders to codecov.yml

--- coverage.sh
+++ coverage.sh
@@ -3,7 +3,7 @@ lcov --directory . --capture --output-file coverage.info # capture coverage info
 lcov --remove coverage.info '/usr/*' \
      '*_test.*' \
      '/home/dealii/BART/inc/*' \
-     '/home/dealii/dealii-v8.5.0/*' \
+     '/home/dealii/dealii.9/*' \
      '/home/dealii/libs/*' \
      --output-file coverage.info # filter out system and includes
 lcov --list coverage.info #debug info
