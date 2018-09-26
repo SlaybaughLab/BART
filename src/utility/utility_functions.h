e6946c9df2988893b09df90ca4feee3341eaf56b
Joshua Rehak <jsrehak@gmail.com>
2018-09-26 13:37:22 -0700

    updated PreciseSum to use Neumaier variation

--- src/utility/utility_functions.h
+++ src/utility/utility_functions.h
@@ -1,22 +1,28 @@
 #ifndef BART_SRC_UTILITY_UTILITY_FUNCTIONS_H_
 #define BART_SRC_UTILITY_UTILITY_FUNCTIONS_H_
 
+#include <cmath>
 #include <vector>
 
 namespace butil {
 
+//! Conducts a precise summation by reducing truncation errors.
+/*! Conduct a precise summation using the Neumaier variation of the Kahan
+  summation.
+*/
 template <typename T>
 double PreciseSum(const T values) {
-  double sum = 0;
-  double error = 0;
-  for (const double& addend : values) {
-    const double corrected_addend = addend - error;
-    const double new_sum = sum + corrected_addend;
-    // error is low order digits of corrected_addend lost when making new_sum
-    error = (new_sum - sum) - corrected_addend;
-    sum = new_sum;
+  double sum = *values.begin();
+  double correction = 0;
+  for (auto cit = values.begin() + 1; cit < values.end(); ++cit) {
+    double temporary_sum = sum + *cit;
+    if (std::abs(sum) >= std::abs(*cit))
+      correction += (sum - temporary_sum) + *cit;
+    else
+      correction += (*cit - temporary_sum) + sum;
+    sum = temporary_sum;
   }
-  return sum;
+  return sum + correction;
 }
 
 } // namespace butil
