21e24025403383a299c7ff41198cc28d65ad864c
Joshua Rehak <jsrehak@gmail.com>
2019-01-09 14:07:25 -0800

    renamed executables to bart and bart_test, added bart_test --mpi flag to indicate running mpi tests

--- src/test_main.cc
+++ src/test_main.cc
@@ -1,36 +1,57 @@
 #include <unistd.h>
 #include <getopt.h>
+#include <vector>
 
-#include <deal.II/base/mpi.h>
+//#include <deal.II/base/mpi.h>
 #include "gtest/gtest.h"
 
 #include "test_helpers/gmock_wrapper.h"
 #include "test_helpers/bart_test_helper.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char** argv) {
   // Parse optional arguments
-  ::testing::InitGoogleMock(&argc, argv);
 
-  int option_index = 0;
+  int option_index = 0, c = 0;
+  bool use_mpi = false;
 
   const struct option longopts[] =
   {
-    {"report", no_argument, nullptr, 'r'}
+    {"report", no_argument, NULL, 'r'},
+    {"mpi",    no_argument, NULL, 'm'},
+    {NULL,     0,           NULL,   0}
   };
 
-  int c;
-  while ((c = getopt_long (argc, argv, "rd:", longopts, &option_index)) != -1) {
+  while ((c = getopt_long (argc, argv, "rmd:", longopts, &option_index)) != -1) {
     switch(c) {
-      case 'r':
+      case 'r': {
         btest::GlobalBARTTestHelper().SetReport(true);
         break;
-      case 'd':
+      }
+      case 'd': {
         btest::GlobalBARTTestHelper().SetGoldFilesDirectory(optarg);
         break;
+      }
+      case 'm': {
+        use_mpi = true;
+        break;
+      }
       default:
         break;
     }
   }
+
+  std::vector<const char*> new_argv(argv, argv + argc);
+  if (use_mpi) {
+    new_argv.push_back("--gtest_filter=*MPI*");
+  } else {
+    new_argv.push_back("--gtest_filter=-*MPI*");
+  }
+  new_argv.push_back(nullptr);
+  argv = const_cast<char**>(new_argv.data());
+  argc += 1;
+
+  ::testing::InitGoogleMock(&argc, argv);
+  
   return RUN_ALL_TESTS();
 }
 
