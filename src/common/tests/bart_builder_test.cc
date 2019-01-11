a1a700f583662f7e369247a8fa81e00b511dbe81
Joshua Rehak <jsrehak@gmail.com>
2019-01-11 15:56:16 -0800

    removed tests for depreciated BuildAQ factory function

--- src/common/tests/bart_builder_test.cc
+++ src/common/tests/bart_builder_test.cc
@@ -14,33 +14,14 @@ class BARTBuilderTest : public ::testing::Test {
   template <int dim>
   void FEBuilderTest ();
 
-  // AQ builder test
-  void SetUpAQBuilderTest ();
-
-  template <int dim>
-  void AQBuilderTest ();
-
   dealii::ParameterHandler prm;
 };
 
 void BARTBuilderTest::SetUp () {
   // Note that setup for different tests
   // had better be separated for clearness.
-  SetUpAQBuilderTest();
 
   SetUpFEBuilderTest();
-}
-
-void BARTBuilderTest::SetUpAQBuilderTest () {
-  prm.declare_entry("have reflective boundary", "false",
-                    dealii::Patterns::Bool(), "");
-  prm.declare_entry("angular quadrature order", "4",
-                    dealii::Patterns::Integer(), "");
-  prm.declare_entry("angular quadrature name", "gl",
-                    dealii::Patterns::Selection("gl|lsgc"), "");
-  prm.declare_entry("number of groups", "1", dealii::Patterns::Integer(), "");
-  prm.declare_entry("transport model", "regular",
-                    dealii::Patterns::Selection("regular|ep"), "");
 }
 
 void BARTBuilderTest::SetUpFEBuilderTest () {
@@ -101,41 +82,4 @@ TEST_F (BARTBuilderTest, FEBuilder2DTest) {
 
 TEST_F (BARTBuilderTest, FEBuilder3DTest) {
   FEBuilderTest<3> ();
-}
-
-template <int dim>
-void BARTBuilderTest::AQBuilderTest () {
-  std::unique_ptr<AQBase<dim>> aq_ptr;
-  std::string aq_name = (dim==1) ? "gl" : "lsgc";
-  prm.set ("angular quadrature name", aq_name);
-  prm.set ("angular quadrature order", "4");
-
-  // call builder function to build aq
-  bbuilders::BuildAQ (prm, aq_ptr);
-  aq_ptr->MakeAQ();
-  auto wi = aq_ptr->GetAQWeights();
-  auto omega_i = aq_ptr->GetAQDirs();
-
-  // check output
-  std::string filename = "aq_builder_"+dealii::Utilities::int_to_string(dim)+"d";
-  btest::GoldTestInit(filename);
-  for (unsigned int i=0; i<wi.size(); ++i) {
-    dealii::deallog << "Weight: " << wi[i] << "; Omega: ";
-    for (int j=0; j<dim; ++j)
-      dealii::deallog << omega_i[i][j] << " ";
-    dealii::deallog << std::endl;
-  }
-  btest::GoldTestRun(filename);
-}
-
-TEST_F (BARTBuilderTest, AQBuilder1DTest) {
-  AQBuilderTest<1> ();
-}
-
-TEST_F (BARTBuilderTest, AQBuilder2DTest) {
-  AQBuilderTest<2> ();
-}
-
-TEST_F (BARTBuilderTest, AQBuilder3DTest) {
-  AQBuilderTest<3> ();
 }
