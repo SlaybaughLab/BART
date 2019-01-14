419afeb0abb1ac9b25a61c8179557992db179a87
Joshua Rehak <jsrehak@gmail.com>
2019-01-14 13:46:18 -0800

    added parameter types with EquationType

---
+++ src/problem/parameter_types.h
@@ -0,0 +1,17 @@
+#ifndef BART_SRC_PROBLEM_PARAMETER_TYPES_H_
+#define BART_SRC_PROBLEM_PARAMETER_TYPES_H_
+
+namespace bart {
+
+namespace problem {
+
+enum class EquationType {
+  kEvenParity,
+  kSelfAdjointAngularFlux,
+};
+
+} // namespace problem
+
+} // namespace bart
+
+#endif // BART_SRC_PROBLEM_PARAMETER_TYPES_H_
