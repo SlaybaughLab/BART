e389aa852d67846100301eca172e026d84bb95b5
Joshua Rehak <jsrehak@gmail.com>
2019-02-04 15:29:18 -0800

    initial commit of FiniteElement data

---
+++ src/data/finite_element.h
@@ -0,0 +1,17 @@
+#ifndef BART_SRC_DATA_FINITE_ELEMENT_H_
+#define BART_SRC_DATA_FINITE_ELEMENT_H_
+
+namespace bart {
+
+namespace data {
+
+class FiniteElement {
+  virtual ~FiniteElement() = default;
+
+};
+
+} // namespace data
+
+} // namespace bart 
+
+#endif // BART_SRC_DATA_FINITE_ELEMENT_H_
