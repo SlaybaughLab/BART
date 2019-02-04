e6a422edafdc429faf6655c664dfc2dfcdbffc96
Joshua Rehak <jsrehak@gmail.com>
2019-02-04 15:34:34 -0800

    initial commit of FiniteElement.h

--- src/data/finite_element.h
+++ src/data/finite_element.h
@@ -1,13 +1,25 @@
 #ifndef BART_SRC_DATA_FINITE_ELEMENT_H_
 #define BART_SRC_DATA_FINITE_ELEMENT_H_
 
+#include <memory>
+
+#include <deal.II/fe/fe_values.h>
+
+#include "../problem/parameter_types.h"
+
 namespace bart {
 
 namespace data {
 
+template <int dim>
 class FiniteElement {
-  virtual ~FiniteElement() = default;
+ public:
+  FiniteElement(bart::problem::DiscretizationType discretization,
+                int polynomial_degree);
+  ~FiniteElement() = default;
 
+ private:
+  std::shared_ptr<dealii::FiniteElement<dim, dim>> finite_element_values_;
 };
 
 } // namespace data
