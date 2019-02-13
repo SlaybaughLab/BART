b8eb94550e298aa848a346dd6bfec76eaf77ba56
Joshua Rehak <jsrehak@gmail.com>
2019-02-12 19:35:14 -0800

    added interface for FiniteElement and Mesh

--- src/domain/finite_element_gaussian.h
+++ src/domain/finite_element_gaussian.h
@@ -6,6 +6,7 @@
 #include <deal.II/base/quadrature_lib.h>
 #include <deal.II/fe/fe_values.h>
 
+#include "finite_element_i.h"
 #include "../problem/parameter_types.h"
 
 namespace bart {
@@ -13,28 +14,30 @@ namespace bart {
 namespace domain {
 
 template <int dim>
-class FiniteElementGaussian {
+class FiniteElementGaussian : public FiniteElementI<dim>{
  public:
   using DiscretizationType = problem::DiscretizationType;
   FiniteElementGaussian(DiscretizationType discretization,
                 int polynomial_degree);
   ~FiniteElementGaussian() = default;
 
-  int polynomial_degree() { return polynomial_degree_; };
-  dealii::FiniteElement<dim, dim> *finite_element() {
+  int polynomial_degree() const override { return polynomial_degree_; };
+  
+  dealii::FiniteElement<dim, dim> *finite_element() override {
     return finite_element_.get(); };
 
-  int dofs_per_cell() const { return finite_element_->dofs_per_cell; }
+  int dofs_per_cell() const override { return finite_element_->dofs_per_cell; }
 
-  int n_cell_quad_pts() const { return cell_quadrature_->size(); }
+  int n_cell_quad_pts() const override { return cell_quadrature_->size(); }
 
-  int n_face_quad_pts() const { return face_quadrature_->size(); }
+  int n_face_quad_pts() const override { return face_quadrature_->size(); }
 
-  dealii::FEValues<dim> *values() { return values_.get(); };
+  dealii::FEValues<dim> *values() override { return values_.get(); };
 
-  dealii::FEFaceValues<dim> *face_values() { return face_values_.get(); };
+  dealii::FEFaceValues<dim> *face_values() override {
+    return face_values_.get(); };
 
-  dealii::FEFaceValues<dim> *neighbor_face_values() {
+  dealii::FEFaceValues<dim> *neighbor_face_values() override {
     return neighbor_face_values_.get(); };
   
   dealii::QGauss<dim> *cell_quadrature() { return cell_quadrature_.get(); };
---
+++ src/domain/finite_element_i.h
@@ -0,0 +1,50 @@
+#ifndef BART_SRC_DOMAIN_FINITE_ELEMENT_I_H
+#define BART_SRC_DOMAIN_FINITE_ELEMENT_I_H
+
+#include <deal.II/fe/fe_values.h>
+
+/*! \brief Interface for a finite element object based on the dealii library.
+ *
+ * This object is responsible for returning finite element data such as: basis
+ * function values at quadrature points, basis function values on faces, numbers
+ * of quadrature points and dofs per cell.
+ * 
+ */
+
+namespace bart {
+
+namespace domain {
+
+template <int dim>
+class FiniteElementI {
+ public:
+  virtual ~FiniteElementI() = default;
+
+  // Basic FE properties
+  /*! \brief Gets polynomial degree */
+  virtual int polynomial_degree() const = 0;
+  /*! \brief Gets degrees of freedom per cell */
+  virtual int dofs_per_cell() const = 0;
+  /*! \brief Gets number of quadrature points per cell */
+  virtual int n_cell_quad_pts() const = 0;
+  /*! \brief Gets number of quadrature points per face */
+  virtual int n_face_quad_pts() const = 0;
+
+  // DealII Finite element object access. These methods access the underlying
+  // finite element objects.
+  /*! \brief Gets pointer to the underlying finite element object */
+  virtual dealii::FiniteElement<dim, dim> *finite_element() = 0;
+  /*! \brief Gets pointer to the underlying finite element values object */
+  virtual dealii::FEValues<dim> *values() = 0;
+  /*! \brief Gets pointer to underlying finite element faces values object */
+  virtual dealii::FEFaceValues<dim> *face_values() = 0;
+  /*! \brief Gets pointer to second face values object.
+   * This is often used if you need to get face values for a neighbor cell. */
+  virtual dealii::FEFaceValues<dim> *neighbor_face_values() = 0;
+};
+
+} // namespace domain
+
+} // namespace bart 
+
+#endif //BART_SRC_DOMAIN_FINITE_ELEMENT_I_H
