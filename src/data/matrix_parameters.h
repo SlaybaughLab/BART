74fda4bf48920183753c5a59886d0f994d6e3f0e
Joshua Rehak <jsrehak@gmail.com>
2019-02-15 14:00:32 -0800

    added matrix parameters struct

---
+++ src/data/matrix_parameters.h
@@ -0,0 +1,24 @@
+#ifndef BART_SRC_DATA_MATRIX_PARAMETERS_
+#define BART_SRC_DATA_MATRIX_PARAMETERS_
+
+#include <deal.II/base/index_set.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+
+/*! This struct holds the parameters necessary for creation of a parallel
+ * PETSc matrix. */
+
+namespace bart {
+
+namespace data {
+
+struct MatrixParameters {
+  dealii::IndexSet row_degrees_of_freedom;
+  dealii::IndexSet column_degrees_of_freedom;
+  dealii::DynamicSparsityPattern dsp;  
+};
+
+} // namespace data
+
+} // namespace bart
+
+#endif // BART_SRC_DATA_MATRIX_PARAMETERS_
