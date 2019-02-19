e80655cd742d34bf157502298871eb67d903ac73
Joshua Rehak <jsrehak@gmail.com>
2019-02-19 13:27:24 -0800

    added BuildMatrix to matrix_parameters.h

--- src/data/matrix_parameters.h
+++ src/data/matrix_parameters.h
@@ -3,6 +3,7 @@
 
 #include <deal.II/base/index_set.h>
 #include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/petsc_parallel_sparse_matrix.h>
 
 /*! This struct holds the parameters necessary for creation of a parallel
  * PETSc matrix. */
@@ -11,11 +12,15 @@ namespace bart {
 
 namespace data {
 
+typedef dealii::PETScWrappers::MPI::SparseMatrix MPISparseMatrix;
+
 struct MatrixParameters {
   dealii::IndexSet rows;
   dealii::IndexSet columns;
   dealii::DynamicSparsityPattern dsp;  
 };
+
+std::shared_ptr<MPISparseMatrix> BuildMatrix(MatrixParameters &parameters); 
 
 } // namespace data
 
