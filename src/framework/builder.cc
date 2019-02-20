13937543ca8cd66d9bf5247676c3f75cd04e7604
Joshua Rehak <jsrehak@gmail.com>
2019-02-19 16:22:40 -0800

    added finite element instantiation to BuilDefinition, fixed iterator in GetMaterialMapFromFile

--- src/framework/builder.cc
+++ src/framework/builder.cc
@@ -2,6 +2,7 @@
 
 #include <fstream>
 
+#include "../domain/finite_element_gaussian.h"
 #include "../domain/mesh_cartesian.h"
 
 namespace bart {
@@ -17,21 +18,23 @@ std::unique_ptr<domain::Definition<dim>> Builder<dim>::BuildDefinition() const {
   // Get material mapping from input file and build mesh
   std::string material_mapping =
       GetMaterialMapFromFile(parameters_->MaterialMapFilename());
-  domain::MeshCartesian mesh(parameters_->SpatialMax(),
-                             parameters_->NCells(),
-                             material_mapping);
+  auto mesh = std::make_unique<domain::MeshCartesian<dim>>(
+      parameters_->SpatialMax(), parameters_->NCells(), material_mapping);
+  auto finite_element = std::make_unique<domain::FiniteElementGaussian<dim>>(
+      parameters_->Discretization(), parameters_->FEPolynomialDegree());
 
-  
-  
 }
 
 template <int dim>
 std::string Builder<dim>::GetMaterialMapFromFile(std::string filename) const {
   std::ifstream input(filename);
-  std::string return_string(std::istreambuf_iterator<char>(input),
-                            std::istreambuf_iterator<char>());
+  std::string return_string((std::istreambuf_iterator<char>(input)),
+                            (std::istreambuf_iterator<char>()));
   return return_string;
 }
+
+template class Builder<1>;
+template class Builder<2>;
 
 } // namespace framework
 
