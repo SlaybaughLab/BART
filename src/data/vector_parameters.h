5cfd2cbe7af802f5a41003c3979ffca88d1639dc
Joshua Rehak <jsrehak@gmail.com>
2019-02-20 11:05:44 -0800

    added typedef for Flux

---
+++ src/data/vector_parameters.h
@@ -0,0 +1,14 @@
+#ifndef BART_SRC_DATA_VECTOR_PARAMETERS_
+#define BART_SRC_DATA_VECTOR_PARAMETERS_
+
+namespace bart {
+
+namespace data {
+
+typedef dealii::PETScWrappers::MPI::Vector Flux;
+
+} // namespace data
+
+} // namespace bart
+
+#endif // BART_SRC_DATA_VECTOR_PARAMETERS_
