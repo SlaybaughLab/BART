d617a122b78e2237ba23dd09c5a819e0d0ceab54
Joshua Rehak <jsrehak@gmail.com>
2019-02-20 11:06:04 -0800

    initial commit of convergence::FluxI

---
+++ src/convergence/flux_i.h
@@ -0,0 +1,20 @@
+#ifndef BART_SRC_CONVERGENCE_FLUX_I_H_
+#define BART_SRC_CONVERGENCE_FLUX_I_H_
+
+#include "../data/vector_parameters.h"
+
+namespace bart {
+
+namespace convergence {
+
+/*! \brief Checks for convergence between two provided fluxes. */
+
+class FluxI {
+  virtual ~FluxI() = default;
+}
+  
+} // namespace convergence
+
+} // namespace bart
+
+#endif // BART_SRC_CONVERGENCE_FLUX_I_H_
