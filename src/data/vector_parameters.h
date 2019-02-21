2637ac0254a6005da93cb50c6733c4a3527f5915
Joshua Rehak <jsrehak@gmail.com>
2019-02-21 10:11:52 -0800

    added GroupFluxes to vector_parameters

--- src/data/vector_parameters.h
+++ src/data/vector_parameters.h
@@ -1,6 +1,8 @@
 #ifndef BART_SRC_DATA_VECTOR_PARAMETERS_
 #define BART_SRC_DATA_VECTOR_PARAMETERS_
 
+#include <unordered_map>
+
 #include <deal.II/lac/petsc_parallel_vector.h>
 
 namespace bart {
@@ -8,6 +10,7 @@ namespace bart {
 namespace data {
 
 typedef dealii::PETScWrappers::MPI::Vector Flux;
+typedef std::unordered_map<int, Flux> GroupFluxes;
 
 } // namespace data
 
