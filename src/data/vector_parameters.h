17e8b1ec804c4593c99f659164e51d4b9a930425
Joshua Rehak <jsrehak@gmail.com>
2019-02-25 09:35:28 -0800

    changed GroupFluxes to GroupFluxPointers

--- src/data/vector_parameters.h
+++ src/data/vector_parameters.h
@@ -2,6 +2,7 @@
 #define BART_SRC_DATA_VECTOR_PARAMETERS_
 
 #include <map>
+#include <memory>
 
 #include <deal.II/lac/petsc_parallel_vector.h>
 
@@ -10,7 +11,8 @@ namespace bart {
 namespace data {
 
 typedef dealii::PETScWrappers::MPI::Vector Flux;
-typedef std::map<int, Flux> GroupFluxes;
+typedef int Group;
+typedef std::map<Group, std::unique_ptr<Flux>> GroupFluxPointers;
 
 } // namespace data
 
