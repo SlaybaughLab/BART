2f198f9ac375bb332a4f0df45d3de9fb0cc7c827
Joshua Rehak <jsrehak@gmail.com>
2019-02-26 10:57:16 -0800

    renamed convergence::FluxCheckerI to convergence::flux::CheckerI, added is_converged() getter

--- src/convergence/flux/checker_i.h
+++ src/convergence/flux/checker_i.h
@@ -1,19 +1,24 @@
 #ifndef BART_SRC_CONVERGENCE_FLUX_CHECKER_I_H_
 #define BART_SRC_CONVERGENCE_FLUX_CHECKER_I_H_
 
-#include "../data/vector_parameters.h"
+#include "../../data/vector_parameters.h"
 
 namespace bart {
 
 namespace convergence {
+ 
+namespace flux {
 
 /*! \brief Checks for convergence between two provided fluxes. */
 
-class FluxCheckerI {
+class CheckerI {
  public:
-  virtual ~FluxCheckerI() = default;
+  virtual ~CheckerI() = default;
   virtual bool CheckIfConverged(data::Flux &, data::Flux &) = 0;
+  virtual bool is_converged() const = 0;
 };
+
+} // namespace flux
   
 } // namespace convergence
 
