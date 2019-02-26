868c9822d47178a55a93948339980c26995687c9
Joshua Rehak <jsrehak@gmail.com>
2019-02-26 12:09:47 -0800

    renamed GroupFluxChecker to MultiChecker

---
+++ src/convergence/flux/multi_checker_i.h
@@ -0,0 +1,30 @@
+#ifndef BART_SRC_CONVERGENCE_GROUP_FLUX_MULTI_CHECKER_I_H_
+#define BART_SRC_CONVERGENCE_GROUP_FLUX_MULTI_CHECKER_I_H_
+
+#include "../../data/vector_parameters.h"
+
+namespace bart {
+
+namespace convergence {
+
+namespace flux {
+
+/*! \brief Checks that all fluxes have converged */
+
+class MultiCheckerI {
+ public:
+  virtual ~MultiCheckerI() = default;
+  /* \brief Check for convergence of all fluxes provided */
+  virtual bool CheckIfConverged(data::MultiFluxPtrs &current_iteration,
+                                data::MultiFluxPtrs &previous_iteration) = 0;
+  /* \brief Returns status of convergence (from last call to CheckIfConverged) */
+  virtual bool is_converged() const = 0;
+};
+
+} // namespace flux
+
+} // namespace convergence
+
+} // namespace bart
+
+#endif // BART_SRC_CONVERGENCE_GROUP_FLUX_MULTI_CHECKER_I_H_
