2756fe2d1410ffee28bb1e54c5e64a61ffae0bbb
Joshua Rehak <jsrehak@gmail.com>
2019-02-26 12:26:56 -0800

    updated interface in MultiCheckerI

--- src/convergence/flux/multi_checker_i.h
+++ src/convergence/flux/multi_checker_i.h
@@ -1,7 +1,11 @@
 #ifndef BART_SRC_CONVERGENCE_GROUP_FLUX_MULTI_CHECKER_I_H_
 #define BART_SRC_CONVERGENCE_GROUP_FLUX_MULTI_CHECKER_I_H_
 
+#include <memory>
+#include <optional>
+
 #include "../../data/vector_parameters.h"
+#include "single_checker_i.h"
 
 namespace bart {
 
@@ -14,11 +18,12 @@ namespace flux {
 class MultiCheckerI {
  public:
   virtual ~MultiCheckerI() = default;
-  /* \brief Check for convergence of all fluxes provided */
+  /*! \brief Check for convergence of all fluxes provided */
   virtual bool CheckIfConverged(data::MultiFluxPtrs &current_iteration,
                                 data::MultiFluxPtrs &previous_iteration) = 0;
   /* \brief Returns status of convergence (from last call to CheckIfConverged) */
   virtual bool is_converged() const = 0;
+  virtual std::optional<int> GetFailedIndex() const = 0;
 };
 
 } // namespace flux
