a140a9db08de029b7696820e75d7d3d54c1cd531
Joshua Rehak <jsrehak@gmail.com>
2019-02-26 12:33:42 -0800

    updated MultiCheckerSequential to inherit from MultiChecker

--- src/convergence/flux/multi_checker_sequential.h
+++ src/convergence/flux/multi_checker_sequential.h
@@ -1,48 +1,35 @@
-#ifndef BART_SRC_CONVERGENCE_GROUP_FLUX_CHECKER_SEQUENTIAL_H_
-#define BART_SRC_CONVERGENCE_GROUP_FLUX_CHECKER_SEQUENTIAL_H_
+#ifndef BART_SRC_CONVERGENCE_GROUP_FLUX_MULTI_CHECKER_SEQUENTIAL_H_
+#define BART_SRC_CONVERGENCE_GROUP_FLUX_MULTI_CHECKER_SEQUENTIAL_H_
 
 #include <memory>
 
-#include "flux_checker_i.h"
-#include "group_flux_checker_i.h"
+#include "../../data/vector_parameters.h"
+#include "single_checker_i.h"
+#include "multi_checker.h"
 
 
 namespace bart {
 
 namespace convergence {
 
-/*! \brief Checks each flux group sequentially for convergence.
- * Will not re-check converged groups until final convergence is believed to
- * have been reached. */
+namespace flux {
 
-class GroupFluxCheckerSequential : public GroupFluxCheckerI {
+/*! \brief Checks each flux group sequentially for convergence using a
+ * provided flux checker. Stops after the first non-converged flux is found */
+
+class MultiCheckerSequential : public MultiChecker {
  public:
-  GroupFluxCheckerSequential() = default;
-  GroupFluxCheckerSequential(std::unique_ptr<FluxCheckerI> &tester);
-  ~GroupFluxCheckerSequential() = default;
-  
-  void ProvideChecker(std::unique_ptr<FluxCheckerI> &tester) {
-    tester_ = std::move(tester); };
-  
-  bool CheckIfConverged(data::ScalarGroupFluxPtrs &current,
-                        data::ScalarGroupFluxPtrs &last) override;
-  bool CheckIfConverged(data::AngularGroupFluxPtrs &current,
-                        data::AngularGroupFluxPtrs &last) override;
-  
-  int GetFailedGroup() const { return failed_group_; };
-  int GetFailedAngle() const { return failed_angle_; };
-
- private:
-  /*! Flux convergence tester that will be used to check sequentially */
-  std::unique_ptr<FluxCheckerI> tester_ = nullptr;
-  /*! Stores the ID of the first group that failed the convergence check */
-  int failed_group_ = 0;
-  /*! Stores the ID of the first angle that failed the convergence check */
-  int failed_angle_ = 0;
+  MultiCheckerSequential() = default;
+  MultiCheckerSequential(std::unique_ptr<SingleCheckerI> &checker);
+  ~MultiCheckerSequential() = default; 
+  virtual bool CheckIfConverged(data::MultiFluxPtrs &current_iteration,
+                                data::MultiFluxPtrs &previous_iteration);
 };
+
+} // namespace flux
 
 } // namespace convergence
 
 } // namespace bart
 
-#endif // BART_SRC_CONVERGENCE_GROUP_FLUX_CHECKER_SEQUENTIAL_H_
+#endif // BART_SRC_CONVERGENCE_GROUP_FLUX_MULTI_CHECKER_SEQUENTIAL_H_
