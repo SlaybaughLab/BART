df9f348def839bf2935be2386d9843586af15cd1
Joshua Rehak <jsrehak@gmail.com>
2019-02-27 12:34:37 -0800

    fixed indentation

--- src/convergence/flux/single_checker_l1_norm.cc
+++ src/convergence/flux/single_checker_l1_norm.cc
@@ -7,7 +7,7 @@ namespace convergence {
 namespace flux {
 
 bool SingleCheckerL1Norm::CheckIfConverged(data::Flux &current_iteration,
-                                                data::Flux &previous_iteration) {
+                                           data::Flux &previous_iteration) {
   data::Flux difference{current_iteration};
   difference -= previous_iteration;
   delta_ = difference.l1_norm()/current_iteration.l1_norm();
