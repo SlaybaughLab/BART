41e271d6d9c7a5b4c876bddd7a8a68ed1d8d6dc1
Joshua Rehak <jsrehak@gmail.com>
2019-02-28 10:29:02 -0800

    initial commit of convergence::FinalI

---
+++ src/convergence/final_i.h
@@ -0,0 +1,34 @@
+#ifndef BART_SRC_CONVERGERNCE_FINAL_I_H_
+#define BART_SRC_CONVERGERNCE_FINAL_I_H_
+
+#include <optional>
+
+namespace bart {
+
+namespace convergence {
+
+/*! \brief Interface for classes that check for final convergence of a
+ * framework.
+ */
+
+class FinalI {
+  /*! Contains the status of a convergence check */
+  struct Status {
+    int iteration_number = 0;
+    int max_iterations = 0;
+    bool is_complete = false;
+    std::optional<int> failed_index = std::nullopt;
+    std::optional<double> delta = std::nullopt;
+  };
+  virtual ~FinalI() = default;
+  virtual Status CheckFinalConvergence();
+  virtual Status convergence_status();
+  virtual bool   convergence_is_complete();
+  virtual void   SetMaxIterations(int to_set);
+};
+
+} // namespace convergence
+
+} // namespace bart
+
+#endif // BART_SRC_CONVERGERNCE_FINAL_I_H_
