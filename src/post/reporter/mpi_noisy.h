ab61b1ddae3c3779f50a92cdb6b03e918bee0a5b
Joshua Rehak <jsrehak@gmail.com>
2019-03-01 13:37:53 -0800

    added initial implementation of post::reporter::MpiNoisy::Report(string) and tests

--- src/post/reporter/mpi_noisy.h
+++ src/post/reporter/mpi_noisy.h
@@ -1,7 +1,12 @@
 #ifndef BART_SRC_POST_REPORTER_MPI_NOISY_H_
 #define BART_SRC_POST_REPORTER_MPI_NOISY_H_
 
+#include <memory>
+
+#include <deal.II/base/conditional_ostream.h>
+
 #include "post/reporter/mpi_i.h"
+#include "utility/uncopyable.h"
 
 namespace bart {
 
@@ -13,10 +18,24 @@ namespace reporter {
  * convergence, or a passed string.
  */
 
-class MpiNoisy : public MpiI {
+class MpiNoisy : public MpiI, private utility::Uncopyable {
  public:
-  MpiNoisy() = default;
+  MpiNoisy(std::unique_ptr<dealii::ConditionalOStream> pout_ptr)
+      : pout_ptr_(std::move(pout_ptr)) {};
   ~MpiNoisy() = default;
+
+  void Report(const bart::convergence::Status &to_report) override {}
+  void Report(const std::string &to_report) override {
+    *pout_ptr_ << to_report;
+  }
+
+//  void Report(const bart::convergence::Status &to_report) {};
+//
+//  void Report(const std::string &to_report) {};
+
+ private:
+  std::unique_ptr<dealii::ConditionalOStream> pout_ptr_;
+
 
 };
 
--- src/post/reporter/test/mpi_noisy_test.cc
+++ src/post/reporter/test/mpi_noisy_test.cc
@@ -1,9 +1,31 @@
 #include "post/reporter/mpi_noisy.h"
 
+#include <deal.II/base/conditional_ostream.h>
+
 #include "test_helpers/gmock_wrapper.h"
 
-class ReporterMpiNoisyTest : public ::testing::Test {};
+class ReporterMpiNoisyTest : public ::testing::Test {
+ protected:
+  std::ostringstream string_stream;
+  std::unique_ptr<dealii::ConditionalOStream> pout_ptr;
+  void SetUp() override;
+};
 
-TEST_F(ReporterMpiNoisyTest, DummyTest) {
-  EXPECT_TRUE(true);
+void ReporterMpiNoisyTest::SetUp() {
+  pout_ptr = std::make_unique<dealii::ConditionalOStream>(string_stream, true);
+}
+
+/* Verifies that the constructor takes ownership of the conditional ostream
+ * pointer */
+TEST_F(ReporterMpiNoisyTest, Constructor) {
+  bart::post::reporter::MpiNoisy reporter(std::move(pout_ptr));
+  EXPECT_EQ(pout_ptr, nullptr);
+}
+
+/* Verifies that the reporter sends the string to the ostream */
+TEST_F(ReporterMpiNoisyTest, StringReport) {
+  bart::post::reporter::MpiNoisy reporter(std::move(pout_ptr));
+  std::string to_report = "report me";
+  reporter.Report(to_report);
+  EXPECT_EQ(string_stream.str(), to_report);
 }
