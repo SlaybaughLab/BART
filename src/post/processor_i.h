61057e571f341d4d2b62f9b7c7824910d32541d0
Joshua Rehak <jsrehak@gmail.com>
2019-03-01 11:57:48 -0800

    initial commit of post::ProcessorI

---
+++ src/post/processor_i.h
@@ -0,0 +1,19 @@
+#ifndef BART_SRC_POST_PROCESSOR_I_H_
+#define BART_SRC_POST_PROCESSOR_I_H_
+
+namespace bart {
+
+namespace post {
+
+class ProcessorI {
+ public:
+  virtual ~ProcessorI() = default;
+};
+
+} // namespace post
+
+} // namespace bart
+
+
+
+#endif //BART_SRC_POST_PROCESSOR_I_H_
