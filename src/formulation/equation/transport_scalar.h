22a70444c828e56a68f726aa11f004d2df1e4404
Joshua Rehak <jsrehak@gmail.com>
2019-03-06 12:32:06 -0800

    initial commit of formulation::equation::TransportScalar

---
+++ src/formulation/equation/transport_scalar.h
@@ -0,0 +1,31 @@
+#ifndef BART_SRC_FORMULATION_EQUATION_TRANSPORT_SCALAR_H_
+#define BART_SRC_FORMULATION_EQUATION_TRANSPORT_SCALAR_H_
+
+#include "formulation/types.h"
+#include "formulation/equation/transport.h"
+
+namespace bart {
+
+namespace formulation {
+
+namespace equation {
+
+template <int dim>
+class TransportScalar : public Transport<dim> {
+  virtual ~TransportScalar() = default;
+
+  TransportScalar(DiscretizationType discretization)
+      : Transport<dim>(EquationType::kScalar, discretization) {};
+
+  ScalarEquations scalar_equation() const { return scalar_equation_; };
+ protected:
+  ScalarEquations scalar_equation_;
+};
+
+} // namespace equation
+
+} // namespace formulation
+
+} // namespace bart
+
+#endif // BART_SRC_FORMULATION_EQUATION_TRANSPORT_SCALAR_H_
