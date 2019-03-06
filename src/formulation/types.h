670f60e95133b7fff43d370e2efce0825c734d82
Joshua Rehak <jsrehak@gmail.com>
2019-03-06 15:32:06 -0800

    removed maps from types.h

--- src/formulation/types.h
+++ src/formulation/types.h
@@ -14,31 +14,15 @@ enum class DiscretizationType {
   kDiscontinuousFEM = 1, //!< Discontinuous FEM
 };
 
-/*! Maps discetization types to strings */
-std::map<DiscretizationType, std::string> discretization_type_name_map {
-    {DiscretizationType::kContinuousFEM, "continuous FEM"},
-    {DiscretizationType::kDiscontinuousFEM, "discontinuous FEM"}
-};
-
 /*! Broad categories of formulations of the transport equation */
 enum class EquationType {
   kScalar,  //!< Equations that solve for scalar flux directly
   kAngular, //!< Equations that solve for angular flux directly
 };
 
-/*! Maps equation types to strings */
-std::map<EquationType, std::string> equation_type_name_map {
-    {EquationType::kScalar, "scalar"},
-    {EquationType::kAngular, "angular"}
-};
-
 /*! Specific scalar formulations of the transport equation */
 enum class ScalarEquations {
   kDiffusion,
-};
-
-std::map<ScalarEquations, std::string> scalar_equations_name_map {
-    {ScalarEquations::kDiffusion, "diffusion equation"},
 };
 
 } // namespace formulation
