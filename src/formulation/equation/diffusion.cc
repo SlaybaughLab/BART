0e1e132396aacb9d680e6fa992776d835ee7747f
Joshua Rehak <jsrehak@gmail.com>
2019-03-07 11:50:05 -0800

    added FillCellBilinearTerm to formulation::equation::TransportScalar and Diffusion

--- src/formulation/equation/diffusion.cc
+++ src/formulation/equation/diffusion.cc
@@ -6,7 +6,14 @@ namespace formulation {
 
 namespace equation {
 
+template<int dim>
+void Diffusion<dim>::FillCellBilinearTerm(Matrix &to_fill,
+                                          const CellPtr &cell_ptr,
+                                          const GroupNumber group) {}
 
+template class Diffusion<1>;
+template class Diffusion<2>;
+template class Diffusion<3>;
 
 } // namespace equation
 
--- src/formulation/equation/transport_scalar.h
+++ src/formulation/equation/transport_scalar.h
@@ -1,6 +1,8 @@
 #ifndef BART_SRC_FORMULATION_EQUATION_TRANSPORT_SCALAR_H_
 #define BART_SRC_FORMULATION_EQUATION_TRANSPORT_SCALAR_H_
 
+
+
 #include "formulation/types.h"
 #include "formulation/equation/transport.h"
 
@@ -13,10 +15,24 @@ namespace equation {
 template <int dim>
 class TransportScalar : public Transport<dim> {
  public:
+  using typename Transport<dim>::CellPtr;
+  using Matrix = dealii::FullMatrix<double>;
+  using GroupNumber = int;
+
   virtual ~TransportScalar() = default;
 
   TransportScalar(const DiscretizationType discretization)
       : Transport<dim>(EquationType::kScalar, discretization) {};
+
+  /*! \brief Fills a cell matrix with the integrated bilinear term for a cell.
+   *
+   * \param[in,out] to_fill local cell matrix to fill with the integrated term.
+   * \param[in] cell_ptr the cell to use to generate the local cell matrix.
+   * \param[in] group energy group number
+   */
+  virtual void FillCellBilinearTerm(Matrix& to_fill,
+                                    const CellPtr &cell_ptr,
+                                    const GroupNumber group) = 0;
 
   ScalarEquations scalar_equation() const { return scalar_equation_; };
  protected:
