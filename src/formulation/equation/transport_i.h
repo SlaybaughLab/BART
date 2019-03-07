70825065f98e9d93c8b5f4b57bdca3c3cf153e95
Joshua Rehak <jsrehak@gmail.com>
2019-03-07 11:49:09 -0800

    added documentation to formulation::equation::TransportI

--- src/formulation/equation/transport_i.h
+++ src/formulation/equation/transport_i.h
@@ -16,12 +16,33 @@ namespace equation {
 template <int dim>
 class TransportI {
  public:
+  using CellPtr = typename dealii::DoFHandler<dim>::active_cell_iterator;
+
   virtual ~TransportI() = default;
 
+  /*! \brief Get the equation type (scalar/angular)
+   *
+   * \return formulation::EquationType that describes the type.
+   */
   virtual EquationType equation_type() const = 0;
+
+  /*! \brief Get the FEM discretization type (continuous/discontinuous)
+   *
+   * \return formulation::DiscretizationType that describes the type.
+   */
   virtual DiscretizationType discretization_type() const = 0;
+
   virtual TransportI& ProvideFiniteElement(
       std::shared_ptr<domain::FiniteElementI<dim>>) = 0;
+
+  /*! \brief Set the formulation to a specific cell.
+   *
+   * This is how cell-specific matrices will be generated.
+   *
+   * \param[in] to_set pointer to the cell to be set
+   * \return this object
+   */
+  virtual TransportI& SetCell(CellPtr &to_set) = 0;
 };
 
 } // namespace formulation
