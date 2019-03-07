e6090c294a8da83f2b016e7cf0961321daeaedf1
Joshua Rehak <jsrehak@gmail.com>
2019-03-07 11:49:36 -0800

    added SetCell member to formulation::equation::Transport

--- src/formulation/equation/transport.h
+++ src/formulation/equation/transport.h
@@ -16,6 +16,9 @@ namespace equation {
 template <int dim>
 class Transport : public TransportI<dim> {
  public:
+
+  using typename TransportI<dim>::CellPtr;
+
   Transport(EquationType equation_type, DiscretizationType discretization_type)
       : equation_type_(equation_type),
         discretization_type_(discretization_type) {}
@@ -24,11 +27,18 @@ class Transport : public TransportI<dim> {
   Transport& ProvideFiniteElement(
       std::shared_ptr<domain::FiniteElementI<dim>> finite_element) override {
     finite_element_ = finite_element;
+    return *this;
   }
 
   EquationType equation_type() const override { return equation_type_; };
   DiscretizationType discretization_type() const override {
     return discretization_type_; };
+
+  Transport& SetCell(CellPtr &to_set) {
+    if (finite_element_->values()->get_cell() != to_set)
+      finite_element_->values()->reinit(to_set);
+    return *this;
+  }
 
  protected:
   EquationType equation_type_;
