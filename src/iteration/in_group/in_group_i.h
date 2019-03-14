907679a72294b08d31dea0668cbeb8e349b2bf51
Joshua Rehak <jsrehak@gmail.com>
2019-03-14 16:42:24 -0700

    initial commit of iteration directory and InGroupI

---
+++ src/iteration/in_group/in_group_i.h
@@ -0,0 +1,22 @@
+#ifndef BART_SRC_ITERATION_IN_GROUP_
+#define BART_SRC_ITERATION_IN_GROUP_
+
+namespace bart {
+
+namespace iteration {
+
+namespace in_group {
+
+class InGroupI {
+ public:
+  virtual ~InGroupI() = default;
+};
+
+
+} // namespace in_group
+
+} // namespace iteration
+
+} // namespace bart
+
+#endif // BART_SRC_ITERATION_IN_GROUP_
