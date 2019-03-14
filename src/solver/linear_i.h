5a0afa63dd2aafd97e967101405cd462280acffe
Joshua Rehak <jsrehak@gmail.com>
2019-03-14 10:46:06 -0700

    initial commit of solver::LinearI

---
+++ src/solver/linear_i.h
@@ -0,0 +1,17 @@
+#ifndef BART_SOLVER_LINEAR_I_H_
+#define BART_SOLVER_LINEAR_I_H_
+
+namespace bart {
+
+namespace solver {
+
+class LinearI {
+ public:
+  virtual ~LinearI() = default;
+};
+
+} // namespace solver
+
+} // namespace bart
+
+#endif // BART_SOLVER_LINEAR_I_H_
