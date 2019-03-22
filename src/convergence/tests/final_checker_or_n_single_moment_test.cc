653fda83b19b9ff6b5301bbb527c645b4a482f7b
Joshua Rehak <jsrehak@gmail.com>
2019-03-22 14:19:50 -0700

    added custom assertion for comparing convergence results to FinalCheckerOrN tests

--- src/convergence/tests/final_checker_or_n_single_moment_test.cc
+++ src/convergence/tests/final_checker_or_n_single_moment_test.cc
@@ -1,6 +1,7 @@
 #include "convergence/final_checker_or_n.h"
 
 #include <memory>
+#include <optional>
 
 #include <gtest/gtest.h>
 
@@ -20,6 +21,7 @@ class ConvergenceFinalCheckerOrNSingleMomentTest : public ::testing::Test {
   using FinalSingleMomentChecker =
       FinalCheckerOrN<bart::data::MomentVector, moments::SingleMomentCheckerI>;
   std::unique_ptr<moments::SingleMomentCheckerMock> checker_ptr;
+  bart::data::MomentVector moment_one, moment_two;
   void SetUp() override;
 };
 
@@ -27,6 +29,33 @@ void ConvergenceFinalCheckerOrNSingleMomentTest::SetUp() {
   checker_ptr = std::make_unique<moments::SingleMomentCheckerMock>();
   ON_CALL(*checker_ptr, CheckIfConverged(_,_)).
       WillByDefault(Return(true));
+}
+
+// -- CUSTOM ASSERTION FOR COMPARING STATUS STRUCTS --
+
+::testing::AssertionResult CompareStatus(const Status lhs, const Status rhs) {
+  if (lhs.iteration_number != rhs.iteration_number) {
+    return ::testing::AssertionFailure() << "Iteration number mismatch "
+                                         << lhs.iteration_number << " != "
+                                         << rhs.iteration_number;
+  } else if (lhs.max_iterations != rhs.max_iterations) {
+    return ::testing::AssertionFailure() << "Max iteration number mismatch "
+                                         << lhs.max_iterations << " != "
+                                         << rhs.max_iterations;
+  } else if (lhs.is_complete != rhs.is_complete) {
+    return ::testing::AssertionFailure() << "Convergence completion mismatch "
+                                         << lhs.is_complete << " != "
+                                         << rhs.is_complete;
+  } else if (lhs.failed_index != rhs.failed_index) {
+    return ::testing::AssertionFailure() << "Failed index mismatch "
+                                         << lhs.failed_index.value_or(-1) << " != "
+                                         << rhs.failed_index.value_or(-1);
+  } else if (lhs.delta != rhs.delta) {
+    return ::testing::AssertionFailure() << "Delta mismatch "
+                                         << lhs.failed_index.value_or(-1) << " != "
+                                         << rhs.failed_index.value_or(-1);
+  }
+  return ::testing::AssertionSuccess();
 }
 
 TEST_F(ConvergenceFinalCheckerOrNSingleMomentTest, Constructor) {
@@ -49,6 +78,13 @@ TEST_F(ConvergenceFinalCheckerOrNSingleMomentTest, SettersAndGetters) {
 }
 
 // -- CONVERGENCE TESTS --
+
+TEST_F(ConvergenceFinalCheckerOrNSingleMomentTest, GoodConvergence) {
+  FinalSingleMomentChecker test_checker(std::move(checker_ptr));
+  Status good_convergence = {1, 100, true, std::nullopt, std::nullopt};
+  auto result = test_checker.CheckFinalConvergence(moment_one, moment_two);
+  EXPECT_TRUE(CompareStatus(result, good_convergence));
+}
 
 // -- ERRORS --
 // Verify setters and getters throw the correct errors
