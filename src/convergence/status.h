058b4b4005abaf40401748df185c15fa6fb6407d
Joshua Rehak <jsrehak@gmail.com>
2019-03-22 14:19:32 -0700

    added operator== to convergence::Status struct

--- src/convergence/status.h
+++ src/convergence/status.h
@@ -14,7 +14,25 @@ struct Status {
   bool is_complete = false;
   std::optional<int> failed_index = std::nullopt;
   std::optional<double> delta = std::nullopt;
+
+  bool operator==(const Status rhs) const {
+    if (iteration_number != rhs.iteration_number) {
+      return false;
+    } else if (max_iterations != rhs.max_iterations) {
+      return false;
+    } else if (is_complete != rhs.is_complete) {
+      return false;
+    } else if (failed_index != rhs.failed_index) {
+      return false;
+    } else if (delta != rhs.delta) {
+      return false;
+    }
+    return true;
+  }
 };
+
+
+
 
 } // namespace convergence
 
