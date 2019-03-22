799574f0c63346f98c047f18620e5d0e85c07891
Joshua Rehak <jsrehak@gmail.com>
2019-03-22 11:40:24 -0700

    renamed FinalOrNTest to FinalOrNSingleMomentTest

---
+++ src/convergence/tests/final_or_n_single_moment_test.cc
@@ -0,0 +1,24 @@
+#include "convergence/final_or_n.h"
+
+#include <gtest/gtest.h>
+
+#include "convergence/moments/tests/single_moment_checker_mock.h"
+#include "test_helpers/gmock_wrapper.h"
+
+namespace {
+
+using testing::Types;
+
+class ConvergenceSingleMomentFinalOrNTest : public ::testing::Test {
+ protected:
+  
+};
+
+
+
+
+} // namespace
+
+
+
+
