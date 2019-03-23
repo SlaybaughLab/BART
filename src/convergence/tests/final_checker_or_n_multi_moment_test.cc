2c460585f00e794caa6224133dd20b584bfe2c3a
Joshua Rehak <jsrehak@gmail.com>
2019-03-23 16:22:44 -0700

    finished adding tests for FinalCheckerOrN for MomentsMap

--- src/convergence/tests/final_checker_or_n_multi_moment_test.cc
+++ src/convergence/tests/final_checker_or_n_multi_moment_test.cc
@@ -25,7 +25,7 @@ class ConvergenceFinalCheckerOrNMultiMomentTest : public ::testing::Test {
   
   std::unique_ptr<NiceMock<moments::MultiMomentCheckerMock>> checker_ptr;
   
-  bart::data::MomentVector moment_map_one, moment_map_two;
+  bart::data::MomentsMap moment_map_one, moment_map_two;
   void SetUp() override;
 };
 
@@ -72,102 +72,119 @@ TEST_F(ConvergenceFinalCheckerOrNMultiMomentTest, Constructor) {
   EXPECT_EQ(checker_ptr, nullptr);
 }
 
-//// Verify setters and getters work properly
-//TEST_F(ConvergenceFinalCheckerOrNMultiMomentTest, SettersAndGetters) {
-//  FinalMultiMomentChecker test_checker(std::move(checker_ptr));
+// Verify setters and getters work properly
+TEST_F(ConvergenceFinalCheckerOrNMultiMomentTest, SettersAndGetters) {
+  FinalMultiMomentChecker test_checker(std::move(checker_ptr));
+
+  EXPECT_EQ(test_checker.max_iterations(), 100);
+  test_checker.SetMaxIterations(50);
+  EXPECT_EQ(test_checker.max_iterations(), 50);
+
+  EXPECT_EQ(test_checker.iteration(), 0);
+  test_checker.SetIteration(10);
+  EXPECT_EQ(test_checker.iteration(), 10);
+}
+
+// -- CONVERGENCE TESTS --
+
+TEST_F(ConvergenceFinalCheckerOrNMultiMomentTest, GoodConvergence) {
+  FinalMultiMomentChecker test_checker(std::move(checker_ptr));
+  Status good_convergence = {1, 100, true, std::nullopt, std::nullopt};
+
+  auto result = test_checker.CheckFinalConvergence(moment_map_one, moment_map_two);
+  EXPECT_TRUE(CompareStatus(result, good_convergence));
+  EXPECT_TRUE(CompareStatus(test_checker.convergence_status(),
+                            good_convergence));
+  EXPECT_TRUE(test_checker.convergence_is_complete());
+}
+
+TEST_F(ConvergenceFinalCheckerOrNMultiMomentTest, GoodConvergenceAfterBad) {
+  Expectation bad_convergence = EXPECT_CALL(*checker_ptr, CheckIfConverged(_,_))
+      .Times(5)
+      .WillRepeatedly(Return(false));
+  EXPECT_CALL(*checker_ptr, CheckIfConverged(_,_))
+      .After(bad_convergence)
+      .WillOnce(Return(true));
+
+  FinalMultiMomentChecker test_checker(std::move(checker_ptr));
+  Status result, good_convergence = {6, 100, true, std::nullopt, std::nullopt};
+
+  for (int i = 0; i < 6; ++i)
+    result = test_checker.CheckFinalConvergence(moment_map_one, moment_map_two);
+
+  EXPECT_TRUE(CompareStatus(result, good_convergence));
+  EXPECT_TRUE(test_checker.convergence_is_complete());
+}
+
+TEST_F(ConvergenceFinalCheckerOrNMultiMomentTest, BadConvergenceAfterGood) {
+
+  auto delta = std::make_optional<double>(0.123);
+  auto failed_index = std::make_optional<int>(5);
+
+  Expectation good_convergence = EXPECT_CALL(*checker_ptr, CheckIfConverged(_,_))
+      .Times(5)
+      .WillRepeatedly(Return(true));
+  EXPECT_CALL(*checker_ptr, delta())
+      .Times(5)
+      .WillRepeatedly(::testing::DoDefault());
+  EXPECT_CALL(*checker_ptr, failed_index())
+      .Times(5)
+      .WillRepeatedly(::testing::DoDefault());
+  Expectation bad_convergence = EXPECT_CALL(*checker_ptr, CheckIfConverged(_,_))
+      .After(good_convergence)
+      .WillOnce(Return(false));
+  EXPECT_CALL(*checker_ptr, delta())
+      .After(bad_convergence)
+      .WillOnce(Return(delta));
+  EXPECT_CALL(*checker_ptr, failed_index())
+      .After(bad_convergence)
+      .WillOnce(Return(failed_index));
+
+  FinalMultiMomentChecker test_checker(std::move(checker_ptr));
+  Status result, expected = {6, 100, false, failed_index, delta};
+
+  for (int i = 0; i < 6; ++i)
+    result = test_checker.CheckFinalConvergence(moment_map_one, moment_map_two);
+
+  EXPECT_TRUE(CompareStatus(result, expected));
+  EXPECT_FALSE(test_checker.convergence_is_complete());
+}
 //
-//  EXPECT_EQ(test_checker.max_iterations(), 100);
-//  test_checker.SetMaxIterations(50);
-//  EXPECT_EQ(test_checker.max_iterations(), 50);
-//
-//  EXPECT_EQ(test_checker.iteration(), 0);
-//  test_checker.SetIteration(10);
-//  EXPECT_EQ(test_checker.iteration(), 10);
-//}
-//
-//// -- CONVERGENCE TESTS --
-//
-//TEST_F(ConvergenceFinalCheckerOrNMultiMomentTest, GoodConvergence) {
-//  FinalMultiMomentChecker test_checker(std::move(checker_ptr));
-//  Status good_convergence = {1, 100, true, std::nullopt, std::nullopt};
-//  auto result = test_checker.CheckFinalConvergence(moment_map_one, moment_map_two);
-//  EXPECT_TRUE(CompareStatus(result, good_convergence));
-//}
-//
-//TEST_F(ConvergenceFinalCheckerOrNMultiMomentTest, GoodConvergenceAfterBad) {
-//  Expectation bad_convergence = EXPECT_CALL(*checker_ptr, CheckIfConverged(_,_))
-//      .Times(5)
-//      .WillRepeatedly(Return(false));
-//  EXPECT_CALL(*checker_ptr, CheckIfConverged(_,_))
-//      .After(bad_convergence)
-//      .WillOnce(Return(true));
-//
-//  FinalMultiMomentChecker test_checker(std::move(checker_ptr));
-//  Status result, good_convergence = {6, 100, true, std::nullopt, std::nullopt};
-//
-//  for (int i = 0; i < 6; ++i)
-//    result = test_checker.CheckFinalConvergence(moment_map_one, moment_map_two);
-//
-//  EXPECT_TRUE(CompareStatus(result, good_convergence));
-//}
-//
-//TEST_F(ConvergenceFinalCheckerOrNMultiMomentTest, BadConvergenceAfterGood) {
-//
-//  auto delta = std::make_optional<double>(0.123);
-//
-//  Expectation good_convergence = EXPECT_CALL(*checker_ptr, CheckIfConverged(_,_))
-//      .Times(5)
-//      .WillRepeatedly(Return(true));
-//  EXPECT_CALL(*checker_ptr, delta())
-//      .Times(5)
-//      .WillRepeatedly(::testing::DoDefault());
-//  Expectation bad_convergence = EXPECT_CALL(*checker_ptr, CheckIfConverged(_,_))
-//      .After(good_convergence)
-//      .WillOnce(Return(false));
-//  EXPECT_CALL(*checker_ptr, delta())
-//      .After(bad_convergence)
-//      .WillOnce(Return(delta));
-//
-//  FinalMultiMomentChecker test_checker(std::move(checker_ptr));
-//  Status result, expected = {6, 100, false, std::nullopt, delta};
-//
-//  for (int i = 0; i < 6; ++i)
-//    result = test_checker.CheckFinalConvergence(moment_map_one, moment_map_two);
-//
-//  EXPECT_TRUE(CompareStatus(result, expected));
-//}
-//
-//TEST_F(ConvergenceFinalCheckerOrNMultiMomentTest, MaxIterationsReached) {
-//
-//  auto delta = std::make_optional<double>(0.123);
-//
-//  ON_CALL(*checker_ptr, CheckIfConverged(_,_))
-//      .WillByDefault(Return(false));
-//  ON_CALL(*checker_ptr, delta())
-//      .WillByDefault(Return(delta));
-//
-//  FinalMultiMomentChecker test_checker(std::move(checker_ptr));
-//  Status result, expected = {10, 10, true, std::nullopt, delta};
-//
-//  test_checker.SetMaxIterations(10).SetIteration(9);
-//
-//  result = test_checker.CheckFinalConvergence(moment_map_one, moment_map_two);
-//
-//  EXPECT_TRUE(CompareStatus(result, expected));
-//}
-//
-//
-//
-//// -- ERRORS --
-//// Verify setters and getters throw the correct errors
-//TEST_F(ConvergenceFinalCheckerOrNMultiMomentTest, SettersBadValues) {
-//  FinalMultiMomentChecker test_checker(std::move(checker_ptr));
-//
-//  EXPECT_ANY_THROW(test_checker.SetIteration(-10));
-//  EXPECT_ANY_THROW(test_checker.SetMaxIterations(-10));
-//  EXPECT_ANY_THROW(test_checker.SetMaxIterations(0));
-//
-//}
+TEST_F(ConvergenceFinalCheckerOrNMultiMomentTest, MaxIterationsReached) {
+
+  auto delta = std::make_optional<double>(0.123);
+  auto failed_index = std::make_optional<int>(5);
+
+  ON_CALL(*checker_ptr, CheckIfConverged(_,_))
+      .WillByDefault(Return(false));
+  ON_CALL(*checker_ptr, delta())
+      .WillByDefault(Return(delta));
+  ON_CALL(*checker_ptr, failed_index())
+      .WillByDefault(Return(failed_index));
+
+  FinalMultiMomentChecker test_checker(std::move(checker_ptr));
+  Status result, expected = {10, 10, true, failed_index, delta};
+
+  test_checker.SetMaxIterations(10).SetIteration(9);
+
+  result = test_checker.CheckFinalConvergence(moment_map_one, moment_map_two);
+
+  EXPECT_TRUE(CompareStatus(result, expected));
+  EXPECT_TRUE(test_checker.convergence_is_complete());
+}
+
+
+
+// -- ERRORS --
+// Verify setters and getters throw the correct errors
+TEST_F(ConvergenceFinalCheckerOrNMultiMomentTest, SettersBadValues) {
+  FinalMultiMomentChecker test_checker(std::move(checker_ptr));
+
+  EXPECT_ANY_THROW(test_checker.SetIteration(-10));
+  EXPECT_ANY_THROW(test_checker.SetMaxIterations(-10));
+  EXPECT_ANY_THROW(test_checker.SetMaxIterations(0));
+
+}
 
 
 } // namespace
