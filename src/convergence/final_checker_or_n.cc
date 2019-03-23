38b3103f0728f8aadf4bc392d63b32a2280489f4
Joshua Rehak <jsrehak@gmail.com>
2019-03-23 16:24:05 -0700

    added implementation to final_checker_or_n for MomentsMap

--- src/convergence/final_checker_or_n.cc
+++ src/convergence/final_checker_or_n.cc
@@ -11,8 +11,8 @@ namespace convergence {
 template <>
 Status FinalCheckerOrN<data::MomentVector ,
                        moments::SingleMomentCheckerI>::CheckFinalConvergence(
-                           data::MomentVector & current_iteration,
-                           data::MomentVector & previous_iteration) {
+    data::MomentVector & current_iteration,
+    data::MomentVector & previous_iteration) {
 
   convergence_status_.is_complete =
       checker_ptr_->CheckIfConverged(current_iteration, previous_iteration);
@@ -34,12 +34,26 @@ Status FinalCheckerOrN<data::MomentsMap ,
                        moments::MultiMomentCheckerI>::CheckFinalConvergence(
     data::MomentsMap & current_iteration,
     data::MomentsMap & previous_iteration) {
+
+  convergence_status_.is_complete =
+      checker_ptr_->CheckIfConverged(current_iteration, previous_iteration);
+
+  convergence_status_.delta = checker_ptr_->delta();
+  convergence_status_.failed_index = checker_ptr_->failed_index();
+
+  ++convergence_status_.iteration_number;
+
+  if (convergence_status_.iteration_number ==
+      convergence_status_.max_iterations) {
+    convergence_status_.is_complete = true;
+  }
+
   return convergence_status_;
 }
 
 template class FinalCheckerOrN<data::MomentVector,
                                moments::SingleMomentCheckerI>;
-template class FinalCheckerOrN<data::MomentsMap ,
+template class FinalCheckerOrN<data::MomentsMap,
                                moments::MultiMomentCheckerI>;
 
 
