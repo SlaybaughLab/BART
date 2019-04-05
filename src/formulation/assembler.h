4d3f23d73b71bb11c023c73eb6d93917c4efd576
Joshua Rehak <jsrehak@gmail.com>
2019-04-05 14:51:33 -0700

    initial commit of formulation::Assembler file and test

---
+++ src/formulation/assembler.h
@@ -0,0 +1,12 @@
+#ifndef BART_SRC_FORMULATION_ASSEMBLER_H_
+#define BART_SRC_FORMULATION_ASSEMBLER_H_
+
+namespace bart {
+
+namespace formulation {
+
+} // namespace formulation
+
+} // namespace bart
+
+#endif // BART_SRC_FORMULATION_ASSEMBLER_H_
---
+++ src/formulation/tests/assembler_cfem_diffusion_test.cc
@@ -0,0 +1,12 @@
+#include "formulation/assembler.h"
+#include "formulation/scalar/cfem_diffusion.h"
+
+#include "test_helpers/gmock_wrapper.h"
+
+class FormulationAssemblerCFEMDiffusionTest : public ::testing::Test {
+
+};
+
+TEST_F(FormulationAssemblerCFEMDiffusionTest, Dummy) {
+  EXPECT_TRUE(false);
+}
