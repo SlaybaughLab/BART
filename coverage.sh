e39f96b1c6cb5f0f5542f6cefd6cb9e6184c340d
Joshua Rehak <jsrehak@gmail.com>
2019-04-18 16:44:11 -0700

    updated coverage.sh to ignore test_main.cc

--- coverage.sh
+++ coverage.sh
@@ -2,6 +2,7 @@ find . -type f \( -iname \*.gcno -or -iname \*.gcda \) -exec cp {} . \;
 lcov --directory . --capture --output-file coverage.info # capture coverage info
 lcov --remove coverage.info '/usr/*' \
      '*_test.*' \
+     '/home/bart/BART/src/test_main.cc' \
      '/home/bart/BART/inc/*' \
      '/home/bart/dealii/*' \
      --output-file coverage.info # filter out system and includes
