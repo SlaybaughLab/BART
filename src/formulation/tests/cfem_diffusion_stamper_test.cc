1f77775cfb28e728c99ab93773aa7e8c80268e80
Joshua Rehak <jsrehak@gmail.com>
2019-04-18 10:46:02 -0700

    added deal.ii implementation to tests for CFEM_DiffusionStamper

--- src/formulation/tests/cfem_diffusion_stamper_test.cc
+++ src/formulation/tests/cfem_diffusion_stamper_test.cc
@@ -2,7 +2,16 @@
 
 #include <memory>
 
+#include <deal.II/base/mpi.h>
+#include <deal.II/dofs/dof_tools.h>
+#include <deal.II/dofs/dof_handler.h>
 #include <deal.II/lac/full_matrix.h>
+#include <deal.II/distributed/tria.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_tools.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/petsc_parallel_sparse_matrix.h>
 
 #include "domain/tests/definition_mock.h"
 #include "formulation/scalar/tests/cfem_diffusion_mock.h"
@@ -67,10 +76,81 @@ TEST_F(CFEMDiffusionStamperTest, Constructor) {
   EXPECT_EQ(mock_definition_ptr, nullptr);
 }
 
-TEST_F(CFEMDiffusionStamperTest, FillCellStreamingTest) {
+class CFEMDiffusionStamperMPITests : public CFEMDiffusionStamperTest {
+ protected:
+  using Cell = typename dealii::DoFHandler<2>::active_cell_iterator;
 
+  CFEMDiffusionStamperMPITests();
+
+  void SetUp() override;
+
+  dealii::ConstraintMatrix constraint_matrix_;
+  dealii::parallel::distributed::Triangulation<2> triangulation_;
+  dealii::DoFHandler<2> dof_handler_;
+  dealii::FE_Q<2> fe_;
+  dealii::IndexSet locally_relevant_dofs;
+  dealii::IndexSet locally_owned_dofs_;
+  dealii::PETScWrappers::MPI::SparseMatrix system_matrix_, index_hits_;
+  std::vector<Cell> cells_;
+};
+
+void CFEMDiffusionStamperMPITests::SetUp() {
+  // Create triangulation
+  dealii::GridGenerator::hyper_cube(triangulation_, 0, 1);
+  triangulation_.refine_global(2);
+  // Distribute DOFS, get local index sets and cells
+  dof_handler_.distribute_dofs(fe_);
+  dealii::DoFTools::extract_locally_relevant_dofs(dof_handler_,
+                                                  locally_relevant_dofs);
+  locally_owned_dofs_ = dof_handler_.locally_owned_dofs();
+  for (auto cell = dof_handler_.begin_active(); cell != dof_handler_.end(); ++ cell) {
+    if (cell->is_locally_owned())
+      cells_.push_back(cell);
+  }
+
+  // Make constraint matrix and DSP for matrices
+  constraint_matrix_.clear();
+  constraint_matrix_.reinit(locally_relevant_dofs);
+  dealii::DoFTools::make_hanging_node_constraints(dof_handler_,
+                                                  constraint_matrix_);
+  constraint_matrix_.close();
+
+  dealii::DynamicSparsityPattern dsp(locally_relevant_dofs);
+  dealii::DoFTools::make_sparsity_pattern(dof_handler_, dsp,
+                                          constraint_matrix_, false);
+  dealii::SparsityTools::distribute_sparsity_pattern(
+      dsp,
+      dof_handler_.n_locally_owned_dofs_per_processor(),
+      MPI_COMM_WORLD, locally_relevant_dofs);
+  constraint_matrix_.condense(dsp);
+
+  // Set up MPI matrices
+  system_matrix_.reinit(locally_owned_dofs_, locally_owned_dofs_, dsp, MPI_COMM_WORLD);
+  index_hits_.reinit(locally_owned_dofs_, locally_owned_dofs_, dsp, MPI_COMM_WORLD);
+
+  std::vector<dealii::types::global_dof_index> local_dof_indices(fe_.dofs_per_cell);
+
+  for (auto cell : cells_) {
+    cell->get_dof_indices(local_dof_indices);
+    for (auto index_i : local_dof_indices) {
+      for (auto index_j : local_dof_indices) {
+        index_hits_.add(index_i, index_j, 1);
+      }
+    }
+    index_hits_.compress(dealii::VectorOperation::add);
+  }
 }
 
+CFEMDiffusionStamperMPITests::CFEMDiffusionStamperMPITests()
+    : triangulation_(MPI_COMM_WORLD,
+                   typename dealii::Triangulation<2>::MeshSmoothing(
+                       dealii::Triangulation<2>::smoothing_on_refinement |
+                           dealii::Triangulation<2>::smoothing_on_coarsening)),
+      dof_handler_(triangulation_),
+      fe_(1) {}
 
+TEST_F(CFEMDiffusionStamperMPITests, DummyMPI) {
+  EXPECT_TRUE(true);
+}
 
 } // namespace
