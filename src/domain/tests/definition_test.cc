ff6598d037bb48d0db640e169d51127189c21210
Joshua Rehak <jsrehak@gmail.com>
2019-04-18 14:02:05 -0700

    added test for domain::Defintion::GetCellMatrix and mad minor updates to the Definition tests

--- src/domain/tests/definition_test.cc
+++ src/domain/tests/definition_test.cc
@@ -1,4 +1,4 @@
-#include "../definition.h"
+#include "domain/definition.h"
 
 #include <functional>
 #include <memory>
@@ -9,9 +9,11 @@
 #include <deal.II/fe/fe_q.h>
 #include <deal.II/grid/grid_generator.h>
 
-#include "../../test_helpers/gmock_wrapper.h"
-#include "../../domain/tests/mesh_mock.h"
-#include "../../domain/tests/finite_element_mock.h"
+#include "test_helpers/gmock_wrapper.h"
+#include "domain/tests/mesh_mock.h"
+#include "domain/tests/finite_element_mock.h"
+
+namespace {
 
 using ::testing::_;
 using ::testing::NiceMock;
@@ -22,7 +24,7 @@ class DefinitionTest : public ::testing::Test {
   std::unique_ptr<bart::domain::MeshMock<2>> mesh_ptr;
   std::unique_ptr<NiceMock<bart::domain::MeshMock<2>>> nice_mesh_ptr;
   std::shared_ptr<bart::domain::FiniteElementMock<2>> fe_ptr;
-  
+
   void SetUp() override;
 };
 
@@ -50,7 +52,7 @@ TEST_F(DefinitionTest, SetUpMesh) {
 
   EXPECT_NO_THROW(test_domain.SetUpMesh(););
 }
-  
+
 TEST_F(DefinitionTest, SetUpMeshMaterialMappingError) {
   EXPECT_CALL(*nice_mesh_ptr, has_material_mapping()).
       WillOnce(::testing::Return(false));
@@ -63,13 +65,14 @@ class DOFTest : public DefinitionTest {
  protected:
   DOFTest() : fe(1) {};
   dealii::Triangulation<2> triangulation;
-  dealii::FE_Q<2>          fe;
+  dealii::FE_Q<2> fe;
   int n_cells_;
 
   void SetUp() override;
   static void SetTriangulation(dealii::Triangulation<2> &to_fill) {
     dealii::GridGenerator::hyper_cube(to_fill, -1, 1);
-    to_fill.refine_global(2);}
+    to_fill.refine_global(2);
+  }
 };
 
 void DOFTest::SetUp() {
@@ -83,6 +86,8 @@ TEST_F(DOFTest, SetUpDOFTest) {
       .WillOnce(::testing::Invoke(SetTriangulation));
   EXPECT_CALL(*fe_ptr, finite_element())
       .WillOnce(::testing::Return(&fe));
+  EXPECT_CALL(*fe_ptr, dofs_per_cell())
+      .WillOnce(::testing::Return(4));
 
   bart::domain::Definition<2> test_domain(std::move(nice_mesh_ptr), fe_ptr);
   test_domain.SetUpMesh();
@@ -90,4 +95,12 @@ TEST_F(DOFTest, SetUpDOFTest) {
 
   EXPECT_EQ(test_domain.total_degrees_of_freedom(), 25);
   EXPECT_EQ(test_domain.Cells().size(), 16);
+
+  auto matrix = test_domain.GetCellMatrix();
+  EXPECT_EQ(matrix.n_rows(), 4);
+  EXPECT_EQ(matrix.n_cols(), 4);
+
+
 }
+
+} // namespace
