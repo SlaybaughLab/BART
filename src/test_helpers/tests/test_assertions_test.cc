8903f5c5c6081d35898179079f8e57a9435b9277
Joshua Rehak <jsrehak@gmail.com>
2019-04-23 16:57:01 -0700

    updated tests in test asertions tests to use new MPI_TestFixture class

--- src/test_helpers/tests/test_assertions_test.cc
+++ src/test_helpers/tests/test_assertions_test.cc
@@ -12,6 +12,7 @@
 #include <deal.II/lac/dynamic_sparsity_pattern.h>
 #include <deal.II/lac/petsc_parallel_sparse_matrix.h>
 
+#include "test_helpers/mpi_test_fixture.h"
 #include "test_helpers/test_helper_functions.h"
 #include "test_helpers/gmock_wrapper.h"
 
@@ -69,60 +70,13 @@ TEST_F(TestAssertionsTest, BadComparisonDealiiVector) {
             bart::testing::CompareVector(dealii_vector_2, dealii_vector_1));
 }
 
-class TestAssertionsMPIMatricesTests : public::testing::Test {
+class TestAssertionsMPIMatricesTests : public bart::testing::MPI_TestFixture<2> {
  protected:
-  using Cell = typename dealii::DoFHandler<2>::active_cell_iterator;
-  TestAssertionsMPIMatricesTests();
   void SetUp() override;
-
-  dealii::ConstraintMatrix constraint_matrix_;
-  dealii::parallel::distributed::Triangulation<2> triangulation_;
-  dealii::DoFHandler<2> dof_handler_;
-  dealii::FE_Q<2> fe_;
-  dealii::IndexSet locally_relevant_dofs;
-  dealii::IndexSet locally_owned_dofs_;
-  dealii::PETScWrappers::MPI::SparseMatrix matrix_1, matrix_2;
-  std::vector<Cell> cells_;
 };
 
-TestAssertionsMPIMatricesTests::TestAssertionsMPIMatricesTests()
-    : triangulation_(MPI_COMM_WORLD,
-                     typename dealii::Triangulation<2>::MeshSmoothing(
-                         dealii::Triangulation<2>::smoothing_on_refinement |
-                             dealii::Triangulation<2>::smoothing_on_coarsening)),
-      dof_handler_(triangulation_),
-      fe_(1) {}
-
 void TestAssertionsMPIMatricesTests::SetUp() {
-  dealii::GridGenerator::hyper_cube(triangulation_, 0, 1);
-  dof_handler_.distribute_dofs(fe_);
-  dealii::DoFTools::extract_locally_relevant_dofs(dof_handler_,
-                                                  locally_relevant_dofs);
-  locally_owned_dofs_ = dof_handler_.locally_owned_dofs();
-
-  for (auto cell = dof_handler_.begin_active(); cell != dof_handler_.end(); ++ cell) {
-    if (cell->is_locally_owned())
-      cells_.push_back(cell);
-  }
-
-  constraint_matrix_.clear();
-  constraint_matrix_.reinit(locally_relevant_dofs);
-  dealii::DoFTools::make_hanging_node_constraints(dof_handler_,
-                                                  constraint_matrix_);
-  constraint_matrix_.close();
-
-  dealii::DynamicSparsityPattern dsp(locally_relevant_dofs);
-  dealii::DoFTools::make_sparsity_pattern(dof_handler_, dsp,
-                                          constraint_matrix_, false);
-  dealii::SparsityTools::distribute_sparsity_pattern(
-      dsp,
-      dof_handler_.n_locally_owned_dofs_per_processor(),
-      MPI_COMM_WORLD, locally_relevant_dofs);
-  constraint_matrix_.condense(dsp);
-
-  matrix_1.reinit(locally_owned_dofs_, locally_owned_dofs_, dsp, MPI_COMM_WORLD);
-  matrix_2.reinit(locally_owned_dofs_, locally_owned_dofs_, dsp, MPI_COMM_WORLD);
-
+  SetUpDealii();
   std::vector<dealii::types::global_dof_index> local_dof_indices(fe_.dofs_per_cell);
 
   for (auto cell : cells_) {
