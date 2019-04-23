5a090ac504f6e0cd8ec072dbd93e718d4a8f671a
Joshua Rehak <jsrehak@gmail.com>
2019-04-23 11:57:54 -0700

    added option to limit listeners to test_main

--- src/test_main.cc
+++ src/test_main.cc
@@ -14,19 +14,20 @@
 int main(int argc, char** argv) {
   // Parse optional arguments
 
-  int option_index = 0, c = 0;
+  int option_index = 0, c = 0, listener = -1;
   bool use_mpi = false;
   std::string filter = "";
 
   const struct option longopts[] =
   {
-    {"filter", no_argument, NULL, 'f'},
-    {"report", no_argument, NULL, 'r'},
-    {"mpi",    no_argument, NULL, 'm'},
-    {NULL,     0,           NULL,   0}
+    {"filter",    no_argument,       NULL, 'f'},
+    {"report",    no_argument,       NULL, 'r'},
+    {"mpi",       no_argument,       NULL, 'm'},
+    {"listeners", required_argument, NULL, 'l'},
+    {NULL,        0,                 NULL,   0}
   };
 
-  while ((c = getopt_long (argc, argv, "rmd:f:", longopts, &option_index)) != -1) {
+  while ((c = getopt_long (argc, argv, "rmd:f:l:", longopts, &option_index)) != -1) {
     switch(c) {
       case 'r': {
         btest::GlobalBARTTestHelper().SetReport(true);
@@ -42,6 +43,10 @@ int main(int argc, char** argv) {
       }
       case 'f': {
         filter = optarg;
+        break;
+      }
+      case 'l': {
+        listener = atoi(optarg);
         break;
       }
       default:
@@ -72,8 +77,10 @@ int main(int argc, char** argv) {
   int world_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-  if (world_rank != 0) {
-    delete listeners.Release(listeners.default_result_printer());
+  if (listener != -1) {
+    if (world_rank != listener) {
+      delete listeners.Release(listeners.default_result_printer());
+    }
   }
   
   // Re-seed random number generator for random number tests
