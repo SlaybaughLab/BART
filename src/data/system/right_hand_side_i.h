7171515206a905cea1d2184a68f3e8e7485b4421
Joshua Rehak <jsrehak@gmail.com>
2019-05-02 10:33:32 -0700

    initial commit of data::system::RightHandSide

---
+++ src/data/system/right_hand_side_i.h
@@ -0,0 +1,21 @@
+#ifndef BART_SRC_DATA_SYSTEM_RIGHT_HAND_SIDE_I_H_
+#define BART_SRC_DATA_SYSTEM_RIGHT_HAND_SIDE_I_H_
+
+namespace bart {
+
+namespace data {
+
+namespace system {
+
+class RightHandSideI {
+ public:
+  virtual ~RightHandSideI() = default;
+};
+
+} // namespace system
+
+} // namespace data
+
+} // namespace bart
+
+#endif // BART_SRC_DATA_SYSTEM_RIGHT_HAND_SIDE_I_H_
