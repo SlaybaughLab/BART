7a52e0fc4a3a988bac65939e3809292f03607f4a
Joshua Rehak <jsrehak@gmail.com>
2019-05-07 10:23:40 -0700

    added Moment types to SystemTypes

--- src/data/system/system_types.h
+++ src/data/system/system_types.h
@@ -1,6 +1,10 @@
 #ifndef BART_SRC_DATA_SYSTEM_RHS_LHS_TYPES_H_
 #define BART_SRC_DATA_SYSTEM_RHS_LHS_TYPES_H_
 
+#include <array>
+#include <utility>
+
+#include <deal.II/lac/vector.h>
 #include <deal.II/lac/petsc_parallel_sparse_matrix.h>
 #include <deal.II/lac/petsc_parallel_vector.h>
 
@@ -21,6 +25,22 @@ using Index = std::pair<GroupNumber, AngleIndex>;
 using MPISparseMatrix = dealii::PETScWrappers::MPI::SparseMatrix;
 //! Sparse MPI vector used for ride-hand-side vectors
 using MPIVector = dealii::PETScWrappers::MPI::Vector;
+
+using HarmonicL = int;
+//!< Spherical harmonic \f$\ell\f$ value for moments
+
+using HarmonicM = int;
+//!< Spherical harmonic \f$m\f$ value for moments
+
+/*! \typedef HarmonicIndex
+ * \brief Index of a spherical harmonic in the form \f$[g, \ell, m]\f$.
+ *
+ */
+using MomentIndex = std::array<int, 3>;
+
+using MomentVector = dealii::Vector<double>; //!< Vector for storing moments
+
+using MomentsMap = std::map<MomentIndex, MomentVector>;
 
 } // namespace system
 
