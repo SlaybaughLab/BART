562f46d04ba2cb63b47b2d7093fcde8393ee13af
Joshua Rehak <jsrehak@gmail.com>
2019-05-09 11:01:59 -0700

    initial commit of data::system::LeftHandSideI, LeftHandSideFixed and tests

---
+++ src/data/system/left_hand_side_i.h
@@ -0,0 +1,23 @@
+#ifndef BART_SRC_SYSTEM_LEFT_HAND_SIDE_I_
+#define BART_SRC_SYSTEM_LEFT_HAND_SIDE_I_
+
+namespace bart {
+
+namespace data {
+
+namespace system {
+
+class LeftHandSideI {
+ public:
+
+  virtual ~LeftHandSideI() = default;
+
+};
+
+} // namespace system
+
+} // namespace data
+
+} // namespace bart
+
+#endif // BART_SRC_SYSTEM_LEFT_HAND_SIDE_I_
---
+++ src/data/system/tests/left_hand_side_fixed_test.cc
@@ -0,0 +1,15 @@
+#include "data/system/left_hand_side_fixed.h"
+
+#include "test_helpers/gmock_wrapper.h"
+
+namespace  {
+
+class SystemLeftHandSideFixedTest : public ::testing::Test {
+
+};
+
+TEST_F(SystemLeftHandSideFixedTest, Dummy) {
+  EXPECT_TRUE(true);
+}
+
+} // namespace
