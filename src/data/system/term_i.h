cfb1d933d1cfce3ca2c6e4cd91838fecce83fdc4
Joshua Rehak <jsrehak@gmail.com>
2019-05-13 10:26:02 -0700

    added templating to TermI

--- src/data/system/term_i.h
+++ src/data/system/term_i.h
@@ -11,87 +11,91 @@ namespace data {
 
 namespace system {
 
-/*! \brief Stores and provides vectors/matrices for linear/bilinear terms.
+/*! \brief Stores and provides linear and bilinear terms.
  *
- * Vector/matrices are stored differently if they are fixed (will not change iteration
+ * This class enables storage of any data storage type, but in general uses 
+ * matrices for bilinear terms, and vectors for linear terms. 
+ * 
+ * Data objects are stored differently if they are fixed (will not change iteration
  * to iteration) or variable (do change iteration to iteration). A single one
  * is stored for the fixed terms, as they can all be stamped once and left. Each
- * variable term has its own vector, so that they can be individually updated
+ * variable term has its own data object, so that they can be individually updated
  * when needed.
  *
- * Each group and angle requires its own, so storage based on
+ * Each group and angle requires its own data object, so storage is based on
  * an index, comprised of the group number, and the _index_ of the angle.
  * Overloads are provided that only require group number, which will retrieve
  * and store those with an angle index of zero.
  */
+template <typename StorageType, typename VariableTermType>
 class TermI {
  public:
   virtual ~TermI() = default;
 
   /*! \brief Returns the set of terms that are set as variable. */
-  virtual std::unordered_set<VariableTerms> GetVariableTerms() const = 0;
+  virtual std::unordered_set<VariableTermType> GetVariableTerms() const = 0;
 
-  /*! \brief Sets the MPIVector (a pointer to the vector) used to store the fixed term.
+  /*! \brief Sets the MPIVector (a pointer to the data object) used to store the fixed term.
    *
    * @param index corresponding index for the MPIVector
-   * @param to_set pointer to the vector to set
+   * @param to_set pointer to the data object to set
    */
-  virtual void SetFixedTermPtr(Index index, std::shared_ptr<MPIVector> to_set) = 0;
-  /*! \brief Sets the MPIVector (a pointer to the vector) used to store the fixed term.
+  virtual void SetFixedTermPtr(Index index, std::shared_ptr<StorageType> to_set) = 0;
+  /*! \brief Sets the MPIVector (a pointer to the data object) used to store the fixed term.
    *
    * @param group group number to be set
-   * @param to_set pointer to the vector to set
+   * @param to_set pointer to the data object to set
    */
-  virtual void SetFixedTermPtr(GroupNumber group, std::shared_ptr<MPIVector> to_set) = 0;
+  virtual void SetFixedTermPtr(GroupNumber group, std::shared_ptr<StorageType> to_set) = 0;
   /*! \brief Returns a pointer to the fixed term for a given index
    *
    * @param index index for the fixed term
    * @return a pointer to the fixed term
    */
-  virtual std::shared_ptr<MPIVector> GetFixedTermPtr(Index index) = 0;
+  virtual std::shared_ptr<StorageType> GetFixedTermPtr(Index index) = 0;
   /*! \brief Returns a pointer to the fixed term for a given group.
    *
    * @param group group to return
    * @return a pointer to the fixed term
    */
-  virtual std::shared_ptr<MPIVector> GetFixedTermPtr(GroupNumber group) = 0;
+  virtual std::shared_ptr<StorageType> GetFixedTermPtr(GroupNumber group) = 0;
 
-  /*! \brief Sets the MPIVector (a pointer to the vector) used to store the
+  /*! \brief Sets the MPIVector (a pointer to the data object) used to store the
    *         specified fixed term.
    *
    * @param index index to be set
    * @param term specifies the variable term to be set
-   * @param to_set pointer to the vector to be set
+   * @param to_set pointer to the data object to be set
    */
   virtual void SetVariableTermPtr(Index index,
-                                  VariableTerms term,
-                                  std::shared_ptr<MPIVector> to_set) = 0;
-  /*! \brief Sets the MPIVector (a pointer to the vector) used to store the
+                                  VariableTermType term,
+                                  std::shared_ptr<StorageType> to_set) = 0;
+  /*! \brief Sets the MPIVector (a pointer to the data object) used to store the
    *         specified fixed term.
    *
    * @param group group number to be set
    * @param term specifies the variable term to be set
-   * @param to_set pointer to the vector to be set
+   * @param to_set pointer to the data object to be set
    */
   virtual void SetVariableTermPtr(GroupNumber group,
-                                  VariableTerms term,
-                                  std::shared_ptr<MPIVector> to_set) = 0;
+                                  VariableTermType term,
+                                  std::shared_ptr<StorageType> to_set) = 0;
   /*! \brief Returns a pointer to the specified variable term.
    *
    * @param index index for the variable term
    * @param term variable term to return
    * @return pointer to the variable term.
    */
-  virtual std::shared_ptr<MPIVector> GetVariableTermPtr(Index index,
-                                                        VariableTerms term) = 0;
+  virtual std::shared_ptr<StorageType> GetVariableTermPtr(Index index,
+                                                          VariableTermType term) = 0;
   /*! \brief Returns a pointer to the specified variable term.
    *
    * @param group group to return
    * @param term variable term to return
    * @return pointer to the variable term.
    */
-  virtual std::shared_ptr<MPIVector> GetVariableTermPtr(GroupNumber group,
-                                                        VariableTerms term) = 0;
+  virtual std::shared_ptr<StorageType> GetVariableTermPtr(GroupNumber group,
+                                                          VariableTermType term) = 0;
 
 };
 
