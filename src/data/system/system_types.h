024ea41db0366ad4c3bf5ae255e572d0c4fdab70
Joshua Rehak <jsrehak@gmail.com>
2019-05-13 10:15:56 -0700

    added Variable structs to the data::system namespace, added TermI interface class

--- src/data/system/system_types.h
+++ src/data/system/system_types.h
@@ -14,6 +14,10 @@ namespace data {
 
 namespace system {
 
+// ===== TERMS =================================================================
+
+// Term indices
+
 //! Group number for rhs and lhs
 using GroupNumber = int;
 //! Angle index for rhs and lhs
@@ -21,10 +25,27 @@ using AngleIndex = int;
 //! Index used to store and access rhs vectors and lhs matrices
 using Index = std::pair<GroupNumber, AngleIndex>;
 
+// Term Data Types
+
 //! Sparse MPI matrix used for left-hand-side matrices
 using MPISparseMatrix = dealii::PETScWrappers::MPI::SparseMatrix;
 //! Sparse MPI vector used for ride-hand-side vectors
 using MPIVector = dealii::PETScWrappers::MPI::Vector;
+
+// Term VariableTerms
+
+//! Linear Terms that may vary iteration-to-iteration
+enum class VariableLinearTerms {
+  kScatteringSource = 0, //!< Scattering source
+  kFissionSource = 1,    //!< Fission source
+};
+
+//! Bilinear Terms that may vary iteration-to-iteration
+enum class VariableBilinearTerms {
+
+};
+
+// ===== HARMONICS =============================================================
 
 using HarmonicL = int;
 //!< Spherical harmonic \f$\ell\f$ value for moments
---
+++ src/data/system/term_i.h
@@ -0,0 +1,104 @@
+#ifndef BART_SRC_DATA_SYSTEM_TERM_I_H_
+#define BART_SRC_DATA_SYSTEM_TERM_I_H_
+
+#include <unordered_set>
+
+#include "data/system/system_types.h"
+
+namespace bart {
+
+namespace data {
+
+namespace system {
+
+/*! \brief Stores and provides vectors/matrices for linear/bilinear terms.
+ *
+ * Vector/matrices are stored differently if they are fixed (will not change iteration
+ * to iteration) or variable (do change iteration to iteration). A single one
+ * is stored for the fixed terms, as they can all be stamped once and left. Each
+ * variable term has its own vector, so that they can be individually updated
+ * when needed.
+ *
+ * Each group and angle requires its own, so storage based on
+ * an index, comprised of the group number, and the _index_ of the angle.
+ * Overloads are provided that only require group number, which will retrieve
+ * and store those with an angle index of zero.
+ */
+class TermI {
+ public:
+  virtual ~TermI() = default;
+
+  /*! \brief Returns the set of terms that are set as variable. */
+  virtual std::unordered_set<VariableTerms> GetVariableTerms() const = 0;
+
+  /*! \brief Sets the MPIVector (a pointer to the vector) used to store the fixed term.
+   *
+   * @param index corresponding index for the MPIVector
+   * @param to_set pointer to the vector to set
+   */
+  virtual void SetFixedTermPtr(Index index, std::shared_ptr<MPIVector> to_set) = 0;
+  /*! \brief Sets the MPIVector (a pointer to the vector) used to store the fixed term.
+   *
+   * @param group group number to be set
+   * @param to_set pointer to the vector to set
+   */
+  virtual void SetFixedTermPtr(GroupNumber group, std::shared_ptr<MPIVector> to_set) = 0;
+  /*! \brief Returns a pointer to the fixed term for a given index
+   *
+   * @param index index for the fixed term
+   * @return a pointer to the fixed term
+   */
+  virtual std::shared_ptr<MPIVector> GetFixedTermPtr(Index index) = 0;
+  /*! \brief Returns a pointer to the fixed term for a given group.
+   *
+   * @param group group to return
+   * @return a pointer to the fixed term
+   */
+  virtual std::shared_ptr<MPIVector> GetFixedTermPtr(GroupNumber group) = 0;
+
+  /*! \brief Sets the MPIVector (a pointer to the vector) used to store the
+   *         specified fixed term.
+   *
+   * @param index index to be set
+   * @param term specifies the variable term to be set
+   * @param to_set pointer to the vector to be set
+   */
+  virtual void SetVariableTermPtr(Index index,
+                                  VariableTerms term,
+                                  std::shared_ptr<MPIVector> to_set) = 0;
+  /*! \brief Sets the MPIVector (a pointer to the vector) used to store the
+   *         specified fixed term.
+   *
+   * @param group group number to be set
+   * @param term specifies the variable term to be set
+   * @param to_set pointer to the vector to be set
+   */
+  virtual void SetVariableTermPtr(GroupNumber group,
+                                  VariableTerms term,
+                                  std::shared_ptr<MPIVector> to_set) = 0;
+  /*! \brief Returns a pointer to the specified variable term.
+   *
+   * @param index index for the variable term
+   * @param term variable term to return
+   * @return pointer to the variable term.
+   */
+  virtual std::shared_ptr<MPIVector> GetVariableTermPtr(Index index,
+                                                        VariableTerms term) = 0;
+  /*! \brief Returns a pointer to the specified variable term.
+   *
+   * @param group group to return
+   * @param term variable term to return
+   * @return pointer to the variable term.
+   */
+  virtual std::shared_ptr<MPIVector> GetVariableTermPtr(GroupNumber group,
+                                                        VariableTerms term) = 0;
+
+};
+
+} // namespace system
+
+} // namespace data
+
+} // namespace bart
+
+#endif // BART_SRC_DATA_SYSTEM_TERM_I_H_
