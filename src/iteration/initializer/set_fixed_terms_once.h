5abdd8a10711de9c3710a54eb22df51ba112907f
Joshua Rehak <jsrehak@gmail.com>
2019-05-16 14:30:02 -0700

    initial commit of SetFixedTermsOnce and tests

---
+++ src/iteration/initializer/set_fixed_terms_once.h
@@ -0,0 +1,33 @@
+#ifndef BART_SRC_ITERATION_INITIALIZER_SET_FIXED_TERMS_ONCE_H_
+#define BART_SRC_ITERATION_INITIALIZER_SET_FIXED_TERMS_ONCE_H_
+
+#include "iteration/initializer/set_fixed_terms.h"
+
+namespace bart {
+
+namespace iteration {
+
+namespace initializer {
+
+/*! \brief Initializes a system by setting the fixed terms once.
+ *
+ * Identical to iteration::initializer::SetFixedTerms but after the first
+ * call to Initialize, subsequent calls will not do anything.
+ *
+ */
+class SetFixedTermsOnce : public SetFixedTerms {
+ public:
+  SetFixedTermsOnce(std::unique_ptr<updater::FixedUpdaterI> fixed_updater_ptr,
+                    const int total_groups,
+                    const int total_angles)
+      : SetFixedTerms(std::move(fixed_updater_ptr), total_groups, total_angles)
+      {}
+};
+
+} // namespace initializer
+
+} // namespace iteration
+
+} // namespace bart
+
+#endif // BART_SRC_ITERATION_INITIALIZER_SET_FIXED_TERMS_ONCE_H_
