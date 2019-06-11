a9a90f05835a3e81fa83bb5eafb021d5fca54e9c
Joshua Rehak <jsrehak@gmail.com>
2019-06-11 15:16:49 -0700

    added test to ensure correct solution indices were created in construction

--- src/system/solution/tests/mpi_angular_tests.cc
+++ src/system/solution/tests/mpi_angular_tests.cc
@@ -22,6 +22,12 @@ TEST_F(SolutionMPIAngularTests, Constructor) {
   EXPECT_EQ(test_solution.total_angles(), test_angles_);
   EXPECT_EQ(test_solution.total_groups(), test_groups_);
   EXPECT_EQ(test_solution.solutions().size(), test_groups_*test_angles_);
+
+  for (int group = 0; group < test_groups_; ++group) {
+    for (int angle = 0; angle < test_angles_; ++angle) {
+      EXPECT_NO_THROW(test_solution.solutions().at({group, angle}));
+    }
+  }
 }
 
 } // namespace
