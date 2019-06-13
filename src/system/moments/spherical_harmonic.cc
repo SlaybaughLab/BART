63cf9fc6dfe17301455dbace1da78b3deef46e4b
Joshua Rehak <jsrehak@gmail.com>
2019-06-13 14:41:36 -0700

    added assertions for bad groups and l_max values for constructor of moment::SphericalHarmonic

--- src/system/moments/spherical_harmonic.cc
+++ src/system/moments/spherical_harmonic.cc
@@ -10,6 +10,14 @@ SphericalHarmonic::SphericalHarmonic(const int total_groups,
                                      const int max_harmonic_l)
     : total_groups_(total_groups),
       max_harmonic_l_(max_harmonic_l) {
+
+  AssertThrow(total_groups > 0,
+              dealii::ExcMessage("Error system::moments::SphericalHarmonic "
+                                 "constructor, total_groups must be > 0"));
+  AssertThrow(max_harmonic_l >= 0 ,
+              dealii::ExcMessage("Error system::moments::SphericalHarmonic "
+                                 "constructor, l_max must be >= 0"));
+
   for (int group = 0; group < total_groups; ++group) {
     for (int l = 0; l <= max_harmonic_l; ++l) {
       for (int m = -l; m <= l; ++m) {
--- src/system/moments/tests/spherical_harmonic_test.cc
+++ src/system/moments/tests/spherical_harmonic_test.cc
@@ -1,5 +1,7 @@
 #include "system/moments/spherical_harmonic.h"
 
+#include <array>
+
 #include "test_helpers/gmock_wrapper.h"
 
 namespace  {
@@ -25,6 +27,19 @@ TEST_F(SystemMomentsSphericalHarmonicTest, Constructor) {
   EXPECT_EQ(test_moments.max_harmonic_l(), max_harmonic_l);
   int total_moments = total_groups * (max_harmonic_l + 1) * (max_harmonic_l + 1);
   EXPECT_EQ(test_moments.moments().size(), total_moments);
+}
+
+TEST_F(SystemMomentsSphericalHarmonicTest, BadGroupsAndHarmonics) {
+  std::array<int, 2> bad_groups{0, -1};
+  std::array<int, 1> bad_max_harmonic_l{-1};
+
+  for (const auto group : bad_groups) {
+    for (const auto max_harmonic_l : bad_max_harmonic_l) {
+      EXPECT_ANY_THROW({
+        system::moments::SphericalHarmonic bad_moment(group, max_harmonic_l);
+      });
+    }
+  }
 }
 
 
