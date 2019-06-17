cc506d03283268cdab990d9ab7ce0b771401562d
Joshua Rehak <jsrehak@gmail.com>
2019-06-17 10:33:06 -0700

    added spherical harmonic mock class

--- src/quadrature/calculators/tests/spherical_harmonic_zeroth_moment_test.cpp
+++ src/quadrature/calculators/tests/spherical_harmonic_zeroth_moment_test.cpp
@@ -1,5 +1,6 @@
 #include "quadrature/calculators/spherical_harmonic_zeroth_moment.h"
 
+#include "system/solution/tests/mpi_angular_mock.h"
 #include "test_helpers/gmock_wrapper.h"
 
 namespace {
@@ -8,7 +9,11 @@ using namespace bart;
 
 class QuadCalcSphericalHarmonicMomentsOnlyScalar : public ::testing::Test {
  protected:
+  // Tested object
   quadrature::calculators::SphericalHarmonicZerothMoment test_calculator;
+
+  // Supporting objects
+  system::solution::MPIAngularMock mock_solution;
 };
 
 TEST_F(QuadCalcSphericalHarmonicMomentsOnlyScalar, Dummy) {
---
+++ src/system/moments/tests/spherical_harmonic_mock.h
@@ -0,0 +1,38 @@
+#ifndef BART_SRC_SYSTEM_MOMENTS_TESTS_SPHERICAL_HARMONIC_MOCK_H_
+#define BART_SRC_SYSTEM_MOMENTS_TESTS_SPHERICAL_HARMONIC_MOCK_H_
+
+#include "system/moments/spherical_harmonic_i.h"
+#include "system/moments/spherical_harmonic_types.h"
+#include "test_helpers/gmock_wrapper.h"
+
+namespace bart {
+
+namespace system {
+
+namespace moments {
+
+class SphericalHarmonicMock : public SphericalHarmonicI {
+ public:
+  MOCK_CONST_METHOD0(total_groups, int());
+  MOCK_CONST_METHOD0(max_harmonic_l, int());
+  MOCK_CONST_METHOD0(moments, const MomentsMap&());
+  MOCK_CONST_METHOD1(BracketOp, const MomentVector&(const MomentIndex));
+  MOCK_METHOD1(BracketOp, MomentVector&(const MomentIndex));
+
+  const MomentVector& operator[](const MomentIndex index) const override {
+    return BracketOp(index);
+  };
+
+  MomentVector& operator[](const MomentIndex index) override {
+    return BracketOp(index);
+  };
+};
+
+} // namespace moments
+
+} // namespace system
+
+
+} // namespace bart
+
+#endif // BART_SRC_SYSTEM_MOMENTS_TESTS_SPHERICAL_HARMONIC_MOCK_H_
