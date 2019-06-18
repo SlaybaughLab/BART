340ef671e15660fb92d491074c55b131ec06b9a9
Joshua Rehak <jsrehak@gmail.com>
2019-06-18 07:25:12 -0700

    added initialization and setting of MPI Vectors for tests of SphericalHarmonic moment calculators

--- src/quadrature/calculators/tests/spherical_harmonic_zeroth_moment_test.cc
+++ src/quadrature/calculators/tests/spherical_harmonic_zeroth_moment_test.cc
@@ -2,6 +2,11 @@
 
 #include <memory>
 
+#include <deal.II/base/mpi.h>
+#include <deal.II/lac/petsc_parallel_vector.h>
+
+#include "system/system_types.h"
+#include "system/moments/spherical_harmonic_types.h"
 #include "quadrature/angular/tests/angular_quadrature_set_mock.h"
 #include "system/solution/tests/mpi_angular_mock.h"
 #include "test_helpers/gmock_wrapper.h"
@@ -11,6 +16,12 @@ namespace {
 using namespace bart;
 
 using ::testing::Ref;
+
+void SetVector(system::MPIVector& to_set, double value) {
+  auto [first_row, last_row] = to_set.local_range();
+  for (unsigned int i = first_row; i < last_row; ++i)
+    to_set[i] = value;
+}
 
 template <typename DimensionWrapper>
 class QuadCalcSphericalHarmonicMomentsOnlyScalar : public ::testing::Test {
@@ -25,12 +36,17 @@ class QuadCalcSphericalHarmonicMomentsOnlyScalar : public ::testing::Test {
 
   // Supporting objects
   system::solution::MPIAngularMock mock_solution_;
+  std::array<system::MPIVector, 3> mpi_vectors_;
 
   // Test object dependency
   std::shared_ptr<AngularQuadratureSetType> mock_angular_quad_;
 
   // Observing pointers
   AngularQuadratureSetType* angular_quad_obs_ptr_;
+
+  // Test parameters
+  const int n_processes = dealii::Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD);
+  const int n_entries_per_proc = 10;
 
   void SetUp() override;
 };
@@ -48,6 +64,16 @@ void QuadCalcSphericalHarmonicMomentsOnlyScalar<DimensionWrapper>::SetUp() {
   angular_quad_obs_ptr_ = dynamic_cast<AngularQuadratureSetType*>(
       test_calculator->angular_quadrature_set_ptr());
 
+
+  for (auto& mpi_vector : mpi_vectors_) {
+    mpi_vector.reinit(MPI_COMM_WORLD,
+                      n_processes * n_entries_per_proc,
+                      n_entries_per_proc);
+  };
+
+  SetVector(mpi_vectors_[0], 1);
+  SetVector(mpi_vectors_[1], 10);
+  SetVector(mpi_vectors_[2], 100);
 }
 
 TYPED_TEST_CASE(QuadCalcSphericalHarmonicMomentsOnlyScalar,
@@ -61,8 +87,14 @@ TYPED_TEST(QuadCalcSphericalHarmonicMomentsOnlyScalar, Constructor) {
   EXPECT_THAT(*mock_angular_quad_, Ref(*angular_quad_obs_ptr_));
 }
 
-TYPED_TEST(QuadCalcSphericalHarmonicMomentsOnlyScalar, CalculateMoment) {
 
+TYPED_TEST(QuadCalcSphericalHarmonicMomentsOnlyScalar, CalculateMomentsMPI) {
+  const int group = 0;
+  const int total_angles = 3;
+
+  for (int angle = 0; angle < total_angles; ++angle) {
+
+  }
 }
 
 
