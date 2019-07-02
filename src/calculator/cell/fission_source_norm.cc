b480c5ce73c26169176d790cd02a4f81db4da068
Joshua Rehak <jsrehak@gmail.com>
2019-07-02 11:27:28 -0700

    added implementation of FissionSourceNorm::GetNorm

--- src/calculator/cell/fission_source_norm.cc
+++ src/calculator/cell/fission_source_norm.cc
@@ -23,13 +23,28 @@ template<int dim>
 double FissionSourceNorm<dim>::GetCellNorm(
     domain::CellPtr<dim> cell_ptr,
     system::moments::SphericalHarmonicI* system_moments) const {
-
+  // TODO: change system_moments variable to system_moments_ptr
   const int material_id = cell_ptr->material_id();
   double fission_source = 0;
 
   if (cross_sections_ptr_->is_material_fissile.at(material_id)) {
     finite_element_ptr_->SetCell(cell_ptr);
-    fission_source = -1;
+
+    const int total_groups = system_moments->total_groups();
+    const auto nu_sigma_f = cross_sections_ptr_->nu_sigma_f.at(material_id);
+
+    for (int group = 0; group < total_groups; ++group) {
+
+      auto scalar_flux_at_cell_quadrature =
+          finite_element_ptr_->ValueAtQuadrature(
+              system_moments->GetMoment({group, 0, 0}));
+
+      for (int q = 0; q < cell_quadrature_points_; ++q) {
+        double scalar_flux = scalar_flux_at_cell_quadrature.at(q) *
+            finite_element_ptr_->Jacobian(q);
+        fission_source += nu_sigma_f.at(group) * scalar_flux;
+      }
+    }
   }
 
   return fission_source;
