e5470941a0df3420ff9d72153f0fdceeba1605a4
Joshua Rehak <jsrehak@gmail.com>
2019-07-08 10:34:50 -0700

    added instance of mock TotalAggregatedFissionSource to tests for UpdaterViaFissionSource

--- src/eigenvalue/k_effective/tests/updater_via_fission_source_test.cc
+++ src/eigenvalue/k_effective/tests/updater_via_fission_source_test.cc
@@ -1,14 +1,28 @@
+#include <calculator/cell/tests/integrated_fission_source_mock.h>
 #include "eigenvalue/k_effective/updater_via_fission_source.h"
 
+#include "calculator/cell/tests/total_aggregated_fission_source_mock.h"
 #include "test_helpers/gmock_wrapper.h"
 
 namespace  {
 
 using namespace bart;
 
- class EigKEffUpdaterViaFissionSourceTest : public ::testing::Test {
+class EigKEffUpdaterViaFissionSourceTest : public ::testing::Test {
  protected:
+
+  using TotalAggregatedFissionSourceType = calculator::cell::TotalAggregatedFissionSourceMock;
+
+  // Supporting objects
+  std::unique_ptr<TotalAggregatedFissionSourceType> fission_source_mock_ptr_;
+
+  void SetUp() override;
 };
+
+void EigKEffUpdaterViaFissionSourceTest::SetUp() {
+  fission_source_mock_ptr_ =
+      std::make_unique<TotalAggregatedFissionSourceType>();
+}
 
 TEST_F(EigKEffUpdaterViaFissionSourceTest, Constructor) {
   eigenvalue::k_effective::UpdaterViaFissionSource test_k_eff_updater;
