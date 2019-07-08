f5652dfba94f4c72d08abb1138ea9c19a60ddfe8
Joshua Rehak <jsrehak@gmail.com>
2019-07-08 11:25:58 -0700

    added tests for CalculateK_Effective

--- src/eigenvalue/k_effective/tests/updater_via_fission_source_test.cc
+++ src/eigenvalue/k_effective/tests/updater_via_fission_source_test.cc
@@ -2,19 +2,26 @@
 #include "eigenvalue/k_effective/updater_via_fission_source.h"
 
 #include "calculator/cell/tests/total_aggregated_fission_source_mock.h"
+#include "system/system.h"
+#include "system/moments/tests/spherical_harmonic_mock.h"
 #include "test_helpers/gmock_wrapper.h"
 
 namespace  {
 
 using namespace bart;
 
+using ::testing::Return;
+
 class EigKEffUpdaterViaFissionSourceTest : public ::testing::Test {
  protected:
 
   using TotalAggregatedFissionSourceType = calculator::cell::TotalAggregatedFissionSourceMock;
+  using MomentsType = system::moments::SphericalHarmonicMock;
 
   // Supporting objects
   std::unique_ptr<TotalAggregatedFissionSourceType> fission_source_mock_ptr_;
+  std::unique_ptr<MomentsType> current_moments_ptr_;
+  system::System test_system;
 
   void SetUp() override;
 };
@@ -22,6 +29,7 @@ class EigKEffUpdaterViaFissionSourceTest : public ::testing::Test {
 void EigKEffUpdaterViaFissionSourceTest::SetUp() {
   fission_source_mock_ptr_ =
       std::make_unique<TotalAggregatedFissionSourceType>();
+  test_system.current_moments = std::move(current_moments_ptr_);
 }
 
 TEST_F(EigKEffUpdaterViaFissionSourceTest, Constructor) {
@@ -35,8 +43,75 @@ TEST_F(EigKEffUpdaterViaFissionSourceTest, Constructor) {
   auto fission_source_obs_ptr_ =
       dynamic_cast<TotalAggregatedFissionSourceType*>(
           test_k_eff_updater.fission_source_calculator());
-  
+
   EXPECT_NE(fission_source_obs_ptr_, nullptr);
+}
+
+TEST_F(EigKEffUpdaterViaFissionSourceTest, BadFissionSources) {
+  eigenvalue::k_effective::UpdaterViaFissionSource
+      test_k_eff_updater(std::move(fission_source_mock_ptr_));
+  auto fission_source_obs_ptr_ =
+      dynamic_cast<TotalAggregatedFissionSourceType*>(
+          test_k_eff_updater.fission_source_calculator());
+
+  EXPECT_CALL(*fission_source_obs_ptr_,
+              AggregatedFissionSource(test_system.current_moments.get()))
+      .WillOnce(Return(-1));
+
+  EXPECT_ANY_THROW(test_k_eff_updater.CalculateK_Effective(test_system));
+
+  EXPECT_CALL(*fission_source_obs_ptr_,
+              AggregatedFissionSource(test_system.current_moments.get()))
+      .WillOnce(Return(0));
+
+  EXPECT_ANY_THROW(test_k_eff_updater.CalculateK_Effective(test_system));
+}
+
+TEST_F(EigKEffUpdaterViaFissionSourceTest, CalculateKEff) {
+  eigenvalue::k_effective::UpdaterViaFissionSource
+      test_k_eff_updater(std::move(fission_source_mock_ptr_));
+  auto fission_source_obs_ptr_ =
+      dynamic_cast<TotalAggregatedFissionSourceType*>(
+          test_k_eff_updater.fission_source_calculator());
+
+  const std::array<double, 3> fission_sources{5.3, 2.6, 1.3};
+
+  EXPECT_CALL(*fission_source_obs_ptr_,
+      AggregatedFissionSource(test_system.current_moments.get()))
+      .WillOnce(Return(fission_sources[0]));
+
+  double k_effective = test_k_eff_updater.CalculateK_Effective(test_system);
+
+  EXPECT_DOUBLE_EQ(k_effective, fission_sources[0]);
+  EXPECT_DOUBLE_EQ(test_k_eff_updater.current_fission_source(), fission_sources[0]);
+  EXPECT_DOUBLE_EQ(test_k_eff_updater.k_effective(), fission_sources[0]);
+
+  EXPECT_CALL(*fission_source_obs_ptr_,
+              AggregatedFissionSource(test_system.current_moments.get()))
+      .WillOnce(Return(fission_sources[1]));
+
+  k_effective = test_k_eff_updater.CalculateK_Effective(test_system);
+
+  double expected_result = 0.4905660377358;
+
+  EXPECT_NEAR(k_effective, expected_result, 1e-12);
+  EXPECT_DOUBLE_EQ(test_k_eff_updater.current_fission_source(), fission_sources[1]);
+  EXPECT_DOUBLE_EQ(test_k_eff_updater.previous_fission_source(), fission_sources[0]);
+  EXPECT_DOUBLE_EQ(test_k_eff_updater.k_effective(), k_effective);
+
+  EXPECT_CALL(*fission_source_obs_ptr_,
+              AggregatedFissionSource(test_system.current_moments.get()))
+      .WillOnce(Return(fission_sources[2]));
+
+  k_effective = test_k_eff_updater.CalculateK_Effective(test_system);
+
+  expected_result = 0.2452830188679;
+
+  EXPECT_NEAR(k_effective, expected_result, 1e-12);
+  EXPECT_DOUBLE_EQ(test_k_eff_updater.current_fission_source(), fission_sources[2]);
+  EXPECT_DOUBLE_EQ(test_k_eff_updater.previous_fission_source(), fission_sources[1]);
+  EXPECT_DOUBLE_EQ(test_k_eff_updater.k_effective(), k_effective);
+
 }
 
 } // namespace
