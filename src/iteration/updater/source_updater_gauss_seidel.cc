7a608035065ba8dfd04d96c757a5096ca8281453
Joshua Rehak <jsrehak@gmail.com>
2019-07-25 14:52:32 -0700

    cleaned up implementation of iteration::updater::SourceUpdaterGaussSeidel

--- src/iteration/updater/source_updater_gauss_seidel.cc
+++ src/iteration/updater/source_updater_gauss_seidel.cc
@@ -41,22 +41,19 @@ void SourceUpdaterGaussSeidel<formulation::CFEMStamperI>::UpdateFissionSource(
     system::GroupNumber group,
     system::AngleIndex angle) {
 
-    double k_effective;
+  AssertThrow(system.k_effective.has_value(),
+      dealii::ExcMessage("System has no k_effective value"));
 
-  if (system.k_effective.has_value()) {
-    k_effective = system.k_effective.value();
-  } else {
-    AssertThrow(false, dealii::ExcMessage("System has no k_effective value"));
-  }
+  const double k_effective = system.k_effective.value();
 
   AssertThrow(k_effective > 0, dealii::ExcMessage("Bad k_effective value"));
 
-  auto scattering_source_vector_ptr_ =
+  auto fission_source_vector_ptr_ =
       this->GetSourceVectorPtr(VariableTerms::kFissionSource,
                                system, group, angle);
 
 
-  *scattering_source_vector_ptr_ = 0;
+  *fission_source_vector_ptr_ = 0;
 
   const system::moments::MomentVector &in_group_moment =
       system.current_moments->GetMoment({group, 0, 0});
@@ -64,9 +61,9 @@ void SourceUpdaterGaussSeidel<formulation::CFEMStamperI>::UpdateFissionSource(
   const system::moments::MomentsMap& out_group_moments =
       system.current_moments->moments();
 
-  stamper_ptr_->StampFissionSource(*scattering_source_vector_ptr_,
+  stamper_ptr_->StampFissionSource(*fission_source_vector_ptr_,
                                    group,
-                                   system.k_effective.value(),
+                                   k_effective,
                                    in_group_moment,
                                    out_group_moments);
 }
