80d8ce6b80191fcfc61e93a0ebad4030671d56c1
Joshua Rehak <jsrehak@gmail.com>
2019-09-17 10:31:29 -0700

    added domain dependency to OutputDealiiVtu

--- src/results/output_dealii_vtu.h
+++ src/results/output_dealii_vtu.h
@@ -3,13 +3,25 @@
 
 #include "output_i.h"
 
+#include <memory>
+
+#include "domain/definition_i.h"
+
 namespace bart {
 
 namespace results {
 
+template <int dim>
 class OutputDealiiVtu : public OutputI {
  public:
+  OutputDealiiVtu(const std::shared_ptr<domain::DefinitionI<dim>> &domain_ptr)
+  : domain_ptr_(domain_ptr) {}
   void Output(system::System &to_output) const override {};
+
+  domain::DefinitionI<dim>* domain_ptr() const { return domain_ptr_.get(); };
+
+ private:
+  std::shared_ptr<domain::DefinitionI<dim>> domain_ptr_ = nullptr;
 };
 
 } // namespace result
--- src/results/tests/output_dealii_vtu_test.cc
+++ src/results/tests/output_dealii_vtu_test.cc
@@ -1,18 +1,41 @@
 #include "results/output_dealii_vtu.h"
 
+#include <memory>
+
+#include "domain/tests/definition_mock.h"
 #include "test_helpers/gmock_wrapper.h"
 
 namespace {
 
-template <typename DimensionWrapper>
-class OutputDealiiVtuTest : public ::testing::Test {
+using namespace bart;
 
+template <typename DimensionWrapper>
+class ResultsOutputDealiiVtuTest : public ::testing::Test {
+ public:
+  static constexpr int dim = DimensionWrapper::value;
+  std::shared_ptr<domain::DefinitionI<dim>> domain_ptr_;
+  std::unique_ptr<results::OutputDealiiVtu<dim>> test_output_;
+
+  void SetUp() override;
 };
 
-TYPED_TEST_CASE(OutputDealiiVtuTest, bart::testing::AllDimensions);
+template <typename DimensionWrapper>
+void ResultsOutputDealiiVtuTest<DimensionWrapper>::SetUp() {
+  domain_ptr_ = std::make_shared<domain::DefinitionMock<dim>>();
 
-TYPED_TEST(OutputDealiiVtuTest, Constructor) {
-  EXPECT_TRUE(true);
+  test_output_ = std::make_unique<results::OutputDealiiVtu<dim>>(domain_ptr_);
+}
+
+TYPED_TEST_CASE(ResultsOutputDealiiVtuTest, bart::testing::AllDimensions);
+
+TYPED_TEST(ResultsOutputDealiiVtuTest, Constructor) {
+  constexpr int dim = this->dim;
+  EXPECT_EQ(this->domain_ptr_.use_count(), 2);
+
+  auto domain_ptr = this->test_output_->domain_ptr();
+
+  ASSERT_NE(domain_ptr, nullptr);
+  EXPECT_NE(nullptr, dynamic_cast<domain::DefinitionMock<dim>*>(domain_ptr));
 }
 
 }
