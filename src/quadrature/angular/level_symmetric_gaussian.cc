be914a34c93c4096c0ff3a304759a18206077276
Joshua Rehak <jsrehak@gmail.com>
2019-10-03 15:24:07 -0700

    added order checking to constructor of LevelSymmetricGaussian

--- src/quadrature/angular/level_symmetric_gaussian.cc
+++ src/quadrature/angular/level_symmetric_gaussian.cc
@@ -1,5 +1,7 @@
 #include "quadrature/angular/level_symmetric_gaussian.h"
 
+#include <deal.II/base/exceptions.h>
+
 namespace bart {
 
 namespace quadrature {
@@ -7,7 +9,17 @@ namespace quadrature {
 namespace angular {
 
 LevelSymmetricGaussian::LevelSymmetricGaussian(bart::quadrature::Order order)
-    : order_(order.get()) {}
+    : order_(order.get()) {
+  AssertThrow(order_ >= 2,
+              dealii::ExcMessage("Error in constructor of LevelSymmetricGaussian "
+                                 "order must be >= 2"));
+  AssertThrow(order_ % 2 == 0,
+              dealii::ExcMessage("Error in constructor of LevelSymmetricGaussian "
+                                 "order must be even"));
+  AssertThrow(order_ <= 16,
+              dealii::ExcMessage("Error in constructor of LevelSymmetricGaussian "
+                                 "order must be <= 16"));
+}
 
 std::vector<std::pair<CartesianPosition<3>, Weight>> LevelSymmetricGaussian::GenerateSet() const {
   return std::vector<std::pair<CartesianPosition<3>, Weight>>();
--- src/quadrature/angular/tests/level_symmetric_gaussian_test.cc
+++ src/quadrature/angular/tests/level_symmetric_gaussian_test.cc
@@ -8,10 +8,19 @@ using namespace bart;
 
 class QuadratureAngularLevelSymmetricGaussianTest : public ::testing::Test {};
 
-TEST_F(QuadratureAngularLevelSymmetricGaussianTest, Dummy) {
-  int order = 4;
-  quadrature::angular::LevelSymmetricGaussian test_quadrature{quadrature::Order(order)};
-  EXPECT_EQ(test_quadrature.order(), order);
+TEST_F(QuadratureAngularLevelSymmetricGaussianTest, Constructor) {
+  quadrature::angular::LevelSymmetricGaussian test_quadrature{quadrature::Order(4)};
+  EXPECT_EQ(test_quadrature.order(), 4);
+}
+
+TEST_F(QuadratureAngularLevelSymmetricGaussianTest, BadOrders) {
+  std::array bad_orders{0, 1, 3, 5, 18, -1, -3, -18};
+
+  for (auto bad_order : bad_orders) {
+    EXPECT_ANY_THROW({
+      quadrature::angular::LevelSymmetricGaussian test_quad{quadrature::Order(bad_order)};
+    });
+  }
 }
 
 } // namespace
