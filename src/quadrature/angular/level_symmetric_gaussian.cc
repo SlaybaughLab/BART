5e0f5da2dcd09d84413a4e651ae07dff82bf08f5
Joshua Rehak <jsrehak@gmail.com>
2019-10-04 13:08:06 -0700

    added test for integration properties of quadrature, added implementation of quadrature

--- src/quadrature/angular/level_symmetric_gaussian.cc
+++ src/quadrature/angular/level_symmetric_gaussian.cc
@@ -1,6 +1,7 @@
 #include "quadrature/angular/level_symmetric_gaussian.h"
 
 #include <deal.II/base/exceptions.h>
+#include <deal.II/base/quadrature_lib.h>
 
 namespace bart {
 
@@ -22,7 +23,39 @@ LevelSymmetricGaussian::LevelSymmetricGaussian(bart::quadrature::Order order)
 }
 
 std::vector<std::pair<CartesianPosition<3>, Weight>> LevelSymmetricGaussian::GenerateSet() const {
-  return std::vector<std::pair<CartesianPosition<3>, Weight>>();
+  std::vector<std::pair<CartesianPosition<3>, Weight>> generated_set;
+
+  const int n_points = order_/2;
+
+  // Gaussian quadrature for theta, the lowest level has order_ points but we
+  // will only use half of the quadrature
+  dealii::QGauss<1> gaussian_quadrature(order_);
+
+  // For each quadrature point in the lowest level, we will create equally spaced
+  // points in phi
+  for (int level = 0; level < n_points; ++level) {
+    int n_points_this_level = level + 1;
+    // mu is just the gaussian quadrature point
+    double mu = 1 - gaussian_quadrature.point(level)[0] * 2;
+
+    // points in phi will be equally spaced from 0 to pi/2
+    double dphi = M_PI/(2*n_points_this_level);
+    // weights on this level are equal to the gaussian times 2PI/total points in
+    // 2PI. Here we have n_points_this_level*4 total points
+    double weight = gaussian_quadrature.weight(level) * M_PI/(n_points_this_level);
+
+    for (int j = 0; j < n_points_this_level; ++j) {
+      double phi = (j + 0.5)*dphi;
+      double x = std::sqrt(1 - mu*mu) * std::cos(phi);
+      double y = std::sqrt(1 - mu*mu) * std::sin(phi);
+      double z = mu;
+      std::array<double, 3> position{x,y,z};
+      generated_set.emplace_back(std::make_pair(CartesianPosition<3>(position),
+                                                Weight(weight)));
+    }
+  }
+
+  return generated_set;
 }
 
 } // namespace angular
--- src/quadrature/angular/tests/level_symmetric_gaussian_test.cc
+++ src/quadrature/angular/tests/level_symmetric_gaussian_test.cc
@@ -1,12 +1,29 @@
 #include "quadrature/angular/level_symmetric_gaussian.h"
 
+#include <cmath>
+
 #include "test_helpers/gmock_wrapper.h"
 
 namespace  {
 
 using namespace bart;
 
-class QuadratureAngularLevelSymmetricGaussianTest : public ::testing::Test {};
+class QuadratureAngularLevelSymmetricGaussianTest : public ::testing::Test {
+ public:
+
+  double sh_y_0_1(quadrature::CartesianPosition<3> position) {
+    double z = position.get().at(2);
+    return 0.5*std::sqrt(3.0/M_PI)*z/r(position);
+  }
+
+  double r(quadrature::CartesianPosition<3> position) {
+    auto &[x, y, z] = position.get();
+    return std::sqrt(std::pow(x, 2) + std::pow(y, 2) + std::pow(z, 2));
+  }
+
+};
+
+
 
 TEST_F(QuadratureAngularLevelSymmetricGaussianTest, Constructor) {
   quadrature::angular::LevelSymmetricGaussian test_quadrature{quadrature::Order(4)};
@@ -21,6 +38,31 @@ TEST_F(QuadratureAngularLevelSymmetricGaussianTest, BadOrders) {
       quadrature::angular::LevelSymmetricGaussian test_quad{quadrature::Order(bad_order)};
     });
   }
+}
+
+TEST_F(QuadratureAngularLevelSymmetricGaussianTest, Integration) {
+  std::array orders{2, 4, 6, 8, 10, 12, 14, 16};
+  for (const auto order : orders) {
+    quadrature::angular::LevelSymmetricGaussian test_quad{quadrature::Order(order)};
+
+    const auto quadrature_set = test_quad.GenerateSet();
+    EXPECT_EQ(quadrature_set.size(), order*(order + 2)/8);
+
+    double sum = 0;
+    for (const auto& quadrature_point : quadrature_set) {
+      auto& [position, weight] = quadrature_point;
+      sum += weight.get()*sh_y_0_1(position)*sh_y_0_1(position);
+    }
+    EXPECT_NEAR(1.0, 8*sum, 1e-6);
+
+//    sum = 0;
+//    for (const auto& quadrature_point : quadrature_set) {
+//      auto& [position, weight] = quadrature_point;
+//      sum += weight.get()*y_1_1_sq(position);
+//    }
+    //EXPECT_NEAR(1.0, 8*sum, 1e-6);
+  }
+
 }
 
 } // namespace
