125001b93279dba8311418ea67e54dce991dc732
Joshua Rehak <jsrehak@gmail.com>
2019-10-11 13:25:00 -0700

    refactored tests to move mock construction to SetUp function

--- src/quadrature/tests/quadrature_set_test.cc
+++ src/quadrature/tests/quadrature_set_test.cc
@@ -13,7 +13,19 @@ template <typename DimensionWrapper>
 class QuadratureSetTest : public ::testing::Test {
  public:
   static constexpr int dim = DimensionWrapper::value;
+
   quadrature::QuadratureSet<dim> test_set_;
+  std::shared_ptr<quadrature::QuadraturePointI<dim>> quadrature_point_,
+      second_quadrature_point_;
+
+  void SetUp() override {
+    quadrature_point_ =
+        std::make_shared<quadrature::QuadraturePointMock<dim>>();
+    second_quadrature_point_ =
+        std::make_shared<quadrature::QuadraturePointMock<dim>>();
+    test_set_.AddPoint(quadrature_point_);
+    test_set_.AddPoint(second_quadrature_point_);
+  }
 };
 
 TYPED_TEST_CASE(QuadratureSetTest, bart::testing::AllDimensions);
@@ -25,21 +37,20 @@ TYPED_TEST(QuadratureSetTest, Constructor) {
 
 TYPED_TEST(QuadratureSetTest, AddPoint) {
   constexpr int dim = this->dim;
-  auto quadrature_point =
-      std::make_shared<quadrature::QuadraturePointMock<dim>>();
-  auto second_quadrature_point =
-      std::make_shared<quadrature::QuadraturePointMock<dim>>();
+  quadrature::QuadratureSet<dim> test_set;
 
-  EXPECT_EQ(this->test_set_.size(), 0);
+  EXPECT_EQ(test_set.size(), 0);
 
-  EXPECT_TRUE(this->test_set_.AddPoint(quadrature_point));
-  EXPECT_EQ(this->test_set_.size(), 1);
+  EXPECT_TRUE(test_set.AddPoint(this->quadrature_point_));
+  EXPECT_EQ(test_set.size(), 1);
 
-  EXPECT_TRUE(this->test_set_.AddPoint(second_quadrature_point));
-  EXPECT_EQ(this->test_set_.size(), 2);
+  EXPECT_TRUE(test_set.AddPoint(this->second_quadrature_point_));
+  EXPECT_EQ(test_set.size(), 2);
 
-  EXPECT_FALSE(this->test_set_.AddPoint(quadrature_point));
-  EXPECT_EQ(this->test_set_.size(), 2);
+  EXPECT_FALSE(test_set.AddPoint(this->quadrature_point_));
+  EXPECT_EQ(test_set.size(), 2);
 }
+
+
 
 } // namespace
