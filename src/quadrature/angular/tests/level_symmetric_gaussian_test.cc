b4d1e14de343a9d61b87e0e6d0d193430d6e609d
Joshua Rehak <jsrehak@gmail.com>
2019-11-18 13:54:05 -0800

    Added documentation and minor refactors to tests for LevelSymmetricGaussian

--- src/quadrature/angular/tests/level_symmetric_gaussian_test.cc
+++ src/quadrature/angular/tests/level_symmetric_gaussian_test.cc
@@ -10,26 +10,27 @@ using namespace bart;
 
 class QuadratureAngularLevelSymmetricGaussianTest : public ::testing::Test {
  public:
-
-  double sh_y_0_1(quadrature::CartesianPosition<3> position) {
-    double z = position.get().at(2);
-    return 0.5*std::sqrt(3.0/M_PI)*z/r(position);
+  // Squared test function to verify integration properties, L = 1, m = 1
+  double squared_integrated_function(quadrature::CartesianPosition<3> position) {
+    const double x = position.get().at(0);
+    const double y = position.get().at(1);
+    const double radius = r(position);
+    return 3.0/8.0/M_PI*(x*x + y*y)/(radius*radius);
   }
-
+  // Radius calculation
   double r(quadrature::CartesianPosition<3> position) {
     auto &[x, y, z] = position.get();
     return std::sqrt(std::pow(x, 2) + std::pow(y, 2) + std::pow(z, 2));
   }
-
 };
 
-
-
+// Constructor should set order value properly.
 TEST_F(QuadratureAngularLevelSymmetricGaussianTest, Constructor) {
   quadrature::angular::LevelSymmetricGaussian test_quadrature{quadrature::Order(4)};
   EXPECT_EQ(test_quadrature.order(), 4);
 }
 
+// Bad values of order (negative, odd, and too large) should throw an error.
 TEST_F(QuadratureAngularLevelSymmetricGaussianTest, BadOrders) {
   std::array bad_orders{0, 1, 3, 5, 18, -1, -3, -18};
 
@@ -40,29 +41,21 @@ TEST_F(QuadratureAngularLevelSymmetricGaussianTest, BadOrders) {
   }
 }
 
+// All orders should integrate a spherical harmonic exactly.
 TEST_F(QuadratureAngularLevelSymmetricGaussianTest, Integration) {
   std::array orders{2, 4, 6, 8, 10, 12, 14, 16};
   for (const auto order : orders) {
     quadrature::angular::LevelSymmetricGaussian test_quad{quadrature::Order(order)};
-
     const auto quadrature_set = test_quad.GenerateSet();
     EXPECT_EQ(quadrature_set.size(), order*(order + 2)/8);
 
     double sum = 0;
     for (const auto& quadrature_point : quadrature_set) {
       auto& [position, weight] = quadrature_point;
-      sum += weight.get()*sh_y_0_1(position)*sh_y_0_1(position);
+      sum += weight.get()*squared_integrated_function(position);
     }
     EXPECT_NEAR(1.0, 8*sum, 1e-6);
-
-//    sum = 0;
-//    for (const auto& quadrature_point : quadrature_set) {
-//      auto& [position, weight] = quadrature_point;
-//      sum += weight.get()*y_1_1_sq(position);
-//    }
-    //EXPECT_NEAR(1.0, 8*sum, 1e-6);
   }
-
 }
 
 } // namespace
