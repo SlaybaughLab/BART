5a006776b2d40f86c372675293bd86f40685e933
Joshua Rehak <jsrehak@gmail.com>
2019-11-19 11:09:33 -0800

    added documentation to QuadratureGeneratorI

--- src/quadrature/quadrature_generator_i.h
+++ src/quadrature/quadrature_generator_i.h
@@ -9,6 +9,7 @@
 namespace bart {
 
 namespace quadrature {
+
 /*! \brief Generates a quadrature set, generally for one quadrant.
  *
  * Quadrature points are generated using the quadrature types that make them
@@ -20,9 +21,18 @@ template <int dim>
 class QuadratureGeneratorI {
  public:
   virtual ~QuadratureGeneratorI() = default;
-  virtual std::vector<std::pair<CartesianPosition<dim>, Weight>> GenerateSet() const = 0;
+  /*! \brief Generates a quadrature set.
+   * Sets are generated as a vector containing pairs of cartesian positions and
+   * weights. This is appropriate for the input to the
+   * quadrature::utility::FillQuadratureSet function.
+   */
+  virtual std::vector<std::pair<CartesianPosition<dim>, Weight>>
+  GenerateSet() const = 0;
+  /*! \brief Returns the order of the quadrature generated.
+   * The meaning of the order may vary by the specific type of quadrature.
+   * @return int indicating the order.
+   */
   virtual int order() const = 0;
-
 };
 
 } // namespace quadrature
