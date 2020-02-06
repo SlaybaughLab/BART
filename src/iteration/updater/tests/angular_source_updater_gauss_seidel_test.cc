fcf80f4aa9eceb1f7570f7545824fc8971f462c3
Joshua Rehak <jsrehak@gmail.com>
2020-02-06 15:30:27 -0800

    updated tests for AngularSourceUpdaterGaussSeidel UpdateScattering

--- src/iteration/updater/tests/angular_source_updater_gauss_seidel_test.cc
+++ src/iteration/updater/tests/angular_source_updater_gauss_seidel_test.cc
@@ -6,13 +6,19 @@
 
 #include "test_helpers/gmock_wrapper.h"
 #include "quadrature/tests/quadrature_set_mock.h"
+#include "quadrature/tests/quadrature_point_mock.h"
 #include "formulation/tests/angular_stamper_mock.h"
 #include "system/terms/tests/linear_term_mock.h"
+#include "system/moments/tests/spherical_harmonic_mock.h"
+#include "test_helpers/test_helper_functions.h"
+#include "test_helpers/test_assertions.h"
 
 namespace  {
 
 using namespace bart;
-using ::testing::A, ::testing::Return, ::testing::_;
+using ::testing::A, ::testing::An, ::testing::Return, ::testing::_;
+using ::testing::NiceMock, ::testing::DoDefault, ::testing::WithArg;
+using ::testing::Invoke, ::testing::Ref;
 
 template <typename DimensionWrapper>
 class IterationUpdaterAngularSourceUpdaterGaussSeidelTest :
@@ -21,8 +27,10 @@ class IterationUpdaterAngularSourceUpdaterGaussSeidelTest :
   static constexpr int dim = DimensionWrapper::value;
   using UpdaterType = iteration::updater::AngularSourceUpdaterGaussSeidel<formulation::AngularStamperI<dim>>;
   using StamperType = formulation::AngularStamperMock<dim>;
-  using QuadratureSetType = quadrature::QuadratureSetMock<dim>;
-  using RightHandSideType = bart::system::terms::LinearTermMock;
+  using QuadratureSetType = NiceMock<quadrature::QuadratureSetMock<dim>>;
+  using QuadraturePointType = quadrature::QuadraturePointI<dim>;
+  using RightHandSideType = NiceMock<bart::system::terms::LinearTermMock>;
+  using MomentsType = bart::system::moments::SphericalHarmonicMock;
 
   // Tested object
   std::unique_ptr<UpdaterType> test_updater_;
@@ -36,6 +44,18 @@ class IterationUpdaterAngularSourceUpdaterGaussSeidelTest :
   std::shared_ptr<system::MPIVector> source_vector_ptr_;
   bart::system::MPIVector expected_vector_;
   RightHandSideType* right_hand_side_obs_ptr_;
+  MomentsType* current_moments_obs_ptr_;
+  std::shared_ptr<QuadraturePointType> quadrature_point_ptr_;
+  bart::system::moments::MomentsMap current_iteration_moments_,
+      previous_iteration_moments_;
+
+  // Test parameters
+  const int total_groups = btest::RandomDouble(2, 5);
+  const int total_angles = btest::RandomDouble(2, 5);
+  const int l_max = 2;
+  const int group = btest::RandomDouble(0, total_groups + 1); // group number
+  const int angle_number = btest::RandomDouble(0, total_angles + 1);
+  const bart::system::Index solution_index{group, angle_number};
 
   void SetUp() override;
   void SetUpTestObject();
@@ -46,6 +66,7 @@ template <typename DimensionWrapper>
 void IterationUpdaterAngularSourceUpdaterGaussSeidelTest<DimensionWrapper>::SetUp() {
   SetUpTestObject();
   SetUpSystem();
+  quadrature_point_ptr_ = std::make_shared<quadrature::QuadraturePointMock<dim>>();
 }
 
 template <typename DimensionWrapper>
@@ -58,6 +79,7 @@ void IterationUpdaterAngularSourceUpdaterGaussSeidelTest<DimensionWrapper>::SetU
 
 template <typename DimensionWrapper>
 void IterationUpdaterAngularSourceUpdaterGaussSeidelTest<DimensionWrapper>::SetUpSystem() {
+  // RHS MPI Vectors
   auto mock_right_hand_side_ptr = std::make_unique<RightHandSideType>();
   right_hand_side_obs_ptr_ = mock_right_hand_side_ptr.get();
 
@@ -70,6 +92,32 @@ void IterationUpdaterAngularSourceUpdaterGaussSeidelTest<DimensionWrapper>::SetU
       .WillByDefault(Return(source_vector_ptr_));
 
   test_system_.right_hand_side_ptr_ = std::move(mock_right_hand_side_ptr);
+
+  // Moments
+  auto current_moments_ptr_ = std::make_unique<system::moments::SphericalHarmonicMock>();
+  current_moments_obs_ptr_ = current_moments_ptr_.get();
+
+  for (system::GroupNumber group = 0; group < total_groups; ++group) {
+    for (system::moments::HarmonicL l = 0; l < l_max; ++l) {
+      for (system::moments::HarmonicM m = -l_max; m <= l_max; ++m) {
+        system::moments::MomentVector current_moment, previous_moment;
+        current_iteration_moments_[{group, l, m}] = current_moment;
+        previous_iteration_moments_[{group, l, m}] = previous_moment;
+      }
+    }
+  }
+
+  test_system_.current_moments = std::move(current_moments_ptr_);
+}
+
+// ===== HELPER FUNCTIONS ======================================================
+
+// Fills an MPI vector with value
+void StampMPIVector(bart::system::MPIVector &to_fill, double value = 2) {
+  auto [local_begin, local_end] = to_fill.local_range();
+  for (unsigned int i = local_begin; i < local_end; ++i)
+    to_fill(i) += value;
+  to_fill.compress(dealii::VectorOperation::add);
 }
 
 
@@ -97,6 +145,109 @@ TYPED_TEST(IterationUpdaterAngularSourceUpdaterGaussSeidelTest, Getters) {
   EXPECT_EQ(stamper_ptr, this->stamper_ptr_.get());
 }
 
+/*
+ * ======== UpdateScatteringSource Tests =======================================
+ */
+
+TYPED_TEST(IterationUpdaterAngularSourceUpdaterGaussSeidelTest,
+    UpdateScatteringSourceBadRHS) {
+  using term = bart::system::terms::VariableLinearTerms;
+  EXPECT_CALL(*this->right_hand_side_obs_ptr_,
+              GetVariableTermPtr(this->solution_index, term::kScatteringSource))
+      .WillOnce(Return(nullptr));
+  EXPECT_ANY_THROW({
+    this->test_updater_->UpdateScatteringSource(this->test_system_,
+                                                this->group,
+                                                this->angle_number);
+  });
+}
+
+TYPED_TEST(IterationUpdaterAngularSourceUpdaterGaussSeidelTest,
+    UpdateScatteringSourceBadIndex) {
+  int bad_group = btest::RandomDouble(this->total_groups,
+                                      this->total_groups + 4);
+  int bad_angle = btest::RandomDouble(this->total_angles,
+                                      this->total_angles + 4);
+  EXPECT_ANY_THROW({
+    this->test_updater_->UpdateScatteringSource(this->test_system_,
+                                                bad_group,
+                                                this->angle_number);
+  });
+
+  EXPECT_ANY_THROW({
+    this->test_updater_->UpdateScatteringSource(this->test_system_,
+                                                this->group,
+                                                bad_angle);
+  });
+}
+
+TYPED_TEST(IterationUpdaterAngularSourceUpdaterGaussSeidelTest,
+    UpdateScatteringSource) {
+  using term = bart::system::terms::VariableLinearTerms;
+  StampMPIVector(*this->source_vector_ptr_, 3); // Fill with a random value, should be zero'd
+  double expected_value = btest::RandomDouble(1, 20);
+  StampMPIVector(this->expected_vector_, expected_value);
+
+  auto stamper_function = [&](bart::system::MPIVector &to_stamp) {
+    StampMPIVector(to_stamp, expected_value);
+  };
+
+  EXPECT_CALL(*this->right_hand_side_obs_ptr_,
+              GetVariableTermPtr(this->solution_index, term::kScatteringSource))
+      .WillOnce(DoDefault());
+  EXPECT_CALL(*this->quadrature_set_ptr_,
+              GetQuadraturePoint(quadrature::QuadraturePointIndex(this->angle_number)))
+      .WillOnce(Return(this->quadrature_point_ptr_));
+  EXPECT_CALL(*this->stamper_ptr_,
+              StampScatteringSourceTerm(Ref(*this->source_vector_ptr_),
+                  this->quadrature_point_ptr_,
+                  system::EnergyGroup(this->group),
+                  Ref(this->current_iteration_moments_[{this->group, 0, 0}]),
+                  Ref(this->current_iteration_moments_)))
+      .WillOnce(WithArg<0>(Invoke(stamper_function)));
+
+  this->test_updater_->UpdateScatteringSource(this->test_system_,
+                                              this->group,
+                                              this->angle_number);
+  EXPECT_TRUE(bart::testing::CompareMPIVectors(this->expected_vector_,
+                                               *this->source_vector_ptr_));
+}
+
+/*
+ * ======== UpdateFissionSource Tests ==========================================
+ */
+
+TYPED_TEST(IterationUpdaterAngularSourceUpdaterGaussSeidelTest,
+           UpdateFissionSourceBadRHS) {
+  using term = bart::system::terms::VariableLinearTerms;
+  EXPECT_CALL(*this->right_hand_side_obs_ptr_,
+              GetVariableTermPtr(this->solution_index, term::kScatteringSource))
+      .WillOnce(Return(nullptr));
+  EXPECT_ANY_THROW({
+    this->test_updater_->UpdateFissionSource(this->test_system_,
+                                             this->group,
+                                             this->angle_number);
+                   });
+}
+
+TYPED_TEST(IterationUpdaterAngularSourceUpdaterGaussSeidelTest,
+           UpdateFissionSourceBadIndex) {
+  int bad_group = btest::RandomDouble(this->total_groups,
+                                      this->total_groups + 4);
+  int bad_angle = btest::RandomDouble(this->total_angles,
+                                      this->total_angles + 4);
+  EXPECT_ANY_THROW({
+    this->test_updater_->UpdateFissionSource(this->test_system_,
+                                             bad_group,
+                                             this->angle_number);
+  });
+
+  EXPECT_ANY_THROW({
+    this->test_updater_->UpdateFissionSource(this->test_system_,
+                                             this->group,
+                                             bad_angle);
+  });
+}
 
 
 } // namespace
