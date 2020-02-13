05611097389b968d0020c256153d9a7e40e1c67e
Joshua Rehak <jsrehak@gmail.com>
2020-02-13 14:30:04 -0800

    moved documentation from Defintion to DefinitionI

--- src/domain/definition_i.h
+++ src/domain/definition_i.h
@@ -15,6 +15,21 @@ namespace bart {
 
 namespace domain {
 
+/*! \brief Defines a domain that couples a cartesian mesh with a finite element basis.
+ *
+ * This class provides the framework on which the domain can be solved. The
+ * purpose of the coupling is to distribute the finite element basis functions
+ * and needed degrees of freedom throughout the provided mesh. Most of this is
+ * done under the hood by a dealii object called the DofHandler. This class
+ * is a mediator for all the required interactions between Dealii objects. It
+ * maintains ownership of the mesh and finite element basis through unique pointers.
+ *
+ * Many portions of this class are adapted from the BARTDriver class written by
+ * Weixiong Zheng.
+ *
+ * \author Joshua Rehak
+ * \date 2019/02
+ */
 template <int dim>
 class DefinitionI {
  public:
@@ -24,21 +39,38 @@ class DefinitionI {
 
   virtual ~DefinitionI() = default;
 
-  virtual DefinitionI<dim>& SetUpMesh() = 0;
-
+  /*! Set up the DOF handler, to access sparsity patterns, etc */
   virtual DefinitionI<dim>& SetUpDOF() = 0;
 
+/*! Fills triangulation with mesh defined in MeshI object
+   * Creates mesh shape, sets up boundary ids and material ids. Requires that
+   * the mesh has a material mapping setup.
+   */
+  virtual DefinitionI<dim>& SetUpMesh() = 0;
+
+  /*! Get the parameters required to build a system matrix for this domain */
   virtual void FillMatrixParameters(
       data::MatrixParameters &to_fill,
       problem::DiscretizationType discretization) const = 0;
 
+  /*! Get a matrix suitible for a cell matrix.
+   *
+   * \return a dealii FullMatrix<double> of appropriate size.
+   */
   virtual dealii::FullMatrix<double> GetCellMatrix() const = 0;
+  /*! Get a matrix suitible for a cell rhs.
+   *
+   * \return a dealii Vector<double> of appropriate size.
+   */
   virtual dealii::Vector<double> GetCellVector() const = 0;
 
+  /*! Get a range of all cells to allow iterating over them */
   virtual CellRange Cells() const = 0;
 
+  /*! Get total degrees of freedom */
   virtual const dealii::DoFHandler<dim>& dof_handler() const = 0;
 
+  /*! Get locally owned degrees of freedom */
   virtual int total_degrees_of_freedom() const = 0;
 };
 
