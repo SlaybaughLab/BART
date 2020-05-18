554af7a036bb6ccbdf3e8338f8e3de4a276b785c
Joshua Rehak <jsrehak@gmail.com>
2020-05-18 13:45:12 -0700

    removed range-based for with initializer not implemented until c++20

--- src/formulation/updater/saaf_updater.cc
+++ src/formulation/updater/saaf_updater.cc
@@ -36,8 +36,8 @@ SAAFUpdater<dim>::SAAFUpdater(
                   quadrature_set_ptr) {
   reflective_boundaries_ = reflective_boundaries;
   angular_solution_ptr_map_ = angular_solution_ptr_map;
-  for (const int total_angles = quadrature_set_ptr_->size();
-       auto& [energy_group, solution_ptr] : angular_solution_ptr_map_) {
+  const int total_angles = quadrature_set_ptr_->size();
+  for (auto& [energy_group, solution_ptr] : angular_solution_ptr_map_) {
     AssertThrow(total_angles == solution_ptr->total_angles(),
                 dealii::ExcMessage("Error in construction of SAAF Updater, "
                                    "total angles in quadrature set does not "
