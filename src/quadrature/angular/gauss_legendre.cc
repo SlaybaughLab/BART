8a4bb3ce6c958c6d99f80d45b1f987dd97b7815c
Joshua Rehak <jsrehak@gmail.com>
2020-06-23 14:04:55 -0700

    added implementation and documentation to GaussLegendre

--- src/quadrature/angular/gauss_legendre.cc
+++ src/quadrature/angular/gauss_legendre.cc
@@ -1,13 +1,36 @@
 #include "quadrature/angular/gauss_legendre.h"
 
+#include <deal.II/base/quadrature_lib.h>
+
 namespace bart {
 
 namespace quadrature {
 
 namespace angular {
 
-std::vector<GaussLegendre::PositionWeightPairType> GaussLegendre::GenerateSet() const {
-  return std::vector<PositionWeightPairType>();
+GaussLegendre::GaussLegendre(const int n_points)
+    : n_points_(n_points) {
+  AssertThrow(n_points > 0,
+      dealii::ExcMessage("Error in constructor of GaussLegendre, n_points must "
+                         "be greater than or equal to 0"))
+}
+
+
+std::vector<GaussLegendre::PositionWeightPairType>
+    GaussLegendre::GenerateSet() const {
+  dealii::QGauss<1> gaussian_quadrature(n_points_);
+  std::vector<PositionWeightPairType> return_vector;
+
+  auto points = gaussian_quadrature.get_points();
+  auto weights = gaussian_quadrature.get_weights();
+
+  for (int i = 0; i < static_cast<int>(gaussian_quadrature.size()); ++i) {
+    CartesianPosition<1> x_position({points.at(i)[0]});
+    Weight weight(weights.at(i));
+    return_vector.push_back({x_position, weight});
+  }
+
+  return return_vector;
 }
 
 } // namespace angular
--- src/quadrature/angular/gauss_legendre.h
+++ src/quadrature/angular/gauss_legendre.h
@@ -9,20 +9,30 @@ namespace quadrature {
 
 namespace angular {
 
-/*! \brief 1D Gauss-Legendre quadrature set.
+/*! \brief Generates a 1D Gauss-Legendre quadrature set.
+ *
+ * The Gauss-Legendre polynomial set with $$n$$-points will integrate
+ * polynomials up to degree $$2n-1$$ exactly. The points are generated on the
+ * interval $$[0, 1]$$.
  *
  */
 class GaussLegendre : public QuadratureGeneratorI<1> {
  public:
+  //! Type for pairs of cartesian positions and weights
   using PositionWeightPairType = std::pair<CartesianPosition<1>, Weight>;
 
-  GaussLegendre(const int n_points) : n_points_(n_points) {}
+  /*! \brief Constructor.
+   *
+   * @param n_points number of points on $$[0, 1]$$
+   */
+  GaussLegendre(const int n_points);
   virtual ~GaussLegendre() = default;
   virtual std::vector<PositionWeightPairType> GenerateSet() const;
+  //! \brief Returns the number of points
   virtual int order() const { return n_points_; };
 
  private:
-  const int n_points_ = 0;
+  const int n_points_{0};
 };
 
 } // namespace angular
