d5702d3bfb5881b0b34be930abc98f65b4125a22
Joshua Rehak <jsrehak@gmail.com>
2020-07-27 15:10:45 -0700

    added instrumentation::output::OutputMock

---
+++ src/instrumentation/output/tests/output_mock.h
@@ -0,0 +1,25 @@
+#ifndef BART_SRC_INSTRUMENTATION_OUTPUT_TESTS_OUTPUT_MOCK_H_
+#define BART_SRC_INSTRUMENTATION_OUTPUT_TESTS_OUTPUT_MOCK_H_
+
+#include "instrumentation/output/output_i.h"
+#include "test_helpers/gmock_wrapper.h"
+
+namespace bart {
+
+namespace instrumentation {
+
+namespace output {
+
+template <typename DataType>
+class OutputMock : OutputI<DataType> {
+ public:
+  MOCK_METHOD(OutputMock&, Output, (const DataType&), (override));
+};
+
+} // namespace output
+
+} // namespace instrumentation
+
+} // namespace bart
+
+#endif //BART_SRC_INSTRUMENTATION_OUTPUT_TESTS_OUTPUT_MOCK_H_
