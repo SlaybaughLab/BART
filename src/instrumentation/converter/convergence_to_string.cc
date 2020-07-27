521c64994ee57f53e73853c6fa8505de7e4dd68e
Joshua Rehak <jsrehak@gmail.com>
2020-07-27 11:53:44 -0700

    refactored Convert

--- src/instrumentation/converter/convergence_to_string.cc
+++ src/instrumentation/converter/convergence_to_string.cc
@@ -10,28 +10,22 @@ namespace converter {
 
 std::string ConvergenceToString::Convert(const convergence::Status &to_convert) const {
   auto return_string = output_format_;
-  // Iteration number
-  std::string iteration_num_string = output_term_to_string_map_.at(OutputTerm::kIterationNum);
-  return_string.replace(return_string.find(iteration_num_string),
-                 iteration_num_string.size(),
-                 std::to_string(to_convert.iteration_number));
-  // Max iterations
-  std::string iteration_max_string = output_term_to_string_map_.at(OutputTerm::kIterationMax);
-  return_string.replace(return_string.find(iteration_max_string),
-                 iteration_max_string.size(),
-                 std::to_string(to_convert.max_iterations));
-  // Delta
   std::ostringstream delta_stream;
   delta_stream << to_convert.delta.value();
-  std::string delta_string = output_term_to_string_map_.at(OutputTerm::kDelta);
-  return_string.replace(return_string.find(delta_string),
-                 delta_string.size(),
-                 delta_stream.str());
-  // Index
-  std::string index_string = output_term_to_string_map_.at(OutputTerm::kIndex);
-  return_string.replace(return_string.find(index_string),
-                 index_string.size(),
-                 std::to_string(to_convert.failed_index.value()));
+  std::map<OutputTerm, std::string> output_term_string_map{
+      {OutputTerm::kIterationNum, std::to_string(to_convert.iteration_number)},
+      {OutputTerm::kIterationMax, std::to_string(to_convert.max_iterations)},
+      {OutputTerm::kIndex, std::to_string(to_convert.failed_index.value_or(0))},
+      {OutputTerm::kDelta, delta_stream.str()}
+  };
+
+  for (const auto& [term, value] : output_term_string_map) {
+    std::string string_to_find = output_term_to_string_map_.at(term);
+    if (auto index = return_string.find(string_to_find);
+        index != std::string::npos) {
+      return_string.replace(index, string_to_find.size(), value);
+    }
+  }
 
   return return_string;
 }
