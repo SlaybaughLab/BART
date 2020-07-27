26ddf8a06560c66d37a7ba8bf14ae579f02a16de
Joshua Rehak <jsrehak@gmail.com>
2020-07-27 15:19:18 -0700

    updated tests to typed test, added dependencies

--- src/instrumentation/tests/instrument_test.cc
+++ src/instrumentation/tests/instrument_test.cc
@@ -1,18 +1,48 @@
 #include "instrumentation/instrument.h"
 
+#include <string>
+#include <utility>
+
 #include "instrumentation/converter/tests/converter_mock.h"
 #include "instrumentation/output/tests/output_mock.h"
+#include "convergence/status.h"
 #include "test_helpers/gmock_wrapper.h"
 
 namespace  {
 
 using namespace bart;
 
+template <typename TypePair>
 class InstrumentationInstrumentTest : public ::testing::Test {
  public:
+  using InputType = typename TypePair::first_type;
+  using OutputType = typename TypePair::second_type;
+  using ConverterType = typename instrumentation::converter::ConverterMock<InputType, OutputType>;
+  using OutputterType = typename instrumentation::output::OutputMock<OutputType>;
+  using InstrumentType = typename instrumentation::Instrument<InputType, OutputType>;
+
+  std::unique_ptr<InstrumentType> test_instrument = nullptr;
+
+  ConverterType* converter_obs_ptr_= nullptr;
+  OutputterType* outputter_obs_ptr_ = nullptr;
+  void SetUp() override;
 };
 
-TEST_F(InstrumentationInstrumentTest, Dummy) {
+template <typename TypePair>
+void InstrumentationInstrumentTest<TypePair>::SetUp() {
+  auto converter_ptr = std::make_unique<ConverterType>();
+  converter_obs_ptr_ = converter_ptr.get();
+  auto outputter_ptr = std::make_unique<OutputterType>();
+  outputter_obs_ptr_ = outputter_ptr.get();
+}
+
+using PairTypes = ::testing::Types<
+    std::pair<convergence::Status, std::string>
+>;
+
+TYPED_TEST_SUITE(InstrumentationInstrumentTest, PairTypes);
+
+TYPED_TEST(InstrumentationInstrumentTest, Dummy) {
   EXPECT_TRUE(false);
 }
 
