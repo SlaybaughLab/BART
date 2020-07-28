01d1f281095d88dd1d493c27145e733173acfe03
Joshua Rehak <jsrehak@gmail.com>
2020-07-28 13:24:28 -0700

    added AssertNotNull function to Instrument

--- src/instrumentation/instrument.cc
+++ src/instrumentation/instrument.cc
@@ -9,23 +9,29 @@ namespace bart {
 
 namespace instrumentation {
 
-template<typename InputType, typename OutputType>
+template <typename InputType, typename OutputType>
 Instrument<InputType, OutputType>::Instrument(
     std::unique_ptr<ConverterType> converter_ptr,
     std::unique_ptr<OutputterType> outputter_ptr)
     : converter_ptr_(std::move(converter_ptr)),
       outputter_ptr_(std::move(outputter_ptr)) {
-  AssertThrow(converter_ptr_ != nullptr,
-              dealii::ExcMessage("Error in constructor of instrument, "
-                                 "converter_ptr passed is null"))
-  AssertThrow(outputter_ptr_ != nullptr,
-              dealii::ExcMessage("Error in constructor of instrument, "
-                                 "outputter_ptr passed is null"))
+  AssertNotNull(converter_ptr_.get(), "converter", __FUNCTION__ );
+  AssertNotNull(outputter_ptr_.get(), "outputter", __FUNCTION__ );
 }
 
 template<typename InputType, typename OutputType>
 void Instrument<InputType, OutputType>::Read(const InputType &input) {
   outputter_ptr_->Output(converter_ptr_->Convert(input));
+}
+
+template<typename InputType, typename OutputType>
+template<typename T>
+void Instrument<InputType, OutputType>::AssertNotNull(T* ptr,
+                                                      std::string dependency,
+                                                      std::string function_name) {
+  AssertThrow(ptr != nullptr,
+              dealii::ExcMessage("Error in " + function_name + " passed "
+                                     + dependency + " pointer is null"))
 }
 
 template class Instrument<convergence::Status, std::string>;
