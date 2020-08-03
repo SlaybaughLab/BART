42a2b9f70d8480c8df6639f6d6f25f83bcdedd73
Joshua Rehak <jsrehak@gmail.com>
2020-08-03 12:19:21 -0700

    added constructor test

--- src/instrumentation/converter/tests/double_to_string_test.cc
+++ src/instrumentation/converter/tests/double_to_string_test.cc
@@ -9,6 +9,21 @@ using namespace bart;
 
 class InstrumentationConverterDoubleToStringTest : public ::testing::Test {
  public:
+  using ConverterType = instrumentation::converter::DoubleToString;
+  const double test_value_{test_helpers::RandomDouble(0, 100)};
+  std::string GetExpectedOutput(const double, ConverterType&, std::string) const;
 };
+
+std::string InstrumentationConverterDoubleToStringTest::GetExpectedOutput(
+    const double to_convert, ConverterType& converter,
+    std::string overload_format = "") const {
+  using OutputTerm = ConverterType::OutputTerm;
+}
+
+TEST_F(InstrumentationConverterDoubleToStringTest, Constructor) {
+  EXPECT_NO_THROW({
+    ConverterType test_converter;
+  });
+}
 
 } // namespace
