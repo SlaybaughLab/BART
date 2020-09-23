68de5572767b8f047650f0de8bbf3e856c083f8e
Joshua Rehak <jsrehak@gmail.com>
2020-09-23 14:46:54 -0700

    Added constructor with minuend and calculate_absolute_value bool.

--- src/instrumentation/converter/calculator/tests/vector_subtractor_test.cc
+++ src/instrumentation/converter/calculator/tests/vector_subtractor_test.cc
@@ -6,21 +6,55 @@
 namespace {
 
 namespace instrumentation = bart::instrumentation;
+namespace test_helpers = bart::test_helpers;
 
 class InstrumentationConverterCalculatorVectorSubtractorTest
- : public ::testing::Test {
+ : public ::testing::TestWithParam<bool> {
  public:
   using VectorSubtractor = instrumentation::converter::calculator::VectorSubtractor;
   using DealiiVector = VectorSubtractor::DealiiVector;
+  using AbsoluteValue = instrumentation::converter::calculator::AbsoluteValue;
+
+  std::unique_ptr<VectorSubtractor> test_subtractor_ptr_;
+  const int vector_length_{test_helpers::RandomInt(10, 20)};
+  DealiiVector minuend_, subtrahend_, difference_; // difference = minuend - subtrahend
+  const bool calculate_absolute_value_{GetParam()};
   void SetUp() override;
 };
 
-void InstrumentationConverterCalculatorVectorSubtractorTest::SetUp() {
+INSTANTIATE_TEST_SUITE_P(AbsoluteValueSubtraction,
+                         InstrumentationConverterCalculatorVectorSubtractorTest,
+                         ::testing::Bool());
 
+void InstrumentationConverterCalculatorVectorSubtractorTest::SetUp() {
+  minuend_.reinit(vector_length_);
+  subtrahend_.reinit(vector_length_);
+  difference_.reinit(vector_length_);
+
+  for (int i = 0; i < vector_length_; ++i) {
+    double minuend_value{test_helpers::RandomDouble(-100, 100)};
+    double subtrahend_value{test_helpers::RandomDouble(-100, 100)};
+
+    minuend_[i] = minuend_value;
+    subtrahend_[i] = subtrahend_value;
+    double difference_value = minuend_value - subtrahend_value;
+
+    if (calculate_absolute_value_) {
+      difference_value = std::abs(difference_value);
+    }
+
+    difference_[i] = difference_value;
+  }
+  test_subtractor_ptr_ = std::make_unique<VectorSubtractor>(
+      minuend_,
+      AbsoluteValue(calculate_absolute_value_));
 }
 
-TEST_F(InstrumentationConverterCalculatorVectorSubtractorTest, Dummy) {
-  EXPECT_TRUE(false);
+TEST_P(InstrumentationConverterCalculatorVectorSubtractorTest, Getters) {
+  EXPECT_EQ(test_subtractor_ptr_->calculate_absolute_value(),
+            calculate_absolute_value_);
+  ASSERT_EQ(test_subtractor_ptr_->minuend().size(), minuend_.size());
+  EXPECT_EQ(test_subtractor_ptr_->minuend(), minuend_);
 }
 
 
--- src/instrumentation/converter/calculator/vector_subtractor.h
+++ src/instrumentation/converter/calculator/vector_subtractor.h
@@ -4,6 +4,7 @@
 #include <deal.II/lac/vector.h>
 
 #include "instrumentation/converter/converter_i.h"
+#include "utility/named_type.h"
 
 namespace bart {
 
@@ -12,6 +13,8 @@ namespace instrumentation {
 namespace converter {
 
 namespace calculator {
+
+using AbsoluteValue = utility::NamedType<bool, struct AbsoluteValueStruct>;
 
 /*! \brief This converter returns the result of subtracting the provided vector
  *         from a stored vector.
@@ -23,9 +26,20 @@ class VectorSubtractor : public ConverterI<dealii::Vector<double>,
                                            dealii::Vector<double>> {
  public:
   using DealiiVector = dealii::Vector<double>;
+
+  VectorSubtractor(DealiiVector minuend, AbsoluteValue calculate_absolute_value)
+      : minuend_(minuend),
+        calculate_absolute_value_(calculate_absolute_value) {}
   DealiiVector Convert(const DealiiVector &input) const override {
     return DealiiVector();
   }
+
+  DealiiVector minuend() const { return minuend_; }
+  bool calculate_absolute_value() const { return calculate_absolute_value_; }
+
+ private:
+  DealiiVector minuend_;
+  bool calculate_absolute_value_{false};
 };
 
 } // namespace calculator
