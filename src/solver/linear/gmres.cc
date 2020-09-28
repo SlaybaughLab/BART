ea08517641ca92539a4296490ef0b16be65bf911
Joshua Rehak <jsrehak@gmail.com>
2020-09-28 14:15:45 -0700

    Added registration for GMRES.

--- src/solver/linear/gmres.cc
+++ src/solver/linear/gmres.cc
@@ -1,10 +1,20 @@
 #include "gmres.h"
 
+#include "solver/factory/solver_factory_registrar.h"
+
 #include <deal.II/lac/petsc_solver.h>
 
 namespace bart {
 
 namespace solver {
+
+factory::SolverFactoryRegistrar<GMRES, int, double> _gmres_registration(
+    LinearSolverName::kGMRES,
+    [] (int max_iterations, double convergence_tolerance) {
+      std::unique_ptr<LinearI> return_ptr;
+      return_ptr = std::make_unique<GMRES>(max_iterations, convergence_tolerance);
+      return return_ptr; }
+);
 
 GMRES::GMRES(int max_iterations, double convergence_tolerance)
     : solver_control_(max_iterations, convergence_tolerance){}
