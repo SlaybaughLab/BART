8ffbeae258a51260ef6501ba13cbf269cc327f75
Joshua Rehak <jsrehak@gmail.com>
2020-10-07 14:44:58 -0700

    Initial commit of InstrumentationBuilder files.

---
+++ src/instrumentation/builder/tests/instrumentation_builder_test.cc
@@ -0,0 +1,15 @@
+#include "instrumentation/builder/instrumentation_builder.h"
+
+#include "test_helpers/gmock_wrapper.h"
+
+namespace  {
+
+class InstrumentationBuilderTest : public ::testing::Test {
+ public:
+};
+
+TEST_F(InstrumentationBuilderTest, Dummy) {
+  EXPECT_TRUE(false);
+}
+
+} // namespace
