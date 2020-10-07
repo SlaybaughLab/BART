57f4a334842c803c17e3bcef195ea95960ddbcd4
Joshua Rehak <jsrehak@gmail.com>
2020-10-07 14:46:38 -0700

    renamed InstrumentationBuilder to InstrumentBuilder.

---
+++ src/instrumentation/builder/instrument_builder.h
@@ -0,0 +1,10 @@
+#ifndef BART_SRC_INSTRUMENTATION_BUILDER_INSTRUMENT_BUILDER_H_
+#define BART_SRC_INSTRUMENTATION_BUILDER_INSTRUMENT_BUILDER_H_
+
+namespace bart::instrumentation::builder {
+
+
+
+} // namespace bart::instrumentation::builder
+
+#endif //BART_SRC_INSTRUMENTATION_BUILDER_INSTRUMENT_BUILDER_H_
---
+++ src/instrumentation/builder/tests/instrument_builder_test.cc
@@ -0,0 +1,15 @@
+#include "instrumentation/builder/instrument_builder.h"
+
+#include "test_helpers/gmock_wrapper.h"
+
+namespace  {
+
+class InstrumentationBuilderInstrumentBuilderTest : public ::testing::Test {
+ public:
+};
+
+TEST_F(InstrumentationBuilderInstrumentBuilderTest, Dummy) {
+  EXPECT_TRUE(false);
+}
+
+} // namespace
