36d8b8624f86f49ae656a6d2b9881f86b56f548a
Joshua Rehak <jsrehak@gmail.com>
2020-10-13 13:39:57 -0700

    Corrected call to compare mpi process in builder for StringColorPairToString.

--- src/instrumentation/builder/instrument_builder.cc
+++ src/instrumentation/builder/instrument_builder.cc
@@ -25,15 +25,15 @@ std::unique_ptr<InstrumentI<StringColorPair>> InstrumentBuilder::BuildInstrument
   switch (name) {
     case InstrumentName::kColorStatusToConditionalOstream: {
       using ConditionalOstream = dealii::ConditionalOStream;
+      using ConditionalOstreamPtr = std::unique_ptr<ConditionalOstream>;
       return std::make_unique<Instrument<StringColorPair, std::string>>(
           converter::ConverterIFactory<StringColorPair, std::string>::get()
               .GetConstructor(converter::ConverterName::kStringColorPairToString)(),
-          outstream::OutstreamIFactory<std::string,
-                                       std::unique_ptr<ConditionalOstream>>::get()
+          outstream::OutstreamIFactory<std::string, ConditionalOstreamPtr>::get()
               .GetConstructor(outstream::OutstreamName::kToConditionalOstream)(
                   std::make_unique<ConditionalOstream>(
                       std::cout,
-                      dealii::Utilities::MPI::this_mpi_process == 0)));
+                      dealii::Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0)));
     }
     default:
       AssertThrow(false,
