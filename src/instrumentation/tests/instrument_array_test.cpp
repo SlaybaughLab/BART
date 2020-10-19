ce30b7502c1201a10887fe29067ad235a1d1155a
Joshua Rehak <jsrehak@gmail.com>
2020-10-19 11:20:12 -0700

    Added dummy test for InstrumentArray and test using SphericalHarmonic.

--- src/instrumentation/tests/instrument_array_test.cpp
+++ src/instrumentation/tests/instrument_array_test.cpp
@@ -4,6 +4,7 @@
 
 #include "convergence/status.h"
 #include "instrumentation/tests/instrument_mock.h"
+#include "system/moments/spherical_harmonic_i.h"
 #include "test_helpers/gmock_wrapper.h"
 #include "utility/colors.h"
 
@@ -12,6 +13,8 @@ namespace  {
 namespace convergence = bart::convergence;
 namespace instrumentation = bart::instrumentation;
 namespace utility = bart::utility;
+namespace moments = bart::system::moments;
+
 
 template <typename InputType>
 class InstrumentationInstrumentArrayTest : public ::testing::Test {
@@ -20,8 +23,15 @@ class InstrumentationInstrumentArrayTest : public ::testing::Test {
 
 using InstrumentTypes = ::testing::Types<convergence::Status, std::pair<int, double>,
                                          std::pair<std::string, utility::Color>,
-                                         dealii::Vector<double>>;
+                                         dealii::Vector<double>,
+                                         moments::SphericalHarmonicI>;
 
 TYPED_TEST_SUITE(InstrumentationInstrumentArrayTest, InstrumentTypes);
+
+TYPED_TEST(InstrumentationInstrumentArrayTest, Dummy) {
+  EXPECT_TRUE(true);
+}
+
+
 
 } // namespace
