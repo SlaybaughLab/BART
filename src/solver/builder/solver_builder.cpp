cda2729e0cf5b4f970ca68e53a4736846ffac07a
Joshua Rehak <jsrehak@gmail.com>
2020-10-30 11:16:17 -0700

    Added implementation of SolverBuilder.

--- src/solver/builder/solver_builder.cpp
+++ src/solver/builder/solver_builder.cpp
@@ -6,7 +6,37 @@
 namespace bart::solver::builder {
 
 template <>
-auto SolverBuilder::BuildSolver(const SolverName) -> std::unique_ptr<group::SingleGroupSolverI> {
+auto SolverBuilder::BuildSolver(const SolverName name, const int max_iterations, const double convergence_tolerance)
+-> std::unique_ptr<group::SingleGroupSolverI> {
+  // Build linear solver
+  std::unique_ptr<linear::LinearI> linear_solver_ptr;
+  switch (name) {
+    case SolverName::kDefaultGMRESGroupSolver: {
+      linear_solver_ptr = std::move(linear::LinearIFactory<int, double>::get()
+                                        .GetConstructor(linear::LinearSolverName::kGMRES)
+                                            (max_iterations, convergence_tolerance));
+    }
+  }
+
+  // Build group solver
+  std::unique_ptr<group::SingleGroupSolverI> return_ptr;
+  switch (name) {
+    case SolverName::kDefaultGMRESGroupSolver: {
+      return solver::group::SingleGroupSolverIFactory<std::unique_ptr<linear::LinearI>>::get()
+          .GetConstructor(solver::group::GroupSolverName::kDefaultImplementation)
+              (std::move(linear_solver_ptr));
+    }
+  }
+  return nullptr;
+}
+
+template <>
+auto SolverBuilder::BuildSolver(const SolverName name) -> std::unique_ptr<group::SingleGroupSolverI> {
+  switch (name) {
+    case SolverName::kDefaultGMRESGroupSolver: {
+      return BuildSolver(SolverName::kDefaultGMRESGroupSolver, 100, 1e-10);
+    }
+  }
   return nullptr;
 }
 
--- src/solver/builder/tests/solver_builder_test.cpp
+++ src/solver/builder/tests/solver_builder_test.cpp
@@ -1,17 +1,49 @@
 #include "solver/builder/solver_builder.hpp"
 
+#include "solver/group/single_group_solver.h"
+#include "solver/linear/gmres.h"
+
 #include "test_helpers/gmock_wrapper.h"
+#include "test_helpers/test_helper_functions.h"
 
 namespace  {
 
 namespace solver = bart::solver;
 namespace builder = solver::builder;
+namespace test_helpers = bart::test_helpers;
 using SolverName = builder::SolverName;
 
+class SolverBuilderDefaultGMRESTest : public ::testing::Test {
+ public:
+  using ExpectedGroupSolver = solver::group::SingleGroupSolver;
+  using ExpectedLinearSolver = solver::linear::GMRES;
+  static constexpr int default_max_iterations{ 100 };
+  static constexpr double default_convergence_tolerance { 1e-10 };
+};
 
-TEST(SolverBuilderTests, DefaultGroupSolverWithGMRES) {
+TEST_F(SolverBuilderDefaultGMRESTest, DefaultParameters) {
   auto solver_ptr = builder::SolverBuilder::BuildSolver(SolverName::kDefaultGMRESGroupSolver);
   ASSERT_NE(solver_ptr, nullptr);
+  auto group_solver_ptr = dynamic_cast<ExpectedGroupSolver*>(solver_ptr.get());
+  ASSERT_NE(group_solver_ptr, nullptr);
+  auto linear_solver_ptr = dynamic_cast<ExpectedLinearSolver*>(group_solver_ptr->linear_solver_ptr());
+  ASSERT_NE(linear_solver_ptr, nullptr);
+  EXPECT_EQ(linear_solver_ptr->max_iterations(), default_max_iterations);
+  EXPECT_EQ(linear_solver_ptr->convergence_tolerance(), default_convergence_tolerance);
+}
+
+TEST_F(SolverBuilderDefaultGMRESTest, SetParameters) {
+  const int max_iterations { test_helpers::RandomInt(150, 200) };
+  const double convergence_tolerance { test_helpers::RandomDouble(1e-10, 1e-6) };
+  auto solver_ptr = builder::SolverBuilder::BuildSolver(SolverName::kDefaultGMRESGroupSolver,
+                                                        max_iterations, convergence_tolerance);
+  ASSERT_NE(solver_ptr, nullptr);
+  auto group_solver_ptr = dynamic_cast<ExpectedGroupSolver*>(solver_ptr.get());
+  ASSERT_NE(group_solver_ptr, nullptr);
+  auto linear_solver_ptr = dynamic_cast<ExpectedLinearSolver*>(group_solver_ptr->linear_solver_ptr());
+  ASSERT_NE(linear_solver_ptr, nullptr);
+  EXPECT_EQ(linear_solver_ptr->max_iterations(), max_iterations);
+  EXPECT_EQ(linear_solver_ptr->convergence_tolerance(), convergence_tolerance);
 }
 
 } // namespace
