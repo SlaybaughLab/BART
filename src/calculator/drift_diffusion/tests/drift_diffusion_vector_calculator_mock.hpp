7094f59590980d88cb4c63a57798b3ffe4c34555
Joshua Rehak <jsrehak@gmail.com>
2020-12-10 09:08:47 -0800

    Added mock DriftDiffusionCalculator.

---
+++ src/calculator/drift_diffusion/tests/drift_diffusion_vector_calculator_mock.hpp
@@ -0,0 +1,19 @@
+#ifndef BART_SRC_CALCULATOR_DRIFT_DIFFUSION_TESTS_DRIFT_DIFFUSION_VECTOR_CALCULATOR_MOCK_HPP_
+#define BART_SRC_CALCULATOR_DRIFT_DIFFUSION_TESTS_DRIFT_DIFFUSION_VECTOR_CALCULATOR_MOCK_HPP_
+
+#include "calculator/drift_diffusion/drift_diffusion_vector_calculator_i.hpp"
+#include "test_helpers/gmock_wrapper.h"
+
+namespace bart::calculator::drift_diffusion {
+
+template <int dim>
+class DriftDiffusionVectorCalculatorMock : public DriftDiffusionVectorCalculatorI<dim> {
+ public:
+  using typename DriftDiffusionVectorCalculatorI<dim>::Tensor;
+  MOCK_METHOD(Tensor, DriftDiffusion, (const double scalar_flux, const double integrated_angular_flux,
+      const Tensor& shape_gradient, const double sigma_t, const double diffusion_coefficient), (const, override));
+};
+
+} // namespace bart::calculator::drift_diffusion
+
+#endif //BART_SRC_CALCULATOR_DRIFT_DIFFUSION_TESTS_DRIFT_DIFFUSION_VECTOR_CALCULATOR_MOCK_HPP_
