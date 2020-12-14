32a46c1c2f441563320fe6365d07e9fe84f7aebf
Joshua Rehak <jsrehak@gmail.com>
2020-12-14 09:54:43 -0800

    Added mock drift diffusion updater.

---
+++ src/formulation/scalar/tests/drift_diffusion_mock.hpp
@@ -0,0 +1,23 @@
+#ifndef BART_SRC_FORMULATION_SCALAR_TESTS_DRIFT_DIFFUSION_MOCK_HPP_
+#define BART_SRC_FORMULATION_SCALAR_TESTS_DRIFT_DIFFUSION_MOCK_HPP_
+
+#include "formulation/scalar/drift_diffusion_i.hpp"
+#include "test_helpers/gmock_wrapper.h"
+
+namespace bart::formulation::scalar {
+
+template <int dim>
+class DriftDiffusionMock : public DriftDiffusionI<dim> {
+ public:
+  using typename DriftDiffusionI<dim>::CellPtr;
+  using typename DriftDiffusionI<dim>::EnergyGroup;
+  using typename DriftDiffusionI<dim>::Matrix;
+  using typename DriftDiffusionI<dim>::Vector;
+
+  MOCK_METHOD(void, FillCellDriftDiffusionTerm, (Matrix& to_fill, const CellPtr&, const system::EnergyGroup,
+      const Vector& group_scalar_flux, const Vector& integrated_angular_flux), (const, override));
+};
+
+} // namespace bart::formulation::scalar
+
+#endif //BART_SRC_FORMULATION_SCALAR_TESTS_DRIFT_DIFFUSION_MOCK_HPP_
