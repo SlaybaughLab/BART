dbf61945877058b8d4fdc8de30ecaeffaee831e3
Joshua Rehak <jsrehak@gmail.com>
2020-12-16 11:49:31 -0800

    Added implementation of Execute to GetScalarFluxFromFramework.

--- src/iteration/subroutine/get_scalar_flux_from_framework.hpp
+++ src/iteration/subroutine/get_scalar_flux_from_framework.hpp
@@ -7,7 +7,26 @@ namespace bart::iteration::subroutine {
 
 class GetScalarFluxFromFramework : public SubroutineI {
  public:
-  auto Execute(system::System &) -> void override {};
+  using Framework = bart::framework::FrameworkI;
+  GetScalarFluxFromFramework(std::unique_ptr<Framework> framework_ptr)
+      : framework_ptr_(std::move(framework_ptr)) {
+    AssertPointerNotNull(framework_ptr_.get(), "framework pointer", "GetScalarFluxFromFramework constructor");
+  }
+
+  auto Execute(system::System& system) -> void override {
+    this->framework_ptr_->SolveSystem();
+    auto& solved_scalar_fluxes = *this->framework_ptr_->system()->current_moments;
+    for (int group = 0; group < system.total_groups; ++group) {
+      std::array<int, 3> index{group, 0, 0};
+      auto& solved_scalar_flux = solved_scalar_fluxes.GetMoment(index);
+      auto& system_scalar_flux = system.current_moments->GetMoment(index);
+      system_scalar_flux = solved_scalar_flux;
+    }
+  };
+
+  auto framework_ptr() -> Framework* { return framework_ptr_.get(); }
+ private:
+  std::unique_ptr<Framework> framework_ptr_{ nullptr };
 };
 
 } // namespace bart::iteration::subroutine
