bd4ef593144098d492ec122fba0d3e705ea26ab4
Joshua Rehak <jsrehak@gmail.com>
2021-01-21 10:19:33 -0800

    Added NDA struct for required NDA data from higher order solve.

--- src/framework/framework_parameters.hpp
+++ src/framework/framework_parameters.hpp
@@ -5,7 +5,10 @@
 #include "problem/parameter_types.hpp"
 #include "data/cross_sections.h"
 #include "utility/named_type.h"
+#include "quadrature/calculators/angular_flux_integrator_i.hpp"
 #include "quadrature/quadrature_set_i.h"
+#include "system/moments/spherical_harmonic_i.h"
+#include "system/solution/solution_types.h"
 
 #include <memory>
 #include <optional>
@@ -14,12 +17,15 @@
 namespace bart::framework {
 
 struct FrameworkParameters {
+  using AngularFluxIntegrator = quadrature::calculators::AngularFluxIntegratorI;
+  using AngularFluxStorage = system::solution::EnergyGroupToAngularSolutionPtrMap;
   using AngularQuadratureOrder = quadrature::Order;
   using DomainSize = utility::NamedType<std::vector<double>, struct DomainSizeStruct>;
   using K_EffectiveUpdaterName = eigenvalue::k_effective::K_EffectiveUpdaterName;
   using NumberOfCells = utility::NamedType<std::vector<int>, struct NumberOfCellsStruct>;
   using PolynomialDegree = utility::NamedType<int, struct PolynomialDegreeStruct>;
   using SpatialDimension = utility::NamedType<int, struct SpatialDimensionStruct>;
+  using Moments = system::moments::SphericalHarmonicI;
 
   std::string name{""};
   std::string output_filename_base{""};
@@ -52,12 +58,21 @@ struct FrameworkParameters {
   problem::CellFiniteElementType cell_finite_element_type{problem::CellFiniteElementType::kGaussian};
   PolynomialDegree polynomial_degree{ 2 };
 
-  // Acceleration methods
-  bool use_nda_{ false };
+
 
   // Optional shared framework parts
   template <typename Part> using OptionalSharedPart = std::optional<std::shared_ptr<Part>>;
   OptionalSharedPart<data::CrossSections> cross_sections_ { std::nullopt };
+
+  // Acceleration methods
+  bool use_nda_{ false };
+  // Higher order data to support NDA
+  struct NDA_Data {
+    std::shared_ptr<AngularFluxIntegrator> angular_flux_integrator_ptr_{ nullptr };
+    std::shared_ptr<Moments> higher_order_moments_ptr_{ nullptr };
+    AngularFluxStorage higher_order_angular_flux_;
+  };
+  NDA_Data nda_data_;
 };
 
 } // namespace bart::framework
