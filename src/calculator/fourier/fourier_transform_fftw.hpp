7e76b175dd17b8c7808e9d66d5986aeea3d15ff3
Joshua Rehak <jsrehak@gmail.com>
2021-02-10 21:50:15 -0800

    Added documentation to FourierTransform classes.

--- src/calculator/fourier/fourier_transform_fftw.hpp
+++ src/calculator/fourier/fourier_transform_fftw.hpp
@@ -8,14 +8,25 @@
 #include "utility/named_type.h"
 
 namespace bart::calculator::fourier {
-
+//! Wrapper namespace for the C FFTW library.
 namespace fftw {
 #include <fftw3.h>
 } // namespace fftw
 
+/*! \brief Default implementation of the calculation of the DFT using the FFTW library.
+ *
+ * Due to the way FFTW works, the size of the input vector must be known when the class is instantiated. This has the
+ * downside of requiring _a priori_ knowledge of the vector size. The benefit is that this enables FFTW to identify an
+ * optimized algorithm for calculating the DFT.
+ *
+ * */
 class FourierTransformFFTW : public FourierTransformI {
  public:
   using FourierTransformI::ComplexVector;
+  /*! \brief Constructor.
+   *
+   * @param n_samples size of the vector that will be provided as input to this class.
+   */
   explicit FourierTransformFFTW(const int n_samples);
   ~FourierTransformFFTW();
 
@@ -23,6 +34,7 @@ class FourierTransformFFTW : public FourierTransformI {
                                   Normalized normalized = Normalized(false)) -> ComplexVector override;
   [[nodiscard]] auto CalculateDFT(const dealii::Vector<double> &input,
                                   Normalized normalized = Normalized(false)) -> ComplexVector override;
+  /*! \brief Get number of samples identified during construction. */
   [[nodiscard]] auto n_samples() const -> int { return n_samples_; }
  private:
   const int n_samples_;
--- src/calculator/fourier/fourier_transform_i.hpp
+++ src/calculator/fourier/fourier_transform_i.hpp
@@ -9,16 +9,31 @@
 #include "utility/named_type.h"
 #include "utility/has_description.h"
 
+//! \brief Calculators for calculating the Fourier transform.
 namespace bart::calculator::fourier {
 
 using Normalized = utility::NamedType<bool, struct NormalizedStruct>;
 
+/*! \brief Interface for classes that calculate the discrete fourier transform. */
 class FourierTransformI : public utility::HasDescription {
  public:
+  /// Vector of complex values, used for both input and output of the DFT.
   using ComplexVector = std::vector<std::complex<double>>;
   virtual ~FourierTransformI() = default;
-  virtual auto CalculateDFT(const ComplexVector& input, Normalized) -> ComplexVector = 0;
-  virtual auto CalculateDFT(const dealii::Vector<double>& input, Normalized) -> ComplexVector = 0;
+  /*! \brief Calculate the DFT of a provided complex vector.
+   *
+   * @param input input vector of complex values to take the DFT of.
+   * @param normalize_return if true, the result is normalized by dividing by the total size of the vector.
+   * @return vector of complex vector that holds the DFT of the input.
+   */
+  virtual auto CalculateDFT(const ComplexVector& input, Normalized normalize_return) -> ComplexVector = 0;
+  /*! \brief Calculate the DFT of a provided dealii-format Vector.
+   *
+   * @param input input vector of real values to take the DFT of.
+   * @param normalize_return if true, the result is normalized by dividing by the total size of the vector.
+   * @return vector of complex vector that holds the DFT of the input.
+   */
+  virtual auto CalculateDFT(const dealii::Vector<double>& input, Normalized normalize_return) -> ComplexVector = 0;
 };
 
 } // namespace bart::calculator::fourier
