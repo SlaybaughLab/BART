b53003d050bba82b6189cd5b9868f8af0f51719f
Joshua Rehak <jsrehak@gmail.com>
2021-02-11 09:45:41 -0800

    Refactored tests for utility::PreciseSum.

--- src/utility/tests/utility_function_test.cpp
+++ src/utility/tests/utility_function_test.cpp
@@ -1,46 +1,30 @@
-#include "../utility_functions.hpp"
+#include "utility/utility_functions.hpp"
 
-#include <algorithm>
 #include <array>
-#include <iostream>
-#include <numeric>
 #include <vector>
 
-#include <gtest/gtest.h>
+#include "test_helpers/gmock_wrapper.h"
 
-#include "../../test_helpers/gmock_wrapper.h"
+namespace  {
 
-class TestUtilityFunctions : public ::testing::Test {
- protected:
-};
-
-// TEST_F(TestUtilityFunctions, VectorTest) {
-//   std::vector<double> numbers =
-//       {0.01, 0.001, 0.0001, 0.000001, 0.00000000001};
-//   double sum = bart::utility::PreciseSum(numbers);
-//   EXPECT_FLOAT_EQ(sum, 0.011101);
-// }
-
-TEST_F(TestUtilityFunctions, VectorTest) {
-  std::vector<double> numbers =
-      {10e100, 1.0, -10e100, 1.0};
-  double sum = bart::utility::PreciseSum(numbers);
-  EXPECT_FLOAT_EQ(sum, 2);
+TEST(TestUtilityFunctions, VectorTest) {
+  std::vector<double> numbers = {10e100, 1.0, -10e100, 1.0};
+  EXPECT_FLOAT_EQ(bart::utility::PreciseSum(numbers), 2);
 }
 
-TEST_F(TestUtilityFunctions, SecondVectorTest) {
-  std::vector<double> numbers =
-      {1.0, 10e100, 1.0, -10e100};
-  double sum = bart::utility::PreciseSum(numbers);
-  EXPECT_FLOAT_EQ(sum, 2);
+TEST(TestUtilityFunctions, SecondVectorTest) {
+  std::vector<double> numbers = {1.0, 10e100, 1.0, -10e100};
+  EXPECT_FLOAT_EQ(bart::utility::PreciseSum(numbers), 2);
 }
 
-TEST_F(TestUtilityFunctions, ArrayTest) {
-  std::array<double, 5> numbers =
-      {0.01, 0.001, 0.0001, 0.000001, 0.00000000001};
-  double sum = bart::utility::PreciseSum(numbers);
-  EXPECT_FLOAT_EQ(sum, 0.011101);
+TEST(TestUtilityFunctions, ArrayTest) {
+  std::array<double, 5> numbers = {0.01, 0.001, 0.0001, 0.000001, 0.00000000001};
+  EXPECT_FLOAT_EQ(bart::utility::PreciseSum(numbers), 0.011101);
 }
+
+} // namespace
+
+
 
 
 
