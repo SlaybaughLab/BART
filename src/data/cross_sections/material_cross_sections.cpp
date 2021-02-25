da3df6767c63a9f8b8a56870d6b7e0bc601871cb
Joshua Rehak <jsrehak@gmail.com>
2021-02-25 14:20:00 -0800

    Added default implementation data::CrossSections.

--- src/data/cross_sections/material_cross_sections.cpp
+++ src/data/cross_sections/material_cross_sections.cpp
@@ -2,18 +2,18 @@
 
 namespace bart::data::cross_sections {
 
-MaterialCrossSections::MaterialCrossSections(data::material::MaterialI &materials)
-    : diffusion_coef_(materials.GetDiffusionCoef()),
-      sigma_t_(materials.GetSigT()),
-      inverse_sigma_t_(materials.GetInvSigT()),
-      sigma_s_(materials.GetSigS()),
-      sigma_s_per_ster_(materials.GetSigSPerSter()),
-      q_(materials.GetQ()),
-      q_per_ster_(materials.GetQPerSter()),
-      is_material_fissile_(materials.GetFissileIDMap()),
-      nu_sigma_f_(materials.GetNuSigF()),
-      fiss_transfer_(materials.GetChiNuSigF()),
-      fiss_transfer_per_ster_(materials.GetChiNuSigFPerSter())
-{}
+MaterialCrossSections::MaterialCrossSections(data::material::MaterialI &materials) {
+  this->diffusion_coef_ = materials.GetDiffusionCoef();
+  this->sigma_t_ = materials.GetSigT();
+  this->inverse_sigma_t_ = materials.GetInvSigT();
+  this->sigma_s_ = materials.GetSigS();
+  this->sigma_s_per_ster_ = materials.GetSigSPerSter();
+  this->q_ = materials.GetQ();
+  this->q_per_ster_ = materials.GetQPerSter();
+  this->is_material_fissile_ = materials.GetFissileIDMap();
+  this->nu_sigma_f_ = materials.GetNuSigF();
+  this->fiss_transfer_ = materials.GetChiNuSigF();
+  this->fiss_transfer_per_ster_ = materials.GetChiNuSigFPerSter();
+}
 
 } // namespace bart::data::cross_sections
