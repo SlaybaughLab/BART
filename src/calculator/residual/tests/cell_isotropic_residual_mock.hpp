e64c168a63febdb568061ed58cb5d34689a36794
Joshua Rehak <jsrehak@gmail.com>
2021-03-16 13:19:50 -0700

    Added CellIsotropicResidualMock.

---
+++ src/calculator/residual/tests/cell_isotropic_residual_mock.hpp
@@ -0,0 +1,21 @@
+#ifndef BART_SRC_CALCULATOR_RESIDUAL_TESTS_CELL_ISOTROPIC_RESIDUAL_MOCK_HPP_
+#define BART_SRC_CALCULATOR_RESIDUAL_TESTS_CELL_ISOTROPIC_RESIDUAL_MOCK_HPP_
+
+#include "calculator/residual/cell_isotropic_residual_i.hpp"
+#include "test_helpers/gmock_wrapper.h"
+
+namespace bart::calculator::residual {
+
+template <int dim>
+class CellIsotropicResidualMock : public CellIsotropicResidualI<dim> {
+ public:
+  using typename CellIsotropicResidualI<dim>::CellPtr;
+  using typename CellIsotropicResidualI<dim>::FluxMoments;
+  using typename CellIsotropicResidualI<dim>::Vector;
+  MOCK_METHOD(double, CalculateCellResidual, (CellPtr, FluxMoments*, FluxMoments*, int), (override));
+};
+
+} // namespace bart::calculator::residual
+
+
+#endif //BART_SRC_CALCULATOR_RESIDUAL_TESTS_CELL_ISOTROPIC_RESIDUAL_MOCK_HPP_
