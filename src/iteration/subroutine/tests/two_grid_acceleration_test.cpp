8d29462f7bf7ff2cba37f2fd9e782d17be9748bd
Joshua Rehak <jsrehak@gmail.com>
2021-03-16 15:34:35 -0700

    Removed cross-sections dependency from TwoGridAcceleration Subroutine.

--- src/iteration/subroutine/tests/two_grid_acceleration_test.cpp
+++ src/iteration/subroutine/tests/two_grid_acceleration_test.cpp
@@ -3,42 +3,54 @@
 #include <deal.II/lac/vector.h>
 
 #include "acceleration/two_grid/tests/flux_corrector_mock.hpp"
-#include "calculator/residual/tests/isotropic_residual_mock.hpp"
-#include "data/cross_sections/tests/cross_sections_mock.hpp"
+#include "calculator/residual/tests/domain_isotropic_residual_mock.hpp"
 #include "framework/tests/framework_mock.hpp"
 #include "system/moments/tests/spherical_harmonic_mock.h"
 #include "system/system.hpp"
+#include "test_helpers/test_helper_functions.h"
+#include "test_helpers/test_assertions.hpp"
 
 #include "test_helpers/gmock_wrapper.h"
 
 namespace  {
 
 using namespace bart;
+using ::testing::NiceMock, ::testing::DoDefault, ::testing::Return, ::testing::ReturnRef, ::testing::_;
 
 class IterationSubroutineTwoGridAccelerationTest : public ::testing::Test {
  public:
-  using CrossSectionsMock = data::cross_sections::CrossSectionsMock;
   using RHSVector = dealii::Vector<double>;
   using FluxCorrectorMock = acceleration::two_grid::FluxCorrectorMock;
   using FrameworkMock = framework::FrameworkMock;
-  using IsotropicResidualCalculatorMock = calculator::residual::IsotropicResidualMock;
+  using IsotropicResidualCalculatorMock = calculator::residual::DomainIsotropicResidualMock;
+  using Moments = system::moments::SphericalHarmonicMock;
   using TwoGridSubroutine = iteration::subroutine::TwoGridAcceleration;
   using System = system::System;
 
   // Dependency pointers
   std::shared_ptr<RHSVector> rhs_vector_ptr_;
-  std::shared_ptr<CrossSectionsMock> cross_sections_ptr_ { std::make_shared<CrossSectionsMock>() };
   FluxCorrectorMock* flux_corrector_mock_obs_ptr_;
   FrameworkMock* framework_mock_obs_ptr_;
   IsotropicResidualCalculatorMock* isotropic_residual_calculator_mock_obs_ptr_;
+  Moments* current_moments_obs_ptr_;
+  Moments* previous_moments_obs_ptr_;
 
   std::unique_ptr<TwoGridSubroutine> test_subroutine_;
   System test_system_;
+
+  dealii::Vector<double> total_isotropic_residual_;
 
   static constexpr int vector_size_{ 10 };
   static constexpr int total_groups_{ 3 };
   auto SetUp() -> void override;
 };
+
+auto DealiiVector(std::vector<double> to_convert) {
+  dealii::Vector<double> return_vector(to_convert.size());
+  for (std::vector<double>::size_type i = 0; i < to_convert.size(); ++i)
+    return_vector(i) = to_convert.at(i);
+  return return_vector;
+}
 
 auto IterationSubroutineTwoGridAccelerationTest::SetUp() -> void {
   rhs_vector_ptr_ = std::make_shared<RHSVector>(vector_size_);
@@ -53,10 +65,17 @@ auto IterationSubroutineTwoGridAccelerationTest::SetUp() -> void {
   test_subroutine_ = std::make_unique<TwoGridSubroutine>(std::move(flux_corrector_ptr),
                                                          std::move(framework_ptr),
                                                          std::move(isotropic_residual_calculator_ptr),
-                                                         cross_sections_ptr_,
                                                          rhs_vector_ptr_);
 
   test_system_.total_groups = this->total_groups_;
+  test_system_.current_moments = std::make_shared<Moments>();
+  test_system_.previous_moments = std::make_unique<Moments>();
+  current_moments_obs_ptr_ = dynamic_cast<Moments*>(test_system_.current_moments.get());
+  previous_moments_obs_ptr_ = dynamic_cast<Moments*>(test_system_.previous_moments.get());
+
+  total_isotropic_residual_ = DealiiVector(test_helpers::RandomVector(vector_size_, 0, 100));
+  ON_CALL(*isotropic_residual_calculator_mock_obs_ptr_, CalculateDomainResidual(_,_))
+      .WillByDefault(Return(total_isotropic_residual_));
 }
 
 // Getters for depdendencies should return correct pointers to dependencies
@@ -64,26 +83,31 @@ TEST_F(IterationSubroutineTwoGridAccelerationTest, DependencyGetters) {
   EXPECT_EQ(test_subroutine_->framework_ptr(), framework_mock_obs_ptr_);
   EXPECT_EQ(test_subroutine_->flux_corrector_ptr(), flux_corrector_mock_obs_ptr_);
   EXPECT_EQ(test_subroutine_->residual_calculator_ptr(), isotropic_residual_calculator_mock_obs_ptr_);
-  EXPECT_EQ(test_subroutine_->cross_sections_ptr(), cross_sections_ptr_);
   EXPECT_EQ(test_subroutine_->isotropic_residual_ptr(), rhs_vector_ptr_);
 }
 
 // Passing a null dependency should throw an error
 TEST_F(IterationSubroutineTwoGridAccelerationTest, NullDependenciesThrow) {
-  constexpr int n_dependencies{ 5 };
+  constexpr int n_dependencies{ 4 };
   for (int i = 0; i < n_dependencies; ++i) {
     EXPECT_ANY_THROW({
       TwoGridSubroutine(i == 0 ? nullptr : std::make_unique<FluxCorrectorMock>(),
                         i == 1 ? nullptr : std::make_unique<FrameworkMock>(),
                         i == 2 ? nullptr : std::make_unique<IsotropicResidualCalculatorMock>(),
-                        i == 3 ? nullptr : cross_sections_ptr_,
-                        i == 4 ? nullptr : rhs_vector_ptr_);
+                        i == 3 ? nullptr : rhs_vector_ptr_);
     });
   }
 }
 
 TEST_F(IterationSubroutineTwoGridAccelerationTest, Execute) {
+  EXPECT_CALL(*this->isotropic_residual_calculator_mock_obs_ptr_, CalculateDomainResidual(current_moments_obs_ptr_,
+                                                                                          previous_moments_obs_ptr_))
+      .WillOnce(DoDefault());
+  EXPECT_CALL(*this->framework_mock_obs_ptr_, SolveSystem());
 
+  this->test_subroutine_->Execute(this->test_system_);
+
+  EXPECT_TRUE(test_helpers::AreEqual(this->total_isotropic_residual_, *this->rhs_vector_ptr_));
 }
 
 } // namespace
--- src/iteration/subroutine/two_grid_acceleration.cpp
+++ src/iteration/subroutine/two_grid_acceleration.cpp
@@ -5,19 +5,20 @@ namespace bart::iteration::subroutine {
 TwoGridAcceleration::TwoGridAcceleration(std::unique_ptr<FluxCorrector> flux_corrector_ptr,
                                          std::unique_ptr<Framework> framework_ptr,
                                          std::unique_ptr<ResidualCalculator> residual_calculator_ptr,
-                                         std::shared_ptr<CrossSections> cross_sections_ptr,
                                          std::shared_ptr<dealii::Vector<double>> isotropic_residual_ptr)
     : flux_corrector_ptr_(std::move(flux_corrector_ptr)),
       framework_ptr_(std::move(framework_ptr)),
       residual_calculator_ptr_(std::move(residual_calculator_ptr)),
-      cross_sections_ptr_(std::move(cross_sections_ptr)),
       isotropic_residual_ptr_(std::move(isotropic_residual_ptr)) {
   std::string function_name{ "TwoGridAcceleration constructor"};
   this->AssertPointerNotNull(flux_corrector_ptr_.get(), "flux corrector", function_name);
   this->AssertPointerNotNull(framework_ptr_.get(), "framework", function_name);
   this->AssertPointerNotNull(residual_calculator_ptr_.get(), "residual calculator", function_name);
-  this->AssertPointerNotNull(cross_sections_ptr_.get(), "cross-sections", function_name);
   this->AssertPointerNotNull(isotropic_residual_ptr_.get(), "isotropic residual vector", function_name);
+}
+
+auto TwoGridAcceleration::Execute(system::System &) -> void {
+
 }
 
 } // namespace bart::iteration::subroutine
